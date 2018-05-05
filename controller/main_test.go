76205a761a66fb8b6d053b4b96e00bcae12ebf90
Jon <38672162+gainsley@users.noreply.github.com>
2018-05-04 17:17:00 -0700

    add cloudlet and operator to controller; autogen test code (#7)

--- controller/main_test.go
+++ controller/main_test.go
@@ -1,11 +1,10 @@
 package main
 
 import (
-	"context"
-	"io"
 	"testing"
 
 	"github.com/mobiledgex/edge-cloud/proto"
+	"github.com/mobiledgex/edge-cloud/testutil"
 	"github.com/mobiledgex/edge-cloud/util"
 	"github.com/stretchr/testify/assert"
 	"google.golang.org/grpc"
@@ -35,43 +34,16 @@ func TestController(t *testing.T) {
 	}
 	defer conn.Close()
 
-	ctx := context.Background()
 	devApi := proto.NewDeveloperApiClient(conn)
-	_, err = devApi.CreateDeveloper(ctx, &Dev1)
-	assert.Nil(t, err, "create dev1")
-	_, err = devApi.CreateDeveloper(ctx, &Dev2)
-	assert.Nil(t, err, "create dev2")
-	_, err = devApi.CreateDeveloper(ctx, &Dev3)
-	assert.Nil(t, err, "create dev3")
-	_, err = devApi.CreateDeveloper(ctx, &Dev1)
-	assert.NotNil(t, err, "create dev1 again")
+	appApi := proto.NewAppApiClient(conn)
+	operApi := proto.NewOperatorApiClient(conn)
+	cloudletApi := proto.NewCloudletApiClient(conn)
 
-	devFilter := proto.Developer{}
-	devMap := make(map[proto.DeveloperKey]proto.Developer)
-	showDevStream, err := devApi.ShowDeveloper(ctx, &devFilter)
-	assert.Nil(t, err, "show")
-	if err == nil {
-		for {
-			showDev, err := showDevStream.Recv()
-			if err == io.EOF {
-				break
-			}
-			util.InfoLog("Show dev", "key", showDev.Key.Name)
-			assert.Nil(t, err, "show")
-			if err != nil {
-				break
-			}
-			devMap[*showDev.Key] = *showDev
-		}
-		_, found := devMap[*Dev1.Key]
-		assert.True(t, found, "Show Dev1")
-		_, found = devMap[*Dev2.Key]
-		assert.True(t, found, "Show Dev2")
-		_, found = devMap[*Dev3.Key]
-		assert.True(t, found, "Show Dev3")
-		_, found = devMap[*Dev4.Key]
-		assert.False(t, found, "Show Dev4)")
-	}
+	testutil.ClientDeveloperCudTest(t, devApi, DevData)
+	testutil.ClientAppCudTest(t, appApi, AppData)
+	testutil.ClientOperatorCudTest(t, operApi, OperatorData)
+	testutil.ClientCloudletCudTest(t, cloudletApi, CloudletData)
+
 	util.InfoLog("done")
 	// closing the signal channel triggers main to exit
 	close(sigChan)
--- protoc-gen-gomex/link.go
+++ protoc-gen-gomex/link.go
@@ -3,6 +3,5 @@
 package main
 
 import (
-	_ "github.com/golang/protobuf/protoc-gen-go/grpc"
 	_ "github.com/mobiledgex/edge-cloud/protoc-gen-gomex/mexgen"
 )
