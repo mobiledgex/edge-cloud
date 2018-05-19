50beecf9e934f104bcdef09c96a67cc2f7d9f30f
Jon Gainsley <jon.gainsley@mobiledgex.com>
2018-05-18 17:30:17 -0700

    auto-generated cli for proto methods

---
+++ edgectl/main.go
@@ -0,0 +1,83 @@
+package main
+
+import (
+	dme "github.com/mobiledgex/edge-cloud/d-match-engine/dme-proto"
+	"github.com/mobiledgex/edge-cloud/edgeproto"
+	"github.com/mobiledgex/edge-cloud/gencmd"
+	"github.com/mobiledgex/edge-cloud/util"
+	"github.com/spf13/cobra"
+	"google.golang.org/grpc"
+)
+
+var addr string
+var conn *grpc.ClientConn
+
+var rootCmd = &cobra.Command{
+	Use:               "edgectl",
+	PersistentPreRun:  connect,
+	PersistentPostRun: close,
+}
+var controllerCmd = &cobra.Command{
+	Use: "controller",
+}
+var dmeCmd = &cobra.Command{
+	Use: "dme",
+}
+var cloudletCmd = &cobra.Command{
+	Use: "cloudlet",
+}
+
+func connect(cmd *cobra.Command, args []string) {
+	var err error
+	conn, err = grpc.Dial(addr, grpc.WithInsecure())
+	if err != nil {
+		util.FatalLog("Connect to server failed", "addr", addr, "err", err)
+	}
+	gencmd.DeveloperApiCmd = edgeproto.NewDeveloperApiClient(conn)
+	gencmd.AppApiCmd = edgeproto.NewAppApiClient(conn)
+	gencmd.OperatorApiCmd = edgeproto.NewOperatorApiClient(conn)
+	gencmd.CloudletApiCmd = edgeproto.NewCloudletApiClient(conn)
+	gencmd.AppInstApiCmd = edgeproto.NewAppInstApiClient(conn)
+	gencmd.Match_Engine_ApiCmd = dme.NewMatch_Engine_ApiClient(conn)
+}
+
+func close(cmd *cobra.Command, args []string) {
+	conn.Close()
+}
+
+func main() {
+	rootCmd.AddCommand(controllerCmd)
+	rootCmd.AddCommand(dmeCmd)
+	rootCmd.AddCommand(cloudletCmd)
+	rootCmd.PersistentFlags().StringVar(&addr, "addr", "127.0.0.1:55001", "address to connect to")
+
+	controllerCmd.AddCommand(gencmd.CreateDeveloperCmd)
+	controllerCmd.AddCommand(gencmd.UpdateDeveloperCmd)
+	controllerCmd.AddCommand(gencmd.DeleteDeveloperCmd)
+	controllerCmd.AddCommand(gencmd.ShowDeveloperCmd)
+
+	controllerCmd.AddCommand(gencmd.CreateAppCmd)
+	controllerCmd.AddCommand(gencmd.UpdateAppCmd)
+	controllerCmd.AddCommand(gencmd.DeleteAppCmd)
+	controllerCmd.AddCommand(gencmd.ShowAppCmd)
+
+	controllerCmd.AddCommand(gencmd.CreateOperatorCmd)
+	controllerCmd.AddCommand(gencmd.UpdateOperatorCmd)
+	controllerCmd.AddCommand(gencmd.DeleteOperatorCmd)
+	controllerCmd.AddCommand(gencmd.ShowOperatorCmd)
+
+	controllerCmd.AddCommand(gencmd.CreateCloudletCmd)
+	controllerCmd.AddCommand(gencmd.UpdateCloudletCmd)
+	controllerCmd.AddCommand(gencmd.DeleteCloudletCmd)
+	controllerCmd.AddCommand(gencmd.ShowCloudletCmd)
+
+	controllerCmd.AddCommand(gencmd.CreateAppInstCmd)
+	controllerCmd.AddCommand(gencmd.UpdateAppInstCmd)
+	controllerCmd.AddCommand(gencmd.DeleteAppInstCmd)
+	controllerCmd.AddCommand(gencmd.ShowAppInstCmd)
+
+	dmeCmd.AddCommand(gencmd.FindCloudletCmd)
+	dmeCmd.AddCommand(gencmd.VerifyLocationCmd)
+
+	rootCmd.Execute()
+}
