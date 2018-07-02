5ad2fe6ba80b8577ed0a598525fc28530e4be1e2
Jon <38672162+gainsley@users.noreply.github.com>
2018-07-02 14:46:32 -0700

    added debug flag to dme (#66)

--- d-match-engine/dme-server/dme-main.go
+++ d-match-engine/dme-server/dme-main.go
@@ -3,12 +3,12 @@ package main
 import (
 	"flag"
 	"fmt"
-	"log"
 	"net"
 
 	dme "github.com/mobiledgex/edge-cloud/d-match-engine/dme-proto"
 	dmetest "github.com/mobiledgex/edge-cloud/d-match-engine/dme-testutil"
 	"github.com/mobiledgex/edge-cloud/edgeproto"
+	"github.com/mobiledgex/edge-cloud/log"
 	"golang.org/x/net/context"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/reflection"
@@ -19,6 +19,7 @@ var rootDir = flag.String("r", "", "root directory for testing")
 var notifyAddrs = flag.String("notifyAddrs", "127.0.0.1:50001", "Comma separated list of controller notify listener addresses")
 var apiAddr = flag.String("apiAddr", "0.0.0.0:50051", "API listener address")
 var standalone = flag.Bool("standalone", false, "Standalone mode. AppInst data is pre-populated. Dme does not interact with controller. AppInsts can be created directly on Dme using controller AppInst API")
+var debugLevels = flag.String("d", "", fmt.Sprintf("comma separated list of %v", log.DebugLevelStrings))
 
 // server is used to implement helloworld.GreeterServer.
 type server struct{}
@@ -69,6 +70,7 @@ func (s *server) RegisterClient(ctx context.Context,
 
 func main() {
 	flag.Parse()
+	log.SetDebugLevelStrs(*debugLevels)
 
 	setupMatchEngine()
 
@@ -86,7 +88,7 @@ func main() {
 
 	lis, err := net.Listen("tcp", *apiAddr)
 	if err != nil {
-		log.Fatalf("failed to listen: %v", err)
+		log.FatalLog("Failed to listen", "addr", *apiAddr, "err", err)
 	}
 	s := grpc.NewServer()
 	dme.RegisterMatch_Engine_ApiServer(s, &server{})
@@ -99,6 +101,6 @@ func main() {
 	// Register reflection service on gRPC server.
 	reflection.Register(s)
 	if err := s.Serve(lis); err != nil {
-		log.Fatalf("failed to serve: %v", err)
+		log.FatalLog("Failed to server", "err", err)
 	}
 }
