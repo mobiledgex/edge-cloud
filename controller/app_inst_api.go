40e27c37572e5b8619bb38aa9fd8fa9832beab86
Jon <38672162+gainsley@users.noreply.github.com>
2018-07-03 15:04:00 -0700

    EDGECLOUD-39 disallow delete of objects with dependencies (#70)
    * EDGECLOUD-39 disallow delete of objects with dependencies

--- controller/app_inst_api.go
+++ controller/app_inst_api.go
@@ -35,6 +35,38 @@ func (s *AppInstApi) Get(key *edgeproto.AppInstKey, val *edgeproto.AppInst) bool
 	return s.cache.Get(key, val)
 }
 
+func (s *AppInstApi) UsesCloudlet(in *edgeproto.CloudletKey, dynInsts map[edgeproto.AppInstKey]struct{}) bool {
+	s.cache.Mux.Lock()
+	defer s.cache.Mux.Unlock()
+	static := false
+	for key, val := range s.cache.Objs {
+		if key.CloudletKey.Matches(in) {
+			if val.Liveness == edgeproto.AppInst_STATIC {
+				static = true
+			} else if val.Liveness == edgeproto.AppInst_DYNAMIC {
+				dynInsts[key] = struct{}{}
+			}
+		}
+	}
+	return static
+}
+
+func (s *AppInstApi) UsesApp(in *edgeproto.AppKey, dynInsts map[edgeproto.AppInstKey]struct{}) bool {
+	s.cache.Mux.Lock()
+	defer s.cache.Mux.Unlock()
+	static := false
+	for key, val := range s.cache.Objs {
+		if key.AppKey.Matches(in) {
+			if val.Liveness == edgeproto.AppInst_STATIC {
+				static = true
+			} else if val.Liveness == edgeproto.AppInst_DYNAMIC {
+				dynInsts[key] = struct{}{}
+			}
+		}
+	}
+	return static
+}
+
 func (s *AppInstApi) CreateAppInst(ctx context.Context, in *edgeproto.AppInst) (*edgeproto.Result, error) {
 	// cache app path in app inst
 	var app edgeproto.App
