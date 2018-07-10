70c33647c47075cd736778f626fb9d23b174efe7
lgarner <1672745+lgarner@users.noreply.github.com>
2018-07-10 11:34:22 -0700

    Sync to new dme-proto: Find_Status intead of boolean. Upgrade grpc to 1.13.1. (#74)

--- edge-mvp/android/EmptyMatchEngineApp/matchingengine/src/main/java/com/mobiledgex/matchingengine/FindCloudletResponse.java
+++ edge-mvp/android/EmptyMatchEngineApp/matchingengine/src/main/java/com/mobiledgex/matchingengine/FindCloudletResponse.java
@@ -6,10 +6,34 @@ public class FindCloudletResponse {
     public byte[] server;
     public int port;
     public GPSLocation loc;
-    public boolean status;
+
+    public enum Find_Status {
+        FIND_UNKNOWN(0),
+        FIND_FOUND(1),
+        FIND_NOTFOUND(2);
+        private int find_status;
+        Find_Status(final int find_status) {
+            this.find_status = find_status;
+        }
+
+        public int getStatus() {
+            return find_status;
+        }
+
+        // Helper to get corresponding enum value from int value.
+        public static Find_Status forNumber(int value) {
+            switch (value) {
+                case 0: return FIND_UNKNOWN;
+                case 1: return FIND_FOUND;
+                case 2: return FIND_NOTFOUND;
+                default: return null;
+            }
+        }
+    }
+    public Find_Status status;
     public String token = "";
 
-    FindCloudletResponse(long version, String uri, byte[] server, int port, GPSLocation loc, boolean status, String token) {
+    FindCloudletResponse(long version, String uri, byte[] server, int port, GPSLocation loc, Find_Status status, String token) {
         this.version = version;
         this.uri = uri;
         this.server = server;
