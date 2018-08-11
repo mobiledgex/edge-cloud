82517b38a9ce77141c3e4ad83a78c769c8206705
Garner Lee <1672745+lgarner@users.noreply.github.com>
2018-08-10 17:54:32 -0700

    Additional canned requestNetwork util funcitons in NetworkManager. Log time to active link.

--- edge-mvp/android/EmptyMatchEngineApp/matchingengine/src/main/java/com/mobiledgex/matchingengine/NetworkManager.java
+++ edge-mvp/android/EmptyMatchEngineApp/matchingengine/src/main/java/com/mobiledgex/matchingengine/NetworkManager.java
@@ -13,12 +13,6 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
 
-import static android.net.NetworkCapabilities.NET_CAPABILITY_INTERNET;
-import static android.net.NetworkCapabilities.TRANSPORT_BLUETOOTH;
-import static android.net.NetworkCapabilities.TRANSPORT_CELLULAR;
-import static android.net.NetworkCapabilities.TRANSPORT_ETHERNET;
-import static android.net.NetworkCapabilities.TRANSPORT_WIFI;
-
 public class NetworkManager {
     public static final String TAG = "NetworkManager";
     public static NetworkManager mNetworkManager;
@@ -28,37 +22,20 @@ public class NetworkManager {
     private final Object mWaitForActiveNetwork = new Object();
     private long mNetworkActiveTimeoutMilliseconds = 5000;
     private final Object mSyncObject = new Object();
-    private long mTimeoutInMilliseconds = 5000;
+    private long mTimeoutInMilliseconds = 10000;
 
     private Network mNetwork;
     private NetworkRequest mDefaultRequest;
 
     private ExecutorService mThreadPool;
+    private boolean networkSwitchingEnabled = true;
 
-    public enum NetworkType {
-        TRANSPORT_UNKNOWN,
-        TRANSPORT_BLUETOOTH,
-        TRANSPORT_WIFI,
-        TRANSPORT_ETHERNET,
-        TRANSPORT_CELL
+    public boolean isNetworkSwitchingEnabled() {
+        return networkSwitchingEnabled;
     }
-    NetworkType currentNetworkType;
 
-    public NetworkType getCurrentNetworkType() {
-        Network currentNetwork = mConnectivityManager.getActiveNetwork();
-        NetworkCapabilities capabilities = mConnectivityManager.getNetworkCapabilities(currentNetwork);
-
-        if (capabilities.hasCapability(TRANSPORT_WIFI)) {
-            return NetworkType.TRANSPORT_WIFI;
-        } else if (capabilities.hasCapability(TRANSPORT_ETHERNET)) {
-            return NetworkType.TRANSPORT_ETHERNET;
-        } else if (capabilities.hasCapability(TRANSPORT_CELLULAR)) {
-            return NetworkType.TRANSPORT_CELL;
-        } else if (capabilities.hasCapability(TRANSPORT_BLUETOOTH)) {
-            return NetworkType.TRANSPORT_BLUETOOTH;
-        } else {
-            return NetworkType.TRANSPORT_UNKNOWN;
-        }
+    public void setNetworkSwitchingEnabled(boolean networkSwitchingEnabled) {
+        this.networkSwitchingEnabled = networkSwitchingEnabled;
     }
 
     /**
@@ -70,6 +47,10 @@ public class NetworkManager {
      */
     synchronized void runOnNetwork(Callable callable, NetworkRequest networkRequest)
             throws InterruptedException, ExecutionException {
+        if (networkSwitchingEnabled == false) {
+            Log.e(TAG, "NetworkManager is disabled.");
+            return;
+        }
         switchToNetworkBlocking(networkRequest);
         Future<Callable> future = mThreadPool.submit(callable);
         future.get();
@@ -122,34 +103,44 @@ public class NetworkManager {
         this.mNetworkActiveTimeoutMilliseconds = networkActiveTimeoutMilliseconds;
     }
 
-    public NetworkRequest getBluetoothNetworkRequest() {
+    public NetworkRequest getCellularNetworkRequest() {
         NetworkRequest networkRequest = new NetworkRequest.Builder()
-                .addTransportType(TRANSPORT_BLUETOOTH)
-                .addCapability(NET_CAPABILITY_INTERNET)
-                .build();
-        return networkRequest;
-    }
-
-    public NetworkRequest getEthernetNetworkRequest() {
-        NetworkRequest networkRequest = new NetworkRequest.Builder()
-                .addTransportType(TRANSPORT_ETHERNET)
-                .addCapability(NET_CAPABILITY_INTERNET)
+                .addTransportType(NetworkCapabilities.TRANSPORT_CELLULAR)
+                .addCapability(NetworkCapabilities.NET_CAPABILITY_INTERNET)
                 .build();
         return networkRequest;
     }
 
     public NetworkRequest getWifiNetworkRequest() {
         NetworkRequest networkRequest = new NetworkRequest.Builder()
-                .addTransportType(TRANSPORT_WIFI)
-                .addCapability(NET_CAPABILITY_INTERNET)
+                .addTransportType(NetworkCapabilities.TRANSPORT_WIFI)
+                .addCapability(NetworkCapabilities.NET_CAPABILITY_INTERNET)
                 .build();
         return networkRequest;
     }
 
-    public NetworkRequest getCellularNetworkRequest() {
+    public NetworkRequest getBluetoothNetworkRequest() {
         NetworkRequest networkRequest = new NetworkRequest.Builder()
-                .addTransportType(TRANSPORT_CELLULAR)
-                .addCapability(NET_CAPABILITY_INTERNET)
+                .addTransportType(NetworkCapabilities.TRANSPORT_BLUETOOTH)
+                .addCapability(NetworkCapabilities.NET_CAPABILITY_INTERNET)
+                .build();
+        return networkRequest;
+    }
+
+    public NetworkRequest getEthernetNetworkRequest() {
+        NetworkRequest networkRequest = new NetworkRequest.Builder()
+                .addTransportType(NetworkCapabilities.TRANSPORT_ETHERNET)
+                .addCapability(NetworkCapabilities.NET_CAPABILITY_INTERNET)
+                .build();
+        return networkRequest;
+    }
+
+    public NetworkRequest getWiFiAwareNetworkRequest() {
+        NetworkRequest networkRequest = new NetworkRequest.Builder()
+                .addTransportType(NetworkCapabilities.TRANSPORT_WIFI_AWARE)
+                .addTransportType(NetworkCapabilities.TRANSPORT_WIFI)
+                .addTransportType(NetworkCapabilities.TRANSPORT_CELLULAR)
+                .addCapability(NetworkCapabilities.NET_CAPABILITY_INTERNET)
                 .build();
         return networkRequest;
     }
@@ -159,22 +150,37 @@ public class NetworkManager {
     }
 
     public void resetNetworkToDefault() throws InterruptedException, ExecutionException {
+        if (networkSwitchingEnabled == false) {
+            Log.e(TAG, "NetworkManager is disabled.");
+            return;
+        }
+
         if (mDefaultRequest == null) {
             mConnectivityManager.bindProcessToNetwork(null);
         } else {
             switchToNetworkBlocking(mDefaultRequest);
         }
+
+        NetworkCapabilities networkCapabilities = mConnectivityManager.getNetworkCapabilities(mConnectivityManager.getActiveNetwork());
+        logTransportCapabilities(networkCapabilities);
+
     }
 
     class NetworkSwitcherCallable implements Callable {
         NetworkRequest mNetworkRequest;
+        boolean activeListenerAdded = false;
         final long start = System.currentTimeMillis();
 
         NetworkSwitcherCallable(NetworkRequest networkRequest) {
             mNetworkRequest = networkRequest;
         }
         @Override
-        public Network call() throws InterruptedException {
+        public Network call() throws InterruptedException, NetworkRequestTimeoutException {
+            if (networkSwitchingEnabled == false) {
+                Log.e(TAG, "NetworkManager is disabled.");
+                return null;
+            }
+
             final ConnectivityManager.OnNetworkActiveListener activeListener = new ConnectivityManager.OnNetworkActiveListener() {
                 @Override
                 public void onNetworkActive() {
@@ -190,11 +196,12 @@ public class NetworkManager {
                     mWaitingForLink = true;
                 }
 
-                mConnectivityManager.requestNetwork(mNetworkRequest, new ConnectivityManager.NetworkCallback() {
+                ConnectivityManager.NetworkCallback networkCallback = new ConnectivityManager.NetworkCallback() {
                     @Override
                     public void onAvailable(Network network) {
                         Log.d(TAG, "requestNetwork onAvailable(), binding process to network.");
                         mConnectivityManager.bindProcessToNetwork(network);
+                        activeListenerAdded = true;
                         mConnectivityManager.addDefaultNetworkActiveListener(activeListener);
                         mNetwork = network;
                     }
@@ -202,11 +209,7 @@ public class NetworkManager {
                     @Override
                     public void onCapabilitiesChanged(Network network, NetworkCapabilities networkCapabilities) {
                         Log.d(TAG, "requestNetwork onCapabilitiesChanged(): " + network.toString());
-                        Log.d(TAG, " -- networkCapabilities: TRANSPORT_WIFI: " + networkCapabilities.hasTransport(TRANSPORT_WIFI));
-                        Log.d(TAG, " -- networkCapabilities: TRANSPORT_CELLULAR: " + networkCapabilities.hasTransport(TRANSPORT_CELLULAR));
-                        Log.d(TAG, " -- networkCapabilities: TRANSPORT_ETHERNET: " + networkCapabilities.hasTransport(TRANSPORT_ETHERNET));
-                        Log.d(TAG, " -- networkCapabilities: TRANSPORT_BLUETOOTH: " + networkCapabilities.hasTransport(TRANSPORT_BLUETOOTH));
-                        Log.d(TAG, " -- networkCapabilities: NET_CAPABILITY_INTERNET: " + networkCapabilities.hasCapability(NET_CAPABILITY_INTERNET));
+                        logTransportCapabilities(networkCapabilities);
                     }
 
                     @Override
@@ -231,7 +234,10 @@ public class NetworkManager {
                         Log.d(TAG, "requestNetwork onLost(): " + network.toString());
                     }
 
-                });
+                };
+                mConnectivityManager.requestNetwork(mNetworkRequest, networkCallback);
+
+                // Wait for availability.
                 synchronized (mSyncObject) {
                     long timeStart = System.currentTimeMillis();
                     long elapsed;
@@ -239,8 +245,21 @@ public class NetworkManager {
                             (elapsed = System.currentTimeMillis() - timeStart) < mTimeoutInMilliseconds) {
                         mSyncObject.wait(mTimeoutInMilliseconds - elapsed);
                     }
+                    if (mWaitingForLink) {
+                        // Timed out waiting for available network.
+                        NetworkCapabilities networkCapabilities = mConnectivityManager.getNetworkCapabilities(mConnectivityManager.getActiveNetwork());
+                        mConnectivityManager.unregisterNetworkCallback(networkCallback);
+                        mNetwork = null;
+                        mNetworkRequest = null;
+
+                        logTransportCapabilities(networkCapabilities);
+                        throw new NetworkRequestTimeoutException("NetworkRequest timed out with no availability.");
+                    }
+                    elapsed = System.currentTimeMillis() - timeStart;
+                    Log.d(TAG, "Elapsed time waiting for link: " + elapsed);
                     mNetworkRequest = null;
                 }
+
                 // Network is available, and link is up, but may not be active yet.
                 if (!mConnectivityManager.isDefaultNetworkActive()) {
                     synchronized (mWaitForActiveNetwork) {
@@ -248,12 +267,24 @@ public class NetworkManager {
                     }
                 }
             } finally {
-                if (activeListener != null) {
+                if (activeListenerAdded) {
                     mConnectivityManager.removeDefaultNetworkActiveListener(activeListener);
                 }
             }
             return mNetwork;
         }
+    }
+
+    private void logTransportCapabilities(NetworkCapabilities networkCapabilities) {
+        Log.d(TAG, " -- networkCapabilities: TRANSPORT_CELLULAR: " + networkCapabilities.hasCapability(NetworkCapabilities.TRANSPORT_CELLULAR));
+        Log.d(TAG, " -- networkCapabilities: TRANSPORT_WIFI: " + networkCapabilities.hasCapability(NetworkCapabilities.TRANSPORT_WIFI));
+        Log.d(TAG, " -- networkCapabilities: TRANSPORT_BLUETOOTH: " + networkCapabilities.hasCapability(NetworkCapabilities.TRANSPORT_BLUETOOTH));
+        Log.d(TAG, " -- networkCapabilities: TRANSPORT_ETHERNET: " + networkCapabilities.hasCapability(NetworkCapabilities.TRANSPORT_ETHERNET));
+        Log.d(TAG, " -- networkCapabilities: TRANSPORT_VPN: " + networkCapabilities.hasCapability(NetworkCapabilities.TRANSPORT_VPN));
+        Log.d(TAG, " -- networkCapabilities: TRANSPORT_WIFI_AWARE: " + networkCapabilities.hasCapability(NetworkCapabilities.TRANSPORT_WIFI_AWARE));
+        Log.d(TAG, " -- networkCapabilities: TRANSPORT_LOWPAN: " + networkCapabilities.hasCapability(NetworkCapabilities.TRANSPORT_LOWPAN));
+
+        Log.d(TAG, " -- networkCapabilities: NET_CAPABILITY_INTERNET: " + networkCapabilities.hasCapability(NetworkCapabilities.NET_CAPABILITY_INTERNET));
     }
 
     public boolean isCurrentNetworkInternetCellularDataCapable() {
@@ -263,8 +294,8 @@ public class NetworkManager {
             Network network = mConnectivityManager.getBoundNetworkForProcess();
             if (network != null) {
                 NetworkCapabilities networkCapabilities = mConnectivityManager.getNetworkCapabilities(network);
-                if (networkCapabilities.hasCapability(TRANSPORT_CELLULAR) &&
-                        networkCapabilities.hasCapability(NET_CAPABILITY_INTERNET)) {
+                if (networkCapabilities.hasCapability(NetworkCapabilities.TRANSPORT_CELLULAR) &&
+                        networkCapabilities.hasCapability(NetworkCapabilities.NET_CAPABILITY_INTERNET)) {
                     hasDataCellCapabilities = true;
                 }
             }
@@ -282,15 +313,15 @@ public class NetworkManager {
         }
 
         NetworkRequest networkRequest = new NetworkRequest.Builder()
-                .addTransportType(TRANSPORT_CELLULAR)
-                .addCapability(NET_CAPABILITY_INTERNET)
+                .addTransportType(NetworkCapabilities.TRANSPORT_CELLULAR)
+                .addCapability(NetworkCapabilities.NET_CAPABILITY_INTERNET)
                 .build();
 
         switchToNetwork(networkRequest, networkCallback);
     }
 
     /**
-     * Switch to a particular network type in a blocking fashion for synchronous execution blocks.
+     * Switch to a particular network type. This is a synchronous call.
      * @return
      */
     public Network switchToCellularInternetNetworkBlocking() throws InterruptedException, ExecutionException {
@@ -367,12 +398,7 @@ public class NetworkManager {
 
             @Override
             public void onCapabilitiesChanged(Network network, NetworkCapabilities networkCapabilities) {
-                Log.d(TAG, "requestNetwork onCapabilitiesChanged()");
-                Log.d(TAG, " -- networkCapabilities: TRANSPORT_WIFI: " + networkCapabilities.hasTransport(TRANSPORT_WIFI));
-                Log.d(TAG, " -- networkCapabilities: TRANSPORT_CELLULAR: " + networkCapabilities.hasTransport(TRANSPORT_CELLULAR));
-                Log.d(TAG, " -- networkCapabilities: TRANSPORT_ETHERNET: " + networkCapabilities.hasTransport(TRANSPORT_ETHERNET));
-                Log.d(TAG, " -- networkCapabilities: TRANSPORT_BLUETOOTH: " + networkCapabilities.hasTransport(TRANSPORT_BLUETOOTH));
-                Log.d(TAG, " -- networkCapabilities: NET_CAPABILITY_INTERNET: " + networkCapabilities.hasCapability(NET_CAPABILITY_INTERNET));
+                logTransportCapabilities(networkCapabilities);
                 if (networkCallback == null) {
                     networkCallback.onCapabilitiesChanged(network, networkCapabilities);
                 }
