0e02e95b79c70abdd2cfa5221cd06be292ea9fbb
anand12gupta <anand.pb.gupta@gmail.com>
2018-08-25 08:26:09 -0500

    EDGECLOUD-133 Display data for local deployment using REST API (#140)
    This is temporary solution. Will explore support for json sequence/stream later.

--- mexui/src/app/appinsts/appinsts.component.ts
+++ mexui/src/app/appinsts/appinsts.component.ts
@@ -13,8 +13,36 @@ export class AppinstsComponent implements OnInit {
 
   ngOnInit() {
     this.data.getAppInstances().subscribe(
-      (data) => (this.appinsts$ = data )
-   );
+      (data) => {
+        data = "[" + data.split("}\n{").join("},\n{") + "]";
+        this.appinsts$ = JSON.parse(data);
+      }
+    );
   }
+
+//   ngOnInit() {
+//     this.data.getAppInstances().subscribe(
+//       (data) => {
+//         // var data1 = '[' + data.replace('/\}\n\{/mg', '},\n{') + ']';
+//         console.log(data);
+//         var str1 = data;
+//         var str2="[";
+// for (var i=0; i < str1.length; i++) {
+//      //console.log(str1.charAt(i));
+//      str2 += str1.charAt(i);
+//      if ( i < (str1.length -2) ) {
+//              if (str1.charAt(i) === '}' && (str1.charAt(i+2) === '{')) {
+//                   str2 += ',';
+//              }
+//      }
+// }
+//         str2 += ']';
+//         console.log(str2);
+//         var data2 = JSON.parse(str2);
+        
+//         this.appinsts$ = data2; 
+//       }
+//    );
+//   }
 
 }
