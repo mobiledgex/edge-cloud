df36ac2a19631a3bae7b86b425a91d1e5011cc38
Anand Gupta <anandgupta@a.attlocal.net>
2018-08-27 19:30:20 -0500

    EDGECLOUD-134 Basic CRUD for MEX Platform Entities

--- mexui/src/app/flavors/flavors.component.ts
+++ mexui/src/app/flavors/flavors.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { DevdataService } from '../devdata.service';
+import {Flavor} from '../app.model';
 
 @Component({
   selector: 'app-flavors',
@@ -8,14 +9,82 @@ import { DevdataService } from '../devdata.service';
 })
 export class FlavorsComponent implements OnInit {
    flavors$: Object;
+   flavors: Flavor[] = [];  
+  flvModel: Flavor;           
+  showNew: Boolean = false;    
+  submitType: string = 'Save';   
+  selectedRow: number;           
+  
   constructor(private data: DevdataService) { }
 
   ngOnInit() {
     this.data.getFlavors().subscribe(
-      (data) => (
-        this.flavors$ = JSON.parse("[" + data.split('}\n{').join('},\n{') + "]")
-      )
+      (data) => {
+        var obj = JSON.parse("[" + data.split('}\n{').join('},\n{') + "]")
+        var flvlist: Flavor[] = [];
+        obj.forEach(function(entry) {
+          flvlist.push(new Flavor(entry.result.key.name, 
+                                             entry.result.ram,
+                                             entry.result.vcpus,
+                                             entry.result.disk
+                                            ));
+      });
+        this.flavors$ = obj;
+        this.flavors = flvlist;
+      }
    );
   }
 
+  onNew(){
+    this.flvModel = new Flavor();
+    this.submitType = 'Save';
+    this.showNew = true;
+ }
+
+ onSave(){
+   if (this.submitType === 'Save') {
+     
+     this.data.createFlavor(this.flvModel).subscribe(
+        (data) => {
+         this.flavors.push(this.flvModel);
+          // console.log(this.flvModel);
+        }
+     );
+   } else {
+     this.data.updateFlavor(this.flvModel).subscribe(
+       (data) => {
+     // Update existing properties based on the model
+     this.flavors[this.selectedRow].name = this.flvModel.name;
+     this.flavors[this.selectedRow].ram = this.flvModel.ram;
+     this.flavors[this.selectedRow].vcpus = this.flvModel.vcpus;
+     this.flavors[this.selectedRow].disk  = this.flvModel.disk;
+       }
+     );
+   }
+   this.showNew = false;
+ }
+
+ 
+ onEdit(index: number){
+     this.selectedRow = index;
+     this.flvModel = new Flavor();
+     this.flvModel = Object.assign({}, this.flavors[this.selectedRow]);
+     this.submitType = 'Update';
+     this.showNew = true;
+ }
+
+ onDelete(index: number) {
+   this.selectedRow = index;
+   this.flvModel = new Flavor();
+   this.flvModel = Object.assign({}, this.flavors[this.selectedRow]);
+   this.data.deleteFlavor(this.flvModel).subscribe(
+     (data) => {
+         this.flavors.splice(index,1);
+     }
+   );
+ }
+
+ onCancel() {
+   this.showNew = false;
+ }
 }
--- mexui/src/app/operators/operators.component.ts
+++ mexui/src/app/operators/operators.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { DevdataService } from '../devdata.service';
-import { Observable } from 'rxjs/Observable';
+import {Oprator} from '../app.model';
 
 @Component({
   selector: 'app-operators',
@@ -9,13 +9,76 @@ import { Observable } from 'rxjs/Observable';
 })
 export class OperatorsComponent implements OnInit {
   operators$: Object;
+  operators: Oprator[] = [];
+  oprModel: Oprator;
+  showNew: Boolean = false;    
+  submitType: string = 'Save';   
+  selectedRow: number;           
+  
   constructor(private data: DevdataService) { }
 
   ngOnInit() {
     this.data.getOperators().subscribe(
-      (data) => (
-        this.operators$ = JSON.parse("[" + data.split('}\n{').join('},\n{') + "]"))
-   );
+      (data) => {
+        var obj =  JSON.parse("[" + data.split('}\n{').join('},\n{') + "]");
+        var oprlist : Oprator[] = [];
+        obj.forEach(function(entry) {
+          oprlist.push(new Oprator(entry.result.key.name));
+        });
+        this.operators$ = obj;
+        this.operators = oprlist;
+      });   
   }
+  
+  onNew(){
+    this.oprModel = new Oprator();
+    this.submitType = 'Save';
+    this.showNew = true;
+ }
 
+ onSave(){
+   if (this.submitType === 'Save') {
+     
+     this.data.createOperator(this.oprModel).subscribe(
+        (data) => {
+         this.operators.push(this.oprModel);
+          // console.log(this.oprModel);
+        }
+     );
+   } else {
+     this.data.updateOperator(this.oprModel).subscribe(
+       (data) => {
+           // Update existing properties based on the model
+           this.operators[this.selectedRow].name = this.oprModel.name;
+       }
+     );
+   }
+   this.showNew = false;
+ }
+
+ 
+ onEdit(index: number){
+     this.selectedRow = index;
+     this.oprModel = new Oprator();
+     this.oprModel = Object.assign({}, this.operators[this.selectedRow]);
+     this.submitType = 'Update';
+     this.showNew = true;
+ }
+
+ onDelete(index: number) {
+   this.selectedRow = index;
+   this.oprModel = new Oprator();
+   this.oprModel = Object.assign({}, this.operators[this.selectedRow]);
+   this.data.deleteOperator(this.oprModel).subscribe(
+     (data) => {
+         this.operators.splice(index,1);
+     }
+   );
+ }
+
+ onCancel() {
+   this.showNew = false;
+ }
 }
+
+
