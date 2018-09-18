27ac6010893b815c5e2053f6dc7f97e8f941fc8b
lgarner <1672745+lgarner@users.noreply.github.com>
2018-09-18 15:59:11 -0700

    Mex GRPC Unity Library (#171)
    MEX Unity GRPC Library, and Sample App for Android.
    
    * Initial working Makefile for GRPC-csharp AppClient generation.
    * Update makefile to restore dependencies referenced in csproj.
    * Rename csharp to unity. Move unity gprc references into packages/Plugins.
    * Initial Mex GRPC Sample App using Mex GPRC only unity library.
    * Update Makefile, add README.md for experimental support in Unity.

---
+++ edge-mvp/unity/MexGrpcSample/Assets/ButtonScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine.UI;
+
+public class ButtonScript : MonoBehaviour {
+    public Button button1;
+    public MexGrpcSample mexGrpcSample;
+
+    void Start () {
+        Button btn = button1.GetComponent<Button>();
+        btn.onClick.AddListener(TaskOnClick);
+    }
+
+    void TaskOnClick(){
+        Debug.Log ("You have clicked the button!");
+        mexGrpcSample.RunSampleFlow();
+    }
+
+}
