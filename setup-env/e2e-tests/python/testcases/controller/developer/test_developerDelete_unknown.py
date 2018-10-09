6606089cf02b94a49b28abc6aae4cb110e568f82
Andy Anderson <andy.anderson@mobiledgex.com>
2018-10-09 17:54:49 -0500

    update developer testcases

--- setup-env/e2e-tests/python/testcases/controller/developer/test_developerDelete_unknown.py
+++ setup-env/e2e-tests/python/testcases/controller/developer/test_developerDelete_unknown.py
@@ -10,6 +10,7 @@ import grpc
 import sys
 import time
 from delayedassert import expect, expect_equal, assert_expectations
+import logging
 
 import mex_controller
 
@@ -20,6 +21,9 @@ developer_name = 'dummyDeveloper'
 mex_root_cert = 'mex-ca.crt'
 mex_cert = 'localserver.crt'
 mex_key = 'localserver.key'
+
+logger = logging.getLogger()
+logger.setLevel(logging.DEBUG)
 
 class tc(unittest.TestCase):
     def setUp(self):
@@ -39,7 +43,7 @@ class tc(unittest.TestCase):
         try:
             self.controller.delete_developer(self.developer.developer)
         except grpc.RpcError as e:
-            print('got exception', e)
+            logger.info('got exception ' + str(e))
             error = e
 
         # print developers after add
