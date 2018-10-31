ec7f03c9f50087a127c7af82bf098812cdced0c6
Andy Anderson <andy.anderson@mobiledgex.com>
2018-10-31 09:48:28 -0500

    add flavor testcases  Changes to be committed: 	modified:   README.md 	modified:   modules/mex_controller.py 	new file:   testcases/controller/flavor/README.md 	new file:   testcases/controller/flavor/__init__.py 	new file:   testcases/controller/flavor/test_flavorAdd_100.py 	new file:   testcases/controller/flavor/test_flavorAdd_disk0.py 	new file:   testcases/controller/flavor/test_flavorAdd_invalidName.py 	new file:   testcases/controller/flavor/test_flavorAdd_invalidParms.py 	new file:   testcases/controller/flavor/test_flavorAdd_largerThanMaxValues.py 	new file:   testcases/controller/flavor/test_flavorAdd_maxValues.py 	new file:   testcases/controller/flavor/test_flavorAdd_minValues.py 	new file:   testcases/controller/flavor/test_flavorAdd_multiControllers.py 	new file:   testcases/controller/flavor/test_flavorAdd_nameOnly.py 	new file:   testcases/controller/flavor/test_flavorAdd_noName.py 	new file:   testcases/controller/flavor/test_flavorAdd_ram0.py 	new file:   testcases/controller/flavor/test_flavorAdd_sameName.py 	new file:   testcases/controller/flavor/test_flavorAdd_vcpus0.py 	new file:   testcases/controller/flavor/test_flavorDelete_beforeClusterFlavor.py 	new file:   testcases/controller/flavor/test_flavorDelete_nameOnly.py 	new file:   testcases/controller/flavor/test_flavorDelete_unknown.py 	new file:   testcases/controller/flavor/test_flavorDelete_wrongParms.py 	new file:   testcases/controller/flavor/test_flavorShow_ramVcupsDisk.py 	new file:   testcases/controller/flavor/test_flavorShow_single.py 	new file:   testcases/controller/flavor/test_flavorUpdate_notSupported.py

---
+++ setup-env/e2e-tests/python/testcases/controller/flavor/test_flavorAdd_disk0.py
@@ -0,0 +1,60 @@
+#!/usr/local/bin/python3
+
+#
+# create flavor with disk=0
+# verify 'Disk cannot be 0' is received
+# 
+
+import unittest
+import grpc
+import sys
+import time
+from delayedassert import expect, expect_equal, assert_expectations
+import logging
+
+import mex_controller
+
+controller_address = '127.0.0.1:55001'
+
+mex_root_cert = 'mex-ca.crt'
+mex_cert = 'localserver.crt'
+mex_key = 'localserver.key'
+
+logger = logging.getLogger()
+logger.setLevel(logging.DEBUG)
+
+class tc(unittest.TestCase):
+    @classmethod
+    def setUpClass(self):
+        self.controller = mex_controller.Controller(controller_address = controller_address,
+                                                    root_cert = mex_root_cert,
+                                                    key = mex_key,
+                                                    client_cert = mex_cert
+                                                   )
+
+    def test_createFlavorDisk0OtherParms(self):
+        # print flavors before add
+        flavor_pre = self.controller.show_flavors()
+
+        # create flavor
+        error = None
+        self.flavor = mex_controller.Flavor(flavor_name='myflavor', ram=1, vcpus=1, disk=0)
+        try:
+            self.controller.create_flavor(self.flavor.flavor)
+        except grpc.RpcError as e:
+            logger.info('got exception ' + str(e))
+            error = e
+
+        # print flavors after add
+        flavor_post = self.controller.show_flavors()
+
+        expect_equal(error.code(), grpc.StatusCode.UNKNOWN, 'status code')
+        expect_equal(error.details(), 'Disk cannot be 0', 'error details')
+        expect_equal(len(flavor_post), len(flavor_pre), 'num flavor')
+
+        assert_expectations()
+
+if __name__ == '__main__':
+    suite = unittest.TestLoader().loadTestsFromTestCase(tc)
+    sys.exit(not unittest.TextTestRunner().run(suite).wasSuccessful())
+
