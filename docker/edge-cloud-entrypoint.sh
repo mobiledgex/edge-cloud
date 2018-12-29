f0e161ac555b5a3cc858364a55c4582584999dfe
bobmex <38759636+bobmex@users.noreply.github.com>
2018-12-28 21:10:54 -0800

    massive infra related changes (#267)
    * fix security group rule per service deployment. pass cluster flavor to kvm
    
    * docker swarm-mode support
    
    * vault and manifest
    
    * vault manifest
    
    * readme
    
    * git ignore
    
    * refactor
    
    * add registry
    
    * mex
    
    * cloud state not present
    
    * revised with new infra
    
    * working version
    
    * broken network
    
    * working docker compose test
    
    * working docker swarm
    
    * new version infra

--- docker/edge-cloud-entrypoint.sh
+++ docker/edge-cloud-entrypoint.sh
@@ -4,46 +4,50 @@ set -e
 source /root/mex-docker.env
 
 if [ $# -le 0 ]; then
-	echo "which program? argument required"
-	exit 1
+    echo "which program? argument required"
+    exit 1
 fi
 
 case "$1" in
-	controller)
-		shift
-		controller $*
-		;;
-	crmctl)
-		shift
-		crmctl $*
-		;;
-	crmserver)
-		shift
-		crmserver $*
-		;;
-	dme-server)
-		shift
-		dme-server $*
-		;;
-	edgectl)
-		shift
-		edgectl $*
-		;;
-	loc-api-sim)
-		shift
-		loc-api-sim $*
-		;;
-	tok-srv-sim)
-		shift
-		tok-srv-sim $*
-		;;
-	test-edgectl)
-		shift
-		test-edgectl.sh $*
-		;;
-	*)
-		echo invalid program $1
-		exit 1
-		;;
+    controller)
+	shift
+	controller $*
+	;;
+    crmctl)
+	shift
+	crmctl $*
+	;;
+    crmserver)
+	shift
+	crmserver $*
+	;;
+    dme-server)
+	shift
+	dme-server $*
+	;;
+    edgectl)
+	shift
+	edgectl $*
+	;;
+    loc-api-sim)
+	shift
+	loc-api-sim $*
+	;;
+    tok-srv-sim)
+	shift
+	tok-srv-sim $*
+	;;
+    test-edgectl)
+	shift
+	test-edgectl.sh $*
+	;;
+    bash)
+	shift
+	/bin/bash $*
+	;;
+    *)
+	echo invalid program $1
+	exit 1
+	;;
 esac
 
