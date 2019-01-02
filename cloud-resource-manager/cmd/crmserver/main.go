88ab00d8926a9496192f3fea98c0ba64c165ef46
bobmex <38759636+bobmex@users.noreply.github.com>
2019-01-02 14:54:12 -0800

    fix bug EDGECLOUD-340 (#275)

--- cloud-resource-manager/cmd/crmserver/main.go
+++ cloud-resource-manager/cmd/crmserver/main.go
@@ -194,7 +194,7 @@ func initPlatform(cloudlet *edgeproto.CloudletInfo) error {
 	loc := util.DNSSanitize(cloudlet.Key.Name) //XXX  key.name => loc
 	oper := util.DNSSanitize(cloudlet.Key.OperatorKey.Name)
 	mf := &mexos.Manifest{Base: *base}
-	uri := fmt.Sprintf("%s/kustomize/infrastructure/output/%s.%s.yaml", *base, loc, oper)
+	uri := fmt.Sprintf("kustomize/infrastructure/output/%s.%s.yaml", loc, oper)
 	log.DebugLog(log.DebugLevelMexos, "init platform, creating new rootLB", "base", *base, "location(cloudlet.key.name)", loc, "operator", oper, "uri", uri)
 	if err := mexos.GetVaultEnv(mf, uri); err != nil {
 		return err
