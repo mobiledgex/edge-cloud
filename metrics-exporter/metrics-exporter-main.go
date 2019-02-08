c56a6460ae75dce3e4d4fafc909a4bd23eca57e4
Lev Shvarts <lev.shvarts@gmail.com>
2019-02-08 10:18:00 -0800

    EDGECLOUD-72 Metrics exporter changes (#300)
    The changes here include metrics-exporter service along with basic unit tests for it.
    
    * Metrics-exporter service queries either specified, or discovered prometheus service in the cluster and forwards the specified set of metrics to influxDB
    * metrics-exporter service is instantiated as an app on any clusterInst that is created by cluster-svc along with the prometheus service
    * influxDB, influxdb username, and password are passed either as parameters to the metrics-exporter process, or as environment variables.
    * exported metrics include the following:
      
      * per pod cpu utilization
      * per pod mem usage
      * per pod network rx/tx rate
      * cluster cpu utilization
      * cluster memory utilization
      * cluster disk utilization
      * cluster network tx/tx rate

---
+++ metrics-exporter/metrics-exporter-main.go
@@ -0,0 +1,112 @@
+package main
+
+import (
+	"errors"
+	"flag"
+	"fmt"
+	"os"
+	"os/signal"
+	"regexp"
+	"time"
+
+	influxq "github.com/mobiledgex/edge-cloud/controller/influxq_client"
+	"github.com/mobiledgex/edge-cloud/edgeproto"
+	"github.com/mobiledgex/edge-cloud/log"
+)
+
+var promAddress = flag.String("apiAddr", "0.0.0.0:9090", "Prometheus address to bind to")
+var influxdb = flag.String("influxdb", "0.0.0.0:8086", "InfluxDB address to export to")
+var debugLevels = flag.String("d", "", fmt.Sprintf("comma separated list of %v", log.DebugLevelStrings))
+var clusterName = flag.String("cluster", "myclust", "Cluster Name")
+
+var promQCpuClust = "sum(rate(container_cpu_usage_seconds_total%7Bid%3D%22%2F%22%7D%5B1m%5D))%2Fsum(machine_cpu_cores)*100"
+var promQMemClust = "sum(container_memory_working_set_bytes%7Bid%3D%22%2F%22%7D)%2Fsum(machine_memory_bytes)*100"
+var promQDiskClust = "sum(container_fs_usage_bytes%7Bdevice%3D~%22%5E%2Fdev%2F%5Bsv%5Dd%5Ba-z%5D%5B1-9%5D%24%22%2Cid%3D%22%2F%22%7D)%2Fsum(container_fs_limit_bytes%7Bdevice%3D~%22%5E%2Fdev%2F%5Bsv%5Dd%5Ba-z%5D%5B1-9%5D%24%22%2Cid%3D%22%2F%22%7D)*100"
+var promQSendBytesRateClust = "sum(irate(container_network_transmit_bytes_total%5B1m%5D))"
+var promQRecvBytesRateClust = "sum(irate(container_network_receive_bytes_total%5B1m%5D))"
+
+var promQCpuPod = "sum(rate(container_cpu_usage_seconds_total%7Bimage!%3D%22%22%7D%5B1m%5D))by(pod_name)"
+var promQMemPod = "sum(container_memory_working_set_bytes%7Bimage!%3D%22%22%7D)by(pod_name)"
+var promQNetRecvRate = "sum(irate(container_network_receive_bytes_total%7Bimage!%3D%22%22%7D%5B1m%5D))by(pod_name)"
+var promQNetSendRate = "sum(irate(container_network_transmit_bytes_total%7Bimage!%3D%22%22%7D%5B1m%5D))by(pod_name)"
+
+var Env = map[string]string{
+	"INFLUXDB_USER": "root",
+	"INFLUXDB_PASS": "root",
+}
+
+var InfluxDBName = "clusterstats"
+var influxQ *influxq.InfluxQ
+
+var sigChan chan os.Signal
+
+func getIPfromEnv() (string, error) {
+	re := regexp.MustCompile(".*PROMETHEUS_PORT_9090_TCP_ADDR=(.*)")
+	for _, e := range os.Environ() {
+		match := re.FindStringSubmatch(e)
+		if len(match) > 1 {
+			return match[1], nil
+		}
+	}
+	return "", errors.New("No Prometheus is running")
+}
+
+func initEnv() {
+	val := os.Getenv("MEX_CLUSTER_NAME")
+	if val != "" {
+		*clusterName = val
+	}
+	val = os.Getenv("MEX_INFLUXDB_ADDR")
+	if val != "" {
+		*influxdb = val
+	}
+	val = os.Getenv("MEX_INFLUXDB_USER")
+	if val != "" {
+		Env["INFLUXDB_USER"] = val
+	}
+	val = os.Getenv("MEX_INFLUXDB_PASS")
+	if val != "" {
+		Env["INFLUXDB_PASS"] = val
+	}
+}
+
+func main() {
+	flag.Parse()
+	log.SetDebugLevelStrs(*debugLevels)
+	fmt.Printf("Starting metrics exporter with Prometheus addr %s\n", *promAddress)
+	clustIP, err := getIPfromEnv()
+	if err == nil {
+		*promAddress = clustIP + ":9090"
+	}
+	fmt.Printf("Found Prometheus running on: %s\n", *promAddress)
+	initEnv()
+	fmt.Printf("InfluxDB is at: %s\n", *influxdb)
+	influxQ = influxq.NewInfluxQ(InfluxDBName)
+	err = influxQ.Start(*influxdb)
+	if err != nil {
+		log.FatalLog("Failed to start influx queue",
+			"address", *influxdb, "err", err)
+	}
+	defer influxQ.Stop()
+
+	stats := NewPromStats(*promAddress, time.Second*15, sendMetric)
+	stats.Start()
+	defer stats.Stop()
+
+	sigChan = make(chan os.Signal, 1)
+	signal.Notify(sigChan, os.Interrupt)
+
+	DebugPrint("Ready\n")
+
+	// wait until process in killed/interrupted
+	sig := <-sigChan
+	fmt.Println(sig)
+}
+
+func DebugPrint(format string, a ...interface{}) {
+	fmt.Printf(format, a...)
+}
+
+func sendMetric(metric *edgeproto.Metric) {
+	influxQ.AddMetric(metric)
+}
---
+++ metrics-exporter/prom-stats.go
@@ -0,0 +1,282 @@
+package main
+
+import (
+	"encoding/json"
+	"net/http"
+	"strconv"
+	"sync"
+	"time"
+
+	"github.com/gogo/protobuf/types"
+	"github.com/mobiledgex/edge-cloud/edgeproto"
+)
+
+type MetricAppInstKey struct {
+	cluster   string
+	pod       string
+	developer string
+}
+
+type PodPromStat struct {
+	cpu     float64
+	mem     uint64
+	disk    float64
+	netSend uint64
+	netRecv uint64
+}
+
+type ClustPromStat struct {
+	cpu     float64
+	mem     float64
+	disk    float64
+	netSend uint64
+	netRecv uint64
+}
+
+type PromStats struct {
+	promAddr    string
+	interval    time.Duration
+	appStatsMap map[MetricAppInstKey]*PodPromStat
+	clusterStat *ClustPromStat
+	send        func(metric *edgeproto.Metric)
+	waitGrp     sync.WaitGroup
+	stop        chan struct{}
+}
+
+type PromResp struct {
+	Status string   `json:"status,omitempty"`
+	Data   PromData `json:"data,omitempty"`
+}
+type PromData struct {
+	ResType string       `json:"resultType,omitempty"`
+	Result  []PromMetric `json:"result,omitempty"`
+}
+type PromMetric struct {
+	Labels PromLables    `json:"metric,omitempty"`
+	Values []interface{} `json:"value,omitempty"`
+}
+type PromLables struct {
+	PodName string `json:"pod_name,omitempty"`
+}
+
+func NewPromStats(promAddr string, interval time.Duration, send func(metric *edgeproto.Metric)) *PromStats {
+	p := PromStats{}
+	p.promAddr = promAddr
+	p.appStatsMap = make(map[MetricAppInstKey]*PodPromStat)
+	p.clusterStat = &ClustPromStat{}
+	p.interval = interval
+	p.send = send
+	return &p
+}
+
+func getPromMetrics(addr string, query string) (*PromResp, error) {
+	reqURI := "http://" + addr + "/api/v1/query?query=" + query
+	resp, err := http.Get(reqURI)
+	if err != nil {
+		DebugPrint("Failed to run <%s>\n",
+			"http://"+addr+"/api/v1/query?query="+query)
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	promResp := &PromResp{}
+	if err = json.NewDecoder(resp.Body).Decode(promResp); err != nil {
+		return nil, err
+	}
+	return promResp, nil
+}
+
+func (p *PromStats) CollectPromStats() error {
+	appKey := MetricAppInstKey{
+		cluster:   *clusterName,
+		developer: "",
+	}
+	// Get Pod CPU usage percentage
+	resp, err := getPromMetrics(p.promAddr, promQCpuPod)
+	if err == nil && resp.Status == "success" {
+		for _, metric := range resp.Data.Result {
+			appKey.pod = metric.Labels.PodName
+			stat, found := p.appStatsMap[appKey]
+			if !found {
+				stat = &PodPromStat{}
+				p.appStatsMap[appKey] = stat
+			}
+			//copy only if we can parse the value
+			if val, err := strconv.ParseFloat(metric.Values[1].(string), 64); err == nil {
+				stat.cpu = val
+			}
+		}
+	}
+	// Get Pod Mem usage
+	resp, err = getPromMetrics(p.promAddr, promQMemPod)
+	if err == nil && resp.Status == "success" {
+		for _, metric := range resp.Data.Result {
+			appKey.pod = metric.Labels.PodName
+			stat, found := p.appStatsMap[appKey]
+			if !found {
+				stat = &PodPromStat{}
+				p.appStatsMap[appKey] = stat
+			}
+			//copy only if we can parse the value
+			if val, err := strconv.ParseUint(metric.Values[1].(string), 10, 64); err == nil {
+				stat.mem = val
+			}
+		}
+	}
+	// Get Pod NetRecv bytes rate averaged over 1m
+	resp, err = getPromMetrics(p.promAddr, promQNetRecvRate)
+	if err == nil && resp.Status == "success" {
+		for _, metric := range resp.Data.Result {
+			appKey.pod = metric.Labels.PodName
+			stat, found := p.appStatsMap[appKey]
+			if !found {
+				stat = &PodPromStat{}
+				p.appStatsMap[appKey] = stat
+			}
+			//copy only if we can parse the value
+			if val, err := strconv.ParseFloat(metric.Values[1].(string), 64); err == nil {
+				stat.netRecv = uint64(val)
+			}
+		}
+	}
+	// Get Pod NetRecv bytes rate averaged over 1m
+	resp, err = getPromMetrics(p.promAddr, promQNetSendRate)
+	if err == nil && resp.Status == "success" {
+		for _, metric := range resp.Data.Result {
+			appKey.pod = metric.Labels.PodName
+			stat, found := p.appStatsMap[appKey]
+			if !found {
+				stat = &PodPromStat{}
+				p.appStatsMap[appKey] = stat
+			}
+			//copy only if we can parse the value
+			if val, err := strconv.ParseFloat(metric.Values[1].(string), 64); err == nil {
+				stat.netSend = uint64(val)
+			}
+		}
+	}
+
+	// Get Cluster CPU usage
+	resp, err = getPromMetrics(p.promAddr, promQCpuClust)
+	if err == nil && resp.Status == "success" {
+		for _, metric := range resp.Data.Result {
+			//copy only if we can parse the value
+			if val, err := strconv.ParseFloat(metric.Values[1].(string), 64); err == nil {
+				p.clusterStat.cpu = val
+				// We should have only one value here
+				break
+			}
+		}
+	}
+	// Get Cluster Mem usage
+	resp, err = getPromMetrics(p.promAddr, promQMemClust)
+	if err == nil && resp.Status == "success" {
+		for _, metric := range resp.Data.Result {
+			//copy only if we can parse the value
+			if val, err := strconv.ParseFloat(metric.Values[1].(string), 64); err == nil {
+				p.clusterStat.mem = val
+				// We should have only one value here
+				break
+			}
+		}
+	}
+	// Get Cluster Disk usage percentage
+	resp, err = getPromMetrics(p.promAddr, promQDiskClust)
+	if err == nil && resp.Status == "success" {
+		for _, metric := range resp.Data.Result {
+			//copy only if we can parse the value
+			if val, err := strconv.ParseFloat(metric.Values[1].(string), 64); err == nil {
+				p.clusterStat.disk = val
+				// We should have only one value here
+				break
+			}
+		}
+	}
+	// Get Cluster NetRecv bytes rate averaged over 1m
+	resp, err = getPromMetrics(p.promAddr, promQRecvBytesRateClust)
+	if err == nil && resp.Status == "success" {
+		for _, metric := range resp.Data.Result {
+			//copy only if we can parse the value
+			if val, err := strconv.ParseFloat(metric.Values[1].(string), 64); err == nil {
+				p.clusterStat.netRecv = uint64(val)
+				// We should have only one value here
+				break
+			}
+		}
+	}
+	// Get Cluster NetSend bytes rate averaged over 1m
+	resp, err = getPromMetrics(p.promAddr, promQSendBytesRateClust)
+	if err == nil && resp.Status == "success" {
+		for _, metric := range resp.Data.Result {
+			//copy only if we can parse the value
+			if val, err := strconv.ParseFloat(metric.Values[1].(string), 64); err == nil {
+				p.clusterStat.netSend = uint64(val)
+				// We should have only one value here
+				break
+			}
+		}
+	}
+	return nil
+}
+
+func (p *PromStats) Start() {
+	p.stop = make(chan struct{})
+	p.waitGrp.Add(1)
+	go p.RunNotify()
+}
+
+func (p *PromStats) Stop() {
+	DebugPrint("Stopping PromStats thread\n")
+	close(p.stop)
+	p.waitGrp.Wait()
+}
+
+func (p *PromStats) RunNotify() {
+	DebugPrint("Started PromStats thread\n")
+	done := false
+	for !done {
+		select {
+		case <-time.After(p.interval):
+			ts, _ := types.TimestampProto(time.Now())
+			if p.CollectPromStats() != nil {
+				continue
+			}
+			DebugPrint("Sending metrics for %s with timestamp %s\n", *clusterName, ts.String())
+			for key, stat := range p.appStatsMap {
+				p.send(PodStatToMetric(ts, &key, stat))
+			}
+			p.send(ClusterStatToMetric(ts, p.clusterStat))
+		case <-p.stop:
+			done = true
+		}
+	}
+	p.waitGrp.Done()
+}
+
+func ClusterStatToMetric(ts *types.Timestamp, stat *ClustPromStat) *edgeproto.Metric {
+	metric := edgeproto.Metric{}
+	metric.Timestamp = *ts
+	metric.Name = "crm-cluster"
+	metric.AddTag("cluster", *clusterName)
+	metric.AddDoubleVal("cpu", stat.cpu)
+	metric.AddDoubleVal("mem", stat.mem)
+	metric.AddDoubleVal("disk", stat.disk)
+	metric.AddIntVal("sendBytes", stat.netSend)
+	metric.AddIntVal("recvBytes", stat.netRecv)
+	return &metric
+}
+
+func PodStatToMetric(ts *types.Timestamp, key *MetricAppInstKey, stat *PodPromStat) *edgeproto.Metric {
+	metric := edgeproto.Metric{}
+	metric.Timestamp = *ts
+	metric.Name = "crm-appinst"
+	metric.AddTag("dev", key.developer)
+	metric.AddTag("cluster", key.cluster)
+	metric.AddTag("app", key.pod)
+	metric.AddDoubleVal("cpu", stat.cpu)
+	metric.AddIntVal("mem", stat.mem)
+	metric.AddDoubleVal("disk", stat.disk)
+	metric.AddIntVal("sendBytes", stat.netSend)
+	metric.AddIntVal("recvBytes", stat.netRecv)
+	return &metric
+}
