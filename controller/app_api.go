cec65ba998f1d954146d98869ab030e697359505
Jon <38672162+gainsley@users.noreply.github.com>
2019-02-11 10:26:32 -0800

    EDGECLOUD-392 fix race condition, decouple app from cluster (#302)

--- controller/app_api.go
+++ controller/app_api.go
@@ -59,30 +59,19 @@ func (s *AppApi) UsesFlavor(key *edgeproto.FlavorKey) bool {
 	s.cache.Mux.Lock()
 	defer s.cache.Mux.Unlock()
 	for _, app := range s.cache.Objs {
-		if app.DefaultFlavor.Matches(key) {
+		if app.DefaultFlavor.Matches(key) && app.DelOpt != edgeproto.DeleteType_AutoDelete {
 			return true
 		}
 	}
 	return false
 }
 
-func (s *AppApi) UsesCluster(key *edgeproto.ClusterKey) bool {
-	s.cache.Mux.Lock()
-	defer s.cache.Mux.Unlock()
-	for _, app := range s.cache.Objs {
-		if app.Cluster.Matches(key) && app.DelOpt == edgeproto.DeleteType_NoAutoDelete {
-			return true
-		}
-	}
-	return false
-}
-
-func (s *AppApi) AutoDeleteApps(ctx context.Context, key *edgeproto.ClusterKey) error {
+func (s *AppApi) AutoDeleteApps(ctx context.Context, key *edgeproto.FlavorKey) {
 	apps := make(map[edgeproto.AppKey]*edgeproto.App)
-	log.DebugLog(log.DebugLevelApi, "Auto-deleting appinsts ", "cluster", key.Name)
+	log.DebugLog(log.DebugLevelApi, "Auto-deleting apps ", "flavor", key)
 	s.cache.Mux.Lock()
 	for k, app := range s.cache.Objs {
-		if app.Cluster.Matches(key) && app.DelOpt == edgeproto.DeleteType_AutoDelete {
+		if app.DefaultFlavor.Matches(key) && app.DelOpt == edgeproto.DeleteType_AutoDelete {
 			apps[k] = app
 		}
 	}
@@ -90,10 +79,9 @@ func (s *AppApi) AutoDeleteApps(ctx context.Context, key *edgeproto.ClusterKey) 
 	for _, val := range apps {
 		log.DebugLog(log.DebugLevelApi, "Auto-deleting app ", "app", val.Key.Name)
 		if _, err := s.DeleteApp(ctx, val); err != nil {
-			return err
+			log.DebugLog(log.DebugLevelApi, "unable to auto-delete app", "app", val, "err", err)
 		}
 	}
-	return nil
 }
 
 // AndroidPackageConflicts returns true if an app with a different developer+name
@@ -190,49 +178,6 @@ func (s *AppApi) CreateApp(ctx context.Context, in *edgeproto.App) (*edgeproto.R
 		return &edgeproto.Result{}, fmt.Errorf("AndroidPackageName: %s in use by another app", in.AndroidPackageName)
 	}
 
-	// make sure cluster exists
-	// This is a separate STM to avoid ordering issues between
-	// auto-cluster create and app create in watch cb.
-	if in.Cluster.Name == "" {
-		if in.DefaultFlavor.Name == "" {
-			return &edgeproto.Result{}, errors.New("DefaultFlavor is required if Cluster is not specified")
-		}
-		err = s.sync.ApplySTMWait(func(stm concurrency.STM) error {
-			if s.store.STMGet(stm, &in.Key, nil) {
-				return objstore.ErrKVStoreKeyExists
-			}
-			// cluster not specified, create one automatically
-			in.Cluster.Name = fmt.Sprintf("%s%s", ClusterAutoPrefix,
-				in.Key.Name)
-			in.Cluster.Name = util.K8SSanitize(in.Cluster.Name)
-			cluster := edgeproto.Cluster{}
-			cluster.Key = in.Cluster
-			clusterFlavorKey, lookup := GetClusterFlavorForFlavor(&in.DefaultFlavor)
-			if lookup != nil {
-				return lookup
-			}
-			cluster.DefaultFlavor = *clusterFlavorKey
-			cluster.Auto = true
-			// it may be possible that cluster already exists
-			if !clusterApi.store.STMGet(stm, &cluster.Key, nil) {
-				log.DebugLog(log.DebugLevelApi,
-					"Create auto-cluster",
-					"key", cluster.Key,
-					"app", in)
-				clusterApi.store.STMPut(stm, &cluster)
-			}
-			return nil
-		})
-		if err != nil {
-			return &edgeproto.Result{}, err
-		}
-		defer func() {
-			if err != nil {
-				s.deleteClusterAuto(ctx, &in.Cluster)
-			}
-		}()
-	}
-
 	err = s.sync.ApplySTMWait(func(stm concurrency.STM) error {
 		if !developerApi.store.STMGet(stm, &in.Key.DeveloperKey, nil) {
 			return errors.New("Specified developer not found")
@@ -242,9 +187,6 @@ func (s *AppApi) CreateApp(ctx context.Context, in *edgeproto.App) (*edgeproto.R
 		}
 		if s.store.STMGet(stm, &in.Key, nil) {
 			return objstore.ErrKVStoreKeyExists
-		}
-		if !clusterApi.store.STMGet(stm, &in.Cluster, nil) {
-			return errors.New("Specified Cluster not found")
 		}
 		s.store.STMPut(stm, in)
 		return nil
@@ -273,22 +215,16 @@ func (s *AppApi) DeleteApp(ctx context.Context, in *edgeproto.App) (*edgeproto.R
 		// disallow delete if static instances are present
 		return &edgeproto.Result{}, errors.New("Application in use by static Application Instance")
 	}
-	clusterKey := edgeproto.ClusterKey{}
 	err := s.sync.ApplySTMWait(func(stm concurrency.STM) error {
 		app := edgeproto.App{}
 		if !s.store.STMGet(stm, &in.Key, &app) {
 			// already deleted
 			return nil
 		}
-		clusterKey = app.Cluster
 		// delete app
 		s.store.STMDel(stm, &in.Key)
 		return nil
 	})
-	if err == nil {
-		// delete cluster afterwards if it was auto-created
-		s.deleteClusterAuto(ctx, &clusterKey)
-	}
 	if err == nil && len(dynInsts) > 0 {
 		// delete dynamic instances
 		for key, _ := range dynInsts {
@@ -302,23 +238,6 @@ func (s *AppApi) DeleteApp(ctx context.Context, in *edgeproto.App) (*edgeproto.R
 		}
 	}
 	return &edgeproto.Result{}, err
-}
-
-func (s *AppApi) deleteClusterAuto(ctx context.Context, key *edgeproto.ClusterKey) {
-	err := s.sync.ApplySTMWait(func(stm concurrency.STM) error {
-		cluster := edgeproto.Cluster{}
-		if clusterApi.store.STMGet(stm, key, &cluster) && cluster.Auto {
-			if err := s.AutoDeleteApps(ctx, key); err != nil {
-				return err
-			}
-			clusterApi.store.STMDel(stm, key)
-		}
-		return nil
-	})
-	if err != nil {
-		log.InfoLog("Failed to delete auto cluster",
-			"clusterInst", key, "err", err)
-	}
 }
 
 func (s *AppApi) ShowApp(in *edgeproto.App, cb edgeproto.AppApi_ShowAppServer) error {
