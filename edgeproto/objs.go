8e8f4f12f03410d5988125b8cbbe740a6c296062
Ashish Jain <ashxjain@gmail.com>
2019-03-25 20:17:04 +0530

    Revert changes, as http will be handled separately

--- edgeproto/objs.go
+++ edgeproto/objs.go
@@ -373,7 +373,7 @@ func LProtoStr(proto dme.LProto) (string, error) {
 	case dme.LProto_LProtoUDP:
 		return "udp", nil
 	case dme.LProto_LProtoHTTP:
-		return "tcp", nil
+		return "http", nil
 	}
 	return "", fmt.Errorf("Invalid proto %d", proto)
 }
