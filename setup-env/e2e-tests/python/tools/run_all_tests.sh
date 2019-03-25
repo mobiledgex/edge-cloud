4dac42d4911b276072676511f68c75046aa2c2f0
Andy Anderson <andy.anderson@mobiledgex.com>
2019-03-25 16:23:24 -0500

    delete python testcase directory since it is moved to edge-cloud-qa

--- setup-env/e2e-tests/python/tools/run_all_tests.sh
+++
@@ -1,4 +0,0 @@
-export PYTHONPATH=./setup-env/e2e-tests/python/protos:./setup-env/e2e-tests/python/modules:./setup-env/e2e-tests/python/certs
-e2e-tests -setupfile ./setup-env/e2e-tests/setups/local_multi_automation.yml -testfile ./setup-env/e2e-tests/testfiles/stop_cleanup.yml
-e2e-tests -setupfile ./setup-env/e2e-tests/setups/local_multi_automation.yml -testfile ./setup-env/e2e-tests/testfiles/deploy_start_create_automation.yml
-python3 -m unittest discover -s ./setup-env/e2e-tests/python/testcases/$dir
