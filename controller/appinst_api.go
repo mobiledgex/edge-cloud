32e8fcf3075463c25e1b32d2258a775f5070f867
Ashish Jain <ashxjain@gmail.com>
2019-03-28 11:03:14 +0530

    EDGECLOUD 324 - Creating an app with tcp and udp ports sets the fqdnprefix to tcp for both ports (#367)
    * EDGECLOUD 324 - Creating an app with tcp and udp ports sets the fqdnprefix to tcp for both ports
    * Add test case to check port FQDN prefix

--- controller/appinst_api.go
+++ controller/appinst_api.go
@@ -717,27 +717,30 @@ func setPortFQDNPrefixes(in *edgeproto.AppInst, app *edgeproto.App) error {
 			return fmt.Errorf("invalid kubernetes deployment yaml, %s", err.Error())
 		}
 		for ii, _ := range in.MappedPorts {
-			err = setPortFQDNPrefix(&in.MappedPorts[ii], objs)
-			if err != nil {
-				return err
-			}
+			setPortFQDNPrefix(&in.MappedPorts[ii], objs)
 		}
 	}
 	return nil
 }
 
-func setPortFQDNPrefix(port *dme.AppPort, objs []runtime.Object) error {
+func setPortFQDNPrefix(port *dme.AppPort, objs []runtime.Object) {
 	for _, obj := range objs {
 		ksvc, ok := obj.(*v1.Service)
 		if !ok {
 			continue
 		}
 		for _, kp := range ksvc.Spec.Ports {
+			lproto, err := edgeproto.LProtoStr(port.Proto)
+			if err != nil {
+				return
+			}
+			if lproto != strings.ToLower(string(kp.Protocol)) {
+				continue
+			}
 			if kp.TargetPort.IntValue() == int(port.InternalPort) {
 				port.FQDNPrefix = cloudcommon.FQDNPrefix(ksvc.Name)
-				return nil
+				return
 			}
 		}
 	}
-	return fmt.Errorf("no service for app port %d found in manifest", port.InternalPort)
 }
