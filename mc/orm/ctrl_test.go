4f205bceeb016b50c5d759fd70c783bc9ec4141e
Jon Gainsley <jon.gainsley@mobiledgex.com>
2019-04-01 16:27:00 -0700

    EDGECLOUD-520 TLS for MC

--- mc/orm/ctrl_test.go
+++ mc/orm/ctrl_test.go
@@ -59,12 +59,14 @@ func TestController(t *testing.T) {
 	err = server.WaitUntilReady()
 	require.Nil(t, err, "server online")
 
+	mcClient := &ormclient.Client{}
+
 	// login as super user
-	token, err := ormclient.DoLogin(uri, DefaultSuperuser, DefaultSuperpass)
+	token, err := mcClient.DoLogin(uri, DefaultSuperuser, DefaultSuperpass)
 	require.Nil(t, err, "login as superuser")
 
 	// test controller api
-	ctrls, status, err := ormclient.ShowController(uri, token)
+	ctrls, status, err := mcClient.ShowController(uri, token)
 	require.Nil(t, err, "show controllers")
 	require.Equal(t, http.StatusOK, status)
 	require.Equal(t, 0, len(ctrls))
@@ -73,10 +75,10 @@ func TestController(t *testing.T) {
 		Address: ctrlAddr,
 	}
 	// create controller
-	status, err = ormclient.CreateController(uri, token, &ctrl)
+	status, err = mcClient.CreateController(uri, token, &ctrl)
 	require.Nil(t, err, "create controller")
 	require.Equal(t, http.StatusOK, status)
-	ctrls, status, err = ormclient.ShowController(uri, token)
+	ctrls, status, err = mcClient.ShowController(uri, token)
 	require.Nil(t, err, "show controllers")
 	require.Equal(t, http.StatusOK, status)
 	require.Equal(t, 1, len(ctrls))
@@ -84,50 +86,50 @@ func TestController(t *testing.T) {
 	require.Equal(t, ctrl.Address, ctrls[0].Address)
 
 	// create a developers
-	_, orgDev, tokenDev := testCreateUserOrg(t, uri, "dev", "developer",
+	_, orgDev, tokenDev := testCreateUserOrg(t, mcClient, uri, "dev", "developer",
 		testutil.DevData[0].Key.Name)
-	_, _, tokenDev2 := testCreateUserOrg(t, uri, "dev2", "developer",
+	_, _, tokenDev2 := testCreateUserOrg(t, mcClient, uri, "dev2", "developer",
 		testutil.DevData[3].Key.Name)
-	dev3, tokenDev3 := testCreateUser(t, uri, "dev3")
-	dev4, tokenDev4 := testCreateUser(t, uri, "dev4")
+	dev3, tokenDev3 := testCreateUser(t, mcClient, uri, "dev3")
+	dev4, tokenDev4 := testCreateUser(t, mcClient, uri, "dev4")
 	// create an operator
-	_, orgOper, tokenOper := testCreateUserOrg(t, uri, "oper", "operator",
+	_, orgOper, tokenOper := testCreateUserOrg(t, mcClient, uri, "oper", "operator",
 		testutil.OperatorData[0].Key.Name)
-	_, _, tokenOper2 := testCreateUserOrg(t, uri, "oper2", "operator",
+	_, _, tokenOper2 := testCreateUserOrg(t, mcClient, uri, "oper2", "operator",
 		testutil.OperatorData[1].Key.Name)
-	oper3, tokenOper3 := testCreateUser(t, uri, "oper3")
-	oper4, tokenOper4 := testCreateUser(t, uri, "oper4")
+	oper3, tokenOper3 := testCreateUser(t, mcClient, uri, "oper3")
+	oper4, tokenOper4 := testCreateUser(t, mcClient, uri, "oper4")
 
 	// additional users don't have access to orgs yet
-	badPermTestApp(t, uri, tokenDev3, ctrl.Region, &testutil.AppData[0])
-	badPermTestAppInst(t, uri, tokenDev3, ctrl.Region, &testutil.AppInstData[0])
-	badPermTestCloudlet(t, uri, tokenOper3, ctrl.Region, &testutil.CloudletData[0])
+	badPermTestApp(t, mcClient, uri, tokenDev3, ctrl.Region, &testutil.AppData[0])
+	badPermTestAppInst(t, mcClient, uri, tokenDev3, ctrl.Region, &testutil.AppInstData[0])
+	badPermTestCloudlet(t, mcClient, uri, tokenOper3, ctrl.Region, &testutil.CloudletData[0])
 
 	// add new users to orgs
-	testAddUserRole(t, uri, tokenDev, orgDev.Name, "DeveloperContributor", dev3.Name, Success)
-	testAddUserRole(t, uri, tokenDev, orgDev.Name, "DeveloperViewer", dev4.Name, Success)
-	testAddUserRole(t, uri, tokenOper, orgOper.Name, "OperatorContributor", oper3.Name, Success)
-	testAddUserRole(t, uri, tokenOper, orgOper.Name, "OperatorViewer", oper4.Name, Success)
+	testAddUserRole(t, mcClient, uri, tokenDev, orgDev.Name, "DeveloperContributor", dev3.Name, Success)
+	testAddUserRole(t, mcClient, uri, tokenDev, orgDev.Name, "DeveloperViewer", dev4.Name, Success)
+	testAddUserRole(t, mcClient, uri, tokenOper, orgOper.Name, "OperatorContributor", oper3.Name, Success)
+	testAddUserRole(t, mcClient, uri, tokenOper, orgOper.Name, "OperatorViewer", oper4.Name, Success)
 	// make sure dev/ops without user perms can't add new users
-	user5, _ := testCreateUser(t, uri, "user5")
-	testAddUserRole(t, uri, tokenDev3, orgDev.Name, "DeveloperViewer", user5.Name, Fail)
-	testAddUserRole(t, uri, tokenDev4, orgDev.Name, "DeveloperViewer", user5.Name, Fail)
-	testAddUserRole(t, uri, tokenOper3, orgOper.Name, "OperatorViewer", user5.Name, Fail)
-	testAddUserRole(t, uri, tokenOper4, orgOper.Name, "OperatorViewer", user5.Name, Fail)
+	user5, _ := testCreateUser(t, mcClient, uri, "user5")
+	testAddUserRole(t, mcClient, uri, tokenDev3, orgDev.Name, "DeveloperViewer", user5.Name, Fail)
+	testAddUserRole(t, mcClient, uri, tokenDev4, orgDev.Name, "DeveloperViewer", user5.Name, Fail)
+	testAddUserRole(t, mcClient, uri, tokenOper3, orgOper.Name, "OperatorViewer", user5.Name, Fail)
+	testAddUserRole(t, mcClient, uri, tokenOper4, orgOper.Name, "OperatorViewer", user5.Name, Fail)
 
 	// make sure developer and operator cannot see or modify controllers
 	ctrlNew := ormapi.Controller{
 		Region:  "Bad",
 		Address: "bad.mobiledgex.net",
 	}
-	status, err = ormclient.CreateController(uri, tokenDev, &ctrlNew)
+	status, err = mcClient.CreateController(uri, tokenDev, &ctrlNew)
 	require.Equal(t, http.StatusForbidden, status)
-	status, err = ormclient.CreateController(uri, tokenOper, &ctrlNew)
+	status, err = mcClient.CreateController(uri, tokenOper, &ctrlNew)
 	require.Equal(t, http.StatusForbidden, status)
-	ctrls, status, err = ormclient.ShowController(uri, tokenDev)
+	ctrls, status, err = mcClient.ShowController(uri, tokenDev)
 	require.Equal(t, http.StatusForbidden, status)
 	require.Equal(t, 0, len(ctrls))
-	ctrls, status, err = ormclient.ShowController(uri, tokenOper)
+	ctrls, status, err = mcClient.ShowController(uri, tokenOper)
 	require.Equal(t, http.StatusForbidden, status)
 	require.Equal(t, 0, len(ctrls))
 
@@ -135,65 +137,65 @@ func TestController(t *testing.T) {
 	setClusterInstDev(orgDev.Name, testutil.ClusterInstData)
 
 	// make sure operator cannot create apps, appinsts, clusters, etc
-	badPermTestApp(t, uri, tokenOper, ctrl.Region, &testutil.AppData[0])
-	badPermTestAppInst(t, uri, tokenOper, ctrl.Region, &testutil.AppInstData[0])
-	badPermTestClusterInst(t, uri, tokenOper, ctrl.Region, &testutil.ClusterInstData[0])
-	badPermTestApp(t, uri, tokenOper2, ctrl.Region, &testutil.AppData[0])
-	badPermTestAppInst(t, uri, tokenOper2, ctrl.Region, &testutil.AppInstData[0])
-	badPermTestClusterInst(t, uri, tokenOper2, ctrl.Region, &testutil.ClusterInstData[0])
+	badPermTestApp(t, mcClient, uri, tokenOper, ctrl.Region, &testutil.AppData[0])
+	badPermTestAppInst(t, mcClient, uri, tokenOper, ctrl.Region, &testutil.AppInstData[0])
+	badPermTestClusterInst(t, mcClient, uri, tokenOper, ctrl.Region, &testutil.ClusterInstData[0])
+	badPermTestApp(t, mcClient, uri, tokenOper2, ctrl.Region, &testutil.AppData[0])
+	badPermTestAppInst(t, mcClient, uri, tokenOper2, ctrl.Region, &testutil.AppInstData[0])
+	badPermTestClusterInst(t, mcClient, uri, tokenOper2, ctrl.Region, &testutil.ClusterInstData[0])
 	// make sure developer cannot create cloudlet
-	badPermTestCloudlet(t, uri, tokenDev, ctrl.Region, &testutil.CloudletData[0])
-	badPermTestCloudlet(t, uri, tokenDev2, ctrl.Region, &testutil.CloudletData[0])
+	badPermTestCloudlet(t, mcClient, uri, tokenDev, ctrl.Region, &testutil.CloudletData[0])
+	badPermTestCloudlet(t, mcClient, uri, tokenDev2, ctrl.Region, &testutil.CloudletData[0])
 
 	// test operators can modify their own objs but not each other's
-	permTestCloudlet(t, uri, tokenOper, tokenOper2, ctrl.Region,
+	permTestCloudlet(t, mcClient, uri, tokenOper, tokenOper2, ctrl.Region,
 		&testutil.CloudletData[0], &testutil.CloudletData[2])
 	// test developers can modify their own objs but not each other's
-	permTestApp(t, uri, tokenDev, tokenDev2, ctrl.Region,
+	permTestApp(t, mcClient, uri, tokenDev, tokenDev2, ctrl.Region,
 		&testutil.AppData[0], &testutil.AppData[5])
-	permTestAppInst(t, uri, tokenDev, tokenDev2, ctrl.Region,
+	permTestAppInst(t, mcClient, uri, tokenDev, tokenDev2, ctrl.Region,
 		&testutil.AppInstData[0], &testutil.AppInstData[5])
 	// test users with different roles
-	goodPermTestApp(t, uri, tokenDev3, ctrl.Region, &testutil.AppData[0])
-	goodPermTestAppInst(t, uri, tokenDev3, ctrl.Region, &testutil.AppInstData[0])
+	goodPermTestApp(t, mcClient, uri, tokenDev3, ctrl.Region, &testutil.AppData[0])
+	goodPermTestAppInst(t, mcClient, uri, tokenDev3, ctrl.Region, &testutil.AppInstData[0])
 	// test users with different roles
-	goodPermTestCloudlet(t, uri, tokenOper3, ctrl.Region, &testutil.CloudletData[0])
-	goodPermTestClusterInst(t, uri, tokenDev, ctrl.Region, &testutil.ClusterInstData[0])
-	badPermTestClusterInst(t, uri, tokenDev2, ctrl.Region, &testutil.ClusterInstData[0])
+	goodPermTestCloudlet(t, mcClient, uri, tokenOper3, ctrl.Region, &testutil.CloudletData[0])
+	goodPermTestClusterInst(t, mcClient, uri, tokenDev, ctrl.Region, &testutil.ClusterInstData[0])
+	badPermTestClusterInst(t, mcClient, uri, tokenDev2, ctrl.Region, &testutil.ClusterInstData[0])
 
 	// remove users from roles, test that they can't modify anything anymore
-	testRemoveUserRole(t, uri, tokenDev, orgDev.Name, "DeveloperContributor", dev3.Name, Success)
-	badPermTestApp(t, uri, tokenDev3, ctrl.Region, &testutil.AppData[0])
-	badPermTestAppInst(t, uri, tokenDev3, ctrl.Region, &testutil.AppInstData[0])
-	testRemoveUserRole(t, uri, tokenOper, orgOper.Name, "OperatorContributor", oper3.Name, Success)
-	badPermTestCloudlet(t, uri, tokenOper3, ctrl.Region, &testutil.CloudletData[0])
+	testRemoveUserRole(t, mcClient, uri, tokenDev, orgDev.Name, "DeveloperContributor", dev3.Name, Success)
+	badPermTestApp(t, mcClient, uri, tokenDev3, ctrl.Region, &testutil.AppData[0])
+	badPermTestAppInst(t, mcClient, uri, tokenDev3, ctrl.Region, &testutil.AppInstData[0])
+	testRemoveUserRole(t, mcClient, uri, tokenOper, orgOper.Name, "OperatorContributor", oper3.Name, Success)
+	badPermTestCloudlet(t, mcClient, uri, tokenOper3, ctrl.Region, &testutil.CloudletData[0])
 
 	// delete controller
-	status, err = ormclient.DeleteController(uri, token, &ctrl)
+	status, err = mcClient.DeleteController(uri, token, &ctrl)
 	require.Nil(t, err, "delete controller")
 	require.Equal(t, http.StatusOK, status)
-	ctrls, status, err = ormclient.ShowController(uri, token)
+	ctrls, status, err = mcClient.ShowController(uri, token)
 	require.Nil(t, err, "show controllers")
 	require.Equal(t, http.StatusOK, status)
 	require.Equal(t, 0, len(ctrls))
 }
 
-func testCreateUser(t *testing.T, uri, name string) (*ormapi.User, string) {
+func testCreateUser(t *testing.T, mcClient *ormclient.Client, uri, name string) (*ormapi.User, string) {
 	user := ormapi.User{
 		Name:     name,
 		Email:    name + "@gmail.com",
 		Passhash: name + "-password",
 	}
-	status, err := ormclient.CreateUser(uri, &user)
+	status, err := mcClient.CreateUser(uri, &user)
 	require.Nil(t, err, "create user ", name)
 	require.Equal(t, http.StatusOK, status)
 	// login
-	token, err := ormclient.DoLogin(uri, user.Name, user.Passhash)
+	token, err := mcClient.DoLogin(uri, user.Name, user.Passhash)
 	require.Nil(t, err, "login as ", name)
 	return &user, token
 }
 
-func testCreateOrg(t *testing.T, uri, token, orgType, orgName string) *ormapi.Organization {
+func testCreateOrg(t *testing.T, mcClient *ormclient.Client, uri, token, orgType, orgName string) *ormapi.Organization {
 	// create org
 	org := ormapi.Organization{
 		Type:    orgType,
@@ -201,25 +203,25 @@ func testCreateOrg(t *testing.T, uri, token, orgType, orgName string) *ormapi.Or
 		Address: orgName,
 		Phone:   "123-123-1234",
 	}
-	status, err := ormclient.CreateOrg(uri, token, &org)
+	status, err := mcClient.CreateOrg(uri, token, &org)
 	require.Nil(t, err, "create org ", orgName)
 	require.Equal(t, http.StatusOK, status)
 	return &org
 }
 
-func testCreateUserOrg(t *testing.T, uri, name, orgType, orgName string) (*ormapi.User, *ormapi.Organization, string) {
-	user, token := testCreateUser(t, uri, name)
-	org := testCreateOrg(t, uri, token, orgType, orgName)
+func testCreateUserOrg(t *testing.T, mcClient *ormclient.Client, uri, name, orgType, orgName string) (*ormapi.User, *ormapi.Organization, string) {
+	user, token := testCreateUser(t, mcClient, uri, name)
+	org := testCreateOrg(t, mcClient, uri, token, orgType, orgName)
 	return user, org, token
 }
 
-func testAddUserRole(t *testing.T, uri, token, org, role, username string, success bool) {
+func testAddUserRole(t *testing.T, mcClient *ormclient.Client, uri, token, org, role, username string, success bool) {
 	roleArg := ormapi.Role{
 		Username: username,
 		Org:      org,
 		Role:     role,
 	}
-	status, err := ormclient.AddUserRole(uri, token, &roleArg)
+	status, err := mcClient.AddUserRole(uri, token, &roleArg)
 	if success {
 		require.Nil(t, err, "add user role")
 		require.Equal(t, http.StatusOK, status)
@@ -228,13 +230,13 @@ func testAddUserRole(t *testing.T, uri, token, org, role, username string, succe
 	}
 }
 
-func testRemoveUserRole(t *testing.T, uri, token, org, role, username string, success bool) {
+func testRemoveUserRole(t *testing.T, mcClient *ormclient.Client, uri, token, org, role, username string, success bool) {
 	roleArg := ormapi.Role{
 		Username: username,
 		Org:      org,
 		Role:     role,
 	}
-	status, err := ormclient.RemoveUserRole(uri, token, &roleArg)
+	status, err := mcClient.RemoveUserRole(uri, token, &roleArg)
 	require.Nil(t, err, "remove user role")
 	require.Equal(t, http.StatusOK, status)
 	if success {
