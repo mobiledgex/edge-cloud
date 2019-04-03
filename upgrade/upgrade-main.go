a6372b476f19535e27f41673131a07b21ed49e47
Lev Shvarts <lev@mobiledgex.com>
2019-04-03 16:11:51 -0700

    File conversion

--- upgrade/upgrade-main.go
+++ upgrade/upgrade-main.go
@@ -4,6 +4,9 @@ import (
 	"flag"
 	"fmt"
 	"log"
+	"os"
+	"path/filepath"
+	"strings"
 
 	"github.com/mobiledgex/edge-cloud/edgeproto"
 	mexlog "github.com/mobiledgex/edge-cloud/log"
@@ -12,7 +15,7 @@ import (
 )
 
 var yamlFile = flag.String("yaml", "", "File to be upgraded")
-var outputFile = flag.String("output", "upgrade.yml", "Output yaml file if the input is a yaml file")
+var outputFile = flag.String("output", "upgrade.yml", "Output yaml file(if theinput is a yaml file)")
 var etcdUrls = flag.String("etcdUrls", "http://127.0.0.1:2380", "etcd client listener URLs")
 var upgradeFuncName = flag.String("upgrade", "", "Upgrade function to run")
 var debugLevels = flag.String("d", "", fmt.Sprintf("comma separated list of %v", mexlog.DebugLevelStrings))
@@ -22,6 +25,14 @@ func main() {
 	mexlog.SetDebugLevelStrs(*debugLevels)
 	// Upgrading an input yaml file
 	if *yamlFile != "" {
+		// Fix up the yaml file paths
+		if strings.HasPrefix(*yamlFile, "~") {
+			*yamlFile = strings.Replace(*yamlFile, "~", os.Getenv("HOME"), 1)
+		}
+		if strings.HasPrefix(*outputFile, "~") {
+			*outputFile = strings.Replace(*outputFile, "~", os.Getenv("HOME"), 1)
+		}
+
 		uf, found := upgrade.UpgradeYamlFuncs[*upgradeFuncName]
 		if !found {
 			log.Fatalf("Upgrade function %s not found.", *upgradeFuncName)
@@ -37,8 +48,10 @@ func main() {
 		if err != nil {
 			log.Fatalf("Failed to do upgrade a file(%s) err:%v\n", *upgradeFuncName, err)
 		}
-		log.Printf("writing to file: %s\n", *outputFile)
-		util.PrintToYamlFile(*outputFile, "", appData, true)
+		outdir := filepath.Dir(*outputFile)
+		outfile := filepath.Base(*outputFile)
+		fmt.Printf("DIrectory: <%s> and file: <%s>\n", outdir, outfile)
+		util.PrintToYamlFile(outfile, outdir, appData, true)
 	}
 	// Upgrading a running etcd database
 	//TODO
