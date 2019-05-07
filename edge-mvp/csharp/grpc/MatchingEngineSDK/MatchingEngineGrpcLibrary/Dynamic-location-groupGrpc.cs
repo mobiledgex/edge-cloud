b4467df309663c6dc607ef7f651a14d430f851e9
Garner Lee <1672745+lgarner@users.noreply.github.com>
2019-05-07 14:34:01 -0700

    Update Android, C++ GRPC/REST, C# Grpc/Rest/UnityRest to latest protos.

--- edge-mvp/csharp/grpc/MatchingEngineSDK/MatchingEngineGrpcLibrary/Dynamic-location-groupGrpc.cs
+++ edge-mvp/csharp/grpc/MatchingEngineSDK/MatchingEngineGrpcLibrary/Dynamic-location-groupGrpc.cs
@@ -95,13 +95,13 @@ namespace DistributedMatchEngine {
           .AddMethod(__Method_SendToGroup, serviceImpl.SendToGroup).Build();
     }
 
-    /// <summary>Register service method implementations with a service binder. Useful when customizing the service binding logic.
+    /// <summary>Register service method with a service binder with or without implementation. Useful when customizing the  service binding logic.
     /// Note: this method is part of an experimental API that can change or be removed without any prior notice.</summary>
     /// <param name="serviceBinder">Service methods will be bound by calling <c>AddMethod</c> on this object.</param>
     /// <param name="serviceImpl">An object implementing the server-side handling logic.</param>
     public static void BindService(grpc::ServiceBinderBase serviceBinder, DynamicLocGroupApiBase serviceImpl)
     {
-      serviceBinder.AddMethod(__Method_SendToGroup, serviceImpl.SendToGroup);
+      serviceBinder.AddMethod(__Method_SendToGroup, serviceImpl == null ? null : new grpc::UnaryServerMethod<global::DistributedMatchEngine.DlgMessage, global::DistributedMatchEngine.DlgReply>(serviceImpl.SendToGroup));
     }
 
   }
