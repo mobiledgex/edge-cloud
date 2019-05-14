df0fd339c5566ad57fc359059b0fb1df95624831
Lev Shvarts <lev@mobiledgex.com>
2019-05-14 16:09:14 -0700

    Implementation of AddClusterInstKeyToAppInstKey

--- edgeproto/upgrade.go
+++ edgeproto/upgrade.go
@@ -1,9 +1,11 @@
 package edgeproto
 
 import (
+	"encoding/json"
 	fmt "fmt"
 
 	"github.com/coreos/etcd/clientv3/concurrency"
+	distributed_match_engine "github.com/mobiledgex/edge-cloud/d-match-engine/dme-proto"
 	"github.com/mobiledgex/edge-cloud/log"
 	"github.com/mobiledgex/edge-cloud/objstore"
 )
@@ -82,6 +84,68 @@ func TestUpgradeExample(objStore objstore.KVStore, stm concurrency.STM) error {
 	return err
 }
 
+// Below is an implementation of AddClusterInstKeyToAppInstKey
+// This upgrade modifies AppInstKey to include ClusterInstKey rather than CloudletKey+Id
+// This allows multiple instances of an app on the same cloudlet, but different cluster instances
 func AddClusterInstKeyToAppInstKey(objStore objstore.KVStore, stm concurrency.STM) error {
-	return nil
+	// Below are the data-structures for the older version of AppInstKey and AppInst
+	type AppInstKeyV0_AddClusterInstKeyToAppInstKey struct {
+		AppKey      AppKey      `protobuf:"bytes,1,opt,name=app_key,json=appKey" json:"app_key"`
+		CloudletKey CloudletKey `protobuf:"bytes,2,opt,name=cloudlet_key,json=cloudletKey" json:"cloudlet_key"`
+		Id          uint64      `protobuf:"fixed64,3,opt,name=id,proto3" json:"id,omitempty"`
+	}
+	type AppInstV0_AddClusterInstKeyToAppInstKey struct {
+		Fields         []string                                   `protobuf:"bytes,1,rep,name=fields" json:"fields,omitempty"`
+		Key            AppInstKeyV0_AddClusterInstKeyToAppInstKey `protobuf:"bytes,2,opt,name=key" json:"key"`
+		CloudletLoc    distributed_match_engine.Loc               `protobuf:"bytes,3,opt,name=cloudlet_loc,json=cloudletLoc" json:"cloudlet_loc"`
+		Uri            string                                     `protobuf:"bytes,4,opt,name=uri,proto3" json:"uri,omitempty"`
+		ClusterInstKey ClusterInstKey                             `protobuf:"bytes,5,opt,name=cluster_inst_key,json=clusterInstKey" json:"cluster_inst_key"`
+		Liveness       Liveness                                   `protobuf:"varint,6,opt,name=liveness,proto3,enum=edgeproto.Liveness" json:"liveness,omitempty"`
+		MappedPorts    []distributed_match_engine.AppPort         `protobuf:"bytes,9,rep,name=mapped_ports,json=mappedPorts" json:"mapped_ports"`
+		Flavor         FlavorKey                                  `protobuf:"bytes,12,opt,name=flavor" json:"flavor"`
+		State          TrackedState                               `protobuf:"varint,14,opt,name=state,proto3,enum=edgeproto.TrackedState" json:"state,omitempty"`
+		Errors         []string                                   `protobuf:"bytes,15,rep,name=errors" json:"errors,omitempty"`
+		CrmOverride    CRMOverride                                `protobuf:"varint,16,opt,name=crm_override,json=crmOverride,proto3,enum=edgeproto.CRMOverride" json:"crm_override,omitempty"`
+		CreatedAt      distributed_match_engine.Timestamp         `protobuf:"bytes,21,opt,name=created_at,json=createdAt" json:"created_at"`
+	}
+
+	var upgCount uint
+	log.DebugLog(log.DebugLevelUpgrade, "AddClusterInstKeyToAppInstKey - change AppInstKey to contain ClusterInstKey instead of CloudletKey")
+	// Define a prefix for a walk
+	keystr := fmt.Sprintf("%s/", objstore.DbKeyPrefixString("AppInst"))
+	err := objStore.List(keystr, func(key, val []byte, rev int64) error {
+		var appV0 AppInstV0_AddClusterInstKeyToAppInstKey
+		err2 := json.Unmarshal(val, &appV0)
+		if err2 != nil {
+			log.DebugLog(log.DebugLevelUpgrade, "Cannot unmarshal key", "val", string(val), "err", err2)
+			return err2
+		}
+		log.DebugLog(log.DebugLevelUpgrade, "Upgrading AppInst from V0 to V1", "AppInstV0", appV0)
+		appV1 := AppInst{}
+		appV1.Fields = appV0.Fields
+		appV1.Key.AppKey = appV0.Key.AppKey
+		appV1.Key.ClusterInstKey = appV0.ClusterInstKey
+		// There is a case in a yml file conversion that autocluster clusterInst doesn't specify cloudletkey
+		if appV0.Key.CloudletKey != appV0.ClusterInstKey.CloudletKey {
+			appV1.Key.ClusterInstKey.CloudletKey = appV0.Key.CloudletKey
+		}
+		appV1.CloudletLoc = appV0.CloudletLoc
+		appV1.Uri = appV0.Uri
+		appV1.Liveness = appV0.Liveness
+		appV1.MappedPorts = appV0.MappedPorts
+		appV1.Flavor = appV0.Flavor
+		appV1.State = appV0.State
+		appV1.Errors = appV0.Errors
+		appV1.CrmOverride = appV0.CrmOverride
+		appV1.CreatedAt = appV0.CreatedAt
+		log.DebugLog(log.DebugLevelUpgrade, "Upgraded AppInstV1", "AppInstV1", appV1)
+		stm.Del(string(key))
+		keystr := objstore.DbKeyString("AppInst", appV1.GetKey())
+		val, _ = json.Marshal(appV1)
+		stm.Put(keystr, string(val))
+		upgCount++
+		return nil
+	})
+	log.DebugLog(log.DebugLevelUpgrade, "Upgrade object count", "Upgrade Count", upgCount)
+	return err
 }
