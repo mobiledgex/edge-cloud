02486507e60b86168f79946dc084e234cd892367
lgarner <1672745+lgarner@users.noreply.github.com>
2019-05-22 09:23:28 -0700

    EDGECLOUD-658: Add a subscriber info check to return a better exception (#478)
    on network switch.

---
+++ edge-mvp/android/EmptyMatchEngineApp/matchingengine/src/main/java/com/mobiledgex/matchingengine/NetworkRequestNoSubscriptionInfoException.java
@@ -0,0 +1,9 @@
+package com.mobiledgex.matchingengine;
+
+import java.lang.Exception;
+
+public class NetworkRequestNoSubscriptionInfoException extends Exception {
+    public NetworkRequestNoSubscriptionInfoException(String msg) {
+        super(msg);
+    }
+}
