fd0a19fa9216e74b8677171be53909b6a9ec7fdd
Lev Shvarts <lev@mobiledgex.com>
2019-06-10 17:02:56 -0700

    Common influxDB names for MC api

--- cloudcommon/names.go
+++ cloudcommon/names.go
@@ -41,6 +41,12 @@ var OperatingSystemLinux = "linux"
 var NetworkSchemePublicIP = "publicip"
 var NetworkSchemePrivateIP = "privateip"
 
+// Metrics common variables - TODO move to edge-cloud-infra after metrics-exporter chagnes
+var InfluxDBVaultPath = "https://vault.mobiledgex.net/v1/secret/data/influxdb/"
+var DeveloperMetricsDbName = "clusterstats"
+var DeveloperAppMetrics = "crm-appinst"
+var DeveloperClusterMetric = "crm-cluster"
+
 // PlatformApps is the set of all special "platform" developers.   Key
 // is DeveloperName:AppName.  Currently only Samsung's Enabling layer is included.
 var platformApps = map[string]bool{
