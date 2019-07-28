a97718a984f1fb6ec295f56a107c3e9e1aaa3255
Jon Gainsley <jon.gainsley@mobiledgex.com>
2019-07-28 15:59:03 -0700

    EDGECLOUD-1029 controllers can talk to each other under kubernetes

--- install-dind.sh
+++ install-dind.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-if [ ! -e  /usr/local/bin/dind-cluster-v1.13.sh ]; then
-   wget https://github.com/kubernetes-sigs/kubeadm-dind-cluster/releases/download/v0.1.0/dind-cluster-v1.13.sh
-   mv dind-cluster-v1.13.sh /usr/local/bin/
-   chmod +x /usr/local/bin/dind-cluster-v1.13.sh
+if [ ! -e  /usr/local/bin/dind-cluster-v1.14.sh ]; then
+   wget https://github.com/kubernetes-sigs/kubeadm-dind-cluster/releases/download/v0.3.0/dind-cluster-v1.14.sh
+   mv dind-cluster-v1.14.sh /usr/local/bin/
+   chmod +x /usr/local/bin/dind-cluster-v1.14.sh
 else
-   echo "dind-cluster-v1.13.sh already installed"
+   echo "dind-cluster-v1.14 already installed"
 fi
