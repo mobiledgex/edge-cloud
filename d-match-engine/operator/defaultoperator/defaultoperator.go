01cce8905a2a2e35716ee9d2c43f558019ea8629
Jim <jim.morris@mobiledgex.com>
2019-08-11 19:55:29 -0500

    DME remove infra plugin dependency  (#598)
    * Allow dme to run independent of infra
    
    * move file
    
    * move files again
    
    * Per review comments:  to completely decouple GDDT DME code and
    associated tests from edge-cloud, move verifylocation GDDT implementation
    including simulators to infra
    
    * fix unit tests

---
+++ d-match-engine/operator/defaultoperator/defaultoperator.go
@@ -0,0 +1,40 @@
+package defaultoperator
+
+import (
+	dme "github.com/mobiledgex/edge-cloud/d-match-engine/dme-proto"
+
+	operator "github.com/mobiledgex/edge-cloud/d-match-engine/operator"
+	simulatedloc "github.com/mobiledgex/edge-cloud/d-match-engine/operator/defaultoperator/simulated-location"
+	simulatedqos "github.com/mobiledgex/edge-cloud/d-match-engine/operator/defaultoperator/simulated-qos"
+
+	"github.com/mobiledgex/edge-cloud/log"
+)
+
+//OperatorApiGw respresent an Operator API Gateway
+type OperatorApiGw struct {
+	Servers *operator.OperatorApiGwServers
+}
+
+func (OperatorApiGw) GetOperatorName() string {
+	return "default"
+}
+
+// Init is called once during startup.
+func (o *OperatorApiGw) Init(operatorName string, servers *operator.OperatorApiGwServers) error {
+	log.DebugLog(log.DebugLevelDmereq, "init for default operator", "operatorName", operatorName)
+	o.Servers = servers
+	return nil
+}
+
+func (*OperatorApiGw) VerifyLocation(mreq *dme.VerifyLocationRequest, mreply *dme.VerifyLocationReply) error {
+	return simulatedloc.VerifySimulatedClientLoc(mreq, mreply)
+}
+
+func (*OperatorApiGw) GetLocation(mreq *dme.GetLocationRequest, mreply *dme.GetLocationReply) error {
+	return simulatedloc.GetSimulatedClientLoc(mreq, mreply)
+}
+
+func (*OperatorApiGw) GetQOSPositionKPI(mreq *dme.QosPositionKpiRequest, getQosSvr dme.MatchEngineApi_GetQosPositionKpiServer) error {
+	log.DebugLog(log.DebugLevelDmereq, "getting simulated results for operator with no QOS Pos implementation")
+	return simulatedqos.GetSimulatedQOSPositionKPI(mreq, getQosSvr)
+}
--- d-match-engine/operator/operator-api-gw.go
+++ d-match-engine/operator/operator-api-gw.go
@@ -1,7 +1,6 @@
 package operator
 
 import (
-	dmecommon "github.com/mobiledgex/edge-cloud/d-match-engine/dme-common"
 	dme "github.com/mobiledgex/edge-cloud/d-match-engine/dme-proto"
 )
 
@@ -18,8 +17,10 @@ type OperatorApiGw interface {
 	GetOperatorName() string
 	// Init is called once during startup.
 	Init(operatorName string, servers *OperatorApiGwServers) error
-	// VerifyLocation verifies a clien's location against the coordinates provided
-	VerifyLocation(mreq *dme.VerifyLocationRequest, mreply *dme.VerifyLocationReply, ckey *dmecommon.CookieKey) (*dme.VerifyLocationReply, error)
+	// VerifyLocation verifies a client's location against the coordinates provided
+	VerifyLocation(mreq *dme.VerifyLocationRequest, mreply *dme.VerifyLocationReply) error
+	// GetLocation gets the client location
+	GetLocation(mreq *dme.GetLocationRequest, mreply *dme.GetLocationReply) error
 	// GetQOSPositionKPI gets QOS KPIs for GPS positions
 	GetQOSPositionKPI(req *dme.QosPositionKpiRequest, getQosSvr dme.MatchEngineApi_GetQosPositionKpiServer) error
 }
