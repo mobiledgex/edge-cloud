32e21f6cc7c4bf8c47155da7499669275bfa302b
lgarner <1672745+lgarner@users.noreply.github.com>
2019-08-26 14:03:43 -0700

    EDGECLOUD-924: Re-org client SDKs into separate git repositories (#621)

--- edge-mvp/android/EmptyMatchEngineApp/matchingengine/src/main/java/com/mobiledgex/matchingengine/MissingRequestException.java
+++
@@ -1,9 +0,0 @@
-package com.mobiledgex.matchingengine;
-
-import java.lang.IllegalArgumentException;
-
-public class MissingRequestException extends IllegalArgumentException {
-    public MissingRequestException(String msg) {
-        super(msg);
-    }
-}
--- edge-mvp/flutter_mex_plugin/ios/Classes/FlutterMexPlugin.m
+++
@@ -1,8 +0,0 @@
-#import "FlutterMexPlugin.h"
-#import <flutter_mex_plugin/flutter_mex_plugin-Swift.h>
-
-@implementation FlutterMexPlugin
-+ (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar>*)registrar {
-  [SwiftFlutterMexPlugin registerWithRegistrar:registrar];
-}
-@end
