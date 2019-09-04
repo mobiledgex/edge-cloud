b80069bb4b250d1c97ac2bd4e547f7963cf49dce
Matthew Chu <matthew.chu@mobiledgex.com>
2019-09-03 17:06:31 -0700

    reordered notify notifications

--- cloud-resource-manager/cmd/crmserver/notify.go
+++ cloud-resource-manager/cmd/crmserver/notify.go
@@ -29,9 +29,9 @@ func InitClientNotify(client *notify.Client, cd *crmutil.ControllerData) {
 }
 
 func initSrvNotify(notifyServer *notify.ServerMgr) {
+	notifyServer.RegisterSendAppCache(&controllerData.AppCache)
 	notifyServer.RegisterSendClusterInstCache(&controllerData.ClusterInstCache)
 	notifyServer.RegisterSendAppInstCache(&controllerData.AppInstCache)
-	notifyServer.RegisterSendAppCache(&controllerData.AppCache)
 	notifyServer.RegisterRecv(notify.NewMetricRecvMany(&CrmMetricsReceiver{}))
 }
 
