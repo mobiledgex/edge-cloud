099e9bd7a3cd873d4b01a358a92b5d370eb32151
Lev Shvarts <lev@mobiledgex.com>
2019-09-30 14:05:22 -0700

    EDGECLOUD-558 Support HELM install (#660)
    * Added a general handling of helm charts as App.
    * Add handling of annotations for helm app and external repo handling
    * Added support to repo hosts with ports.
    * Added a check for possible arbitrary command execution
    * Unit tests for helm functions

---
+++ cloud-resource-manager/k8smgmt/helm_test.go
@@ -0,0 +1,73 @@
+package k8smgmt
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+var testCustomizationFileList = []string{"file1.yml", "file2.yml", "file3.yml"}
+var testInvalidBooleanAnnotationStr = "version=val1.2,timeout"
+var testValidBooleanAnnotationStr = "version=val1.2,wait=true,timeout=60"
+var testInvalidAnnotationsVal = "version=1.2.2;touch /tmp/broken"
+var testInvalidAnnotationsOpt = "version`touch /tmp/broken`;ls -lf /tmp/broken=1.2.2"
+
+var testHemlExistingRepoChart = "existing/testchart"
+var testHemlNewRepoChart = "testcharts/testchart"
+var testHelmNewRepoImage = "http://testchartRepo.mex/charts:testcharts/testchart"
+var testHelmImageWithPort = "http://testchartRepo.mex:8000/charts:testcharts/testchart"
+var testInvalidHelmImageNoRepoName = "http://testchartRepo.mex/charts:testchart"
+
+func TestHelm(t *testing.T) {
+	str := getHelmYamlOpt(testCustomizationFileList)
+	require.Equal(t, "-f file1.yml,file2.yml,file3.yml", str)
+	str = getHelmYamlOpt([]string{})
+	require.Equal(t, "", str)
+
+	str, err := getHelmInstallOptsString("invalid annotations string")
+	require.NotNil(t, err, "This should return an error")
+	require.Equal(t, "", str, "error should return an empty string")
+	str, err = getHelmInstallOptsString("")
+	require.Nil(t, err, "No annotations should be a valid string")
+	require.Equal(t, "", str, "empty options for empty annotations")
+	str, err = getHelmInstallOptsString(testInvalidBooleanAnnotationStr)
+	require.NotNil(t, err, "Incorrect way of specifying boolean option")
+	require.Contains(t, err.Error(), "Invalid annotations string")
+	require.Equal(t, "", str, "error should return an empty string")
+	str, err = getHelmInstallOptsString(testInvalidAnnotationsVal)
+	require.NotNil(t, err)
+	require.Contains(t, err.Error(), "\";\" not allowed in annotations")
+	require.Equal(t, "", str, "error should return an empty string")
+	str, err = getHelmInstallOptsString(testInvalidAnnotationsOpt)
+	require.NotNil(t, err)
+	require.Contains(t, err.Error(), "\"`\" not allowed in annotations")
+	require.Equal(t, "", str, "error should return an empty string")
+
+	str, err = getHelmInstallOptsString(testValidBooleanAnnotationStr)
+	require.Nil(t, err)
+	require.Equal(t, "--version \"val1.2\" --wait --timeout 60", str, "Invalid options string returned")
+
+	repo, chart, err := getHelmRepoAndChart(testHemlExistingRepoChart)
+	require.Nil(t, err)
+	require.Equal(t, "", repo, "Repo should be empty")
+	require.Equal(t, testHemlExistingRepoChart, chart)
+	repo, chart, err = getHelmRepoAndChart(testHelmNewRepoImage)
+	require.Nil(t, err)
+	require.Equal(t, "testcharts http://testchartRepo.mex/charts", repo, "Couldn't get repo from path")
+	require.Equal(t, testHemlNewRepoChart, chart)
+	repo, chart, err = getHelmRepoAndChart(testHelmImageWithPort)
+	require.Nil(t, err)
+	require.Equal(t, "testcharts http://testchartRepo.mex:8000/charts", repo)
+	require.Equal(t, testHemlNewRepoChart, chart)
+	repo, chart, err = getHelmRepoAndChart(testInvalidHelmImageNoRepoName)
+	require.NotNil(t, err, "Repo should have a name")
+	require.Contains(t, err.Error(), "Could not parse the chart")
+	require.Equal(t, "", repo, "Error should result in an empty repo")
+	require.Equal(t, "", chart, "Error should result in an emty chart")
+	//Random string test
+	repo, chart, err = getHelmRepoAndChart("random string : ")
+	require.NotNil(t, err, "Random string should be invalid path")
+	require.Equal(t, "", repo, "Error should result in an empty repo")
+	require.Equal(t, "", chart, "Error should result in an emty chart")
+
+}
