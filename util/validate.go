77b8c760c2ce6b7e2661a0166ffd7ed78c9dc584
Ashish Jain <ashish.jain@mobiledgex.com>
2019-10-14 23:08:28 +0530

    common validation function

--- util/validate.go
+++ util/validate.go
@@ -10,7 +10,6 @@ import (
 	"net/url"
 	"regexp"
 	"strings"
-	"unicode"
 )
 
 // If new valid characters are added here, be sure to update
@@ -81,27 +80,6 @@ func K8SSanitize(name string) string {
 	return strings.ToLower(r.Replace(name))
 }
 
-// Gitlab groups can only contain letters, digits, _ . -
-// cannot start with '-' or end in '.', '.git' or '.atom'
-// This combines the rules for both name and path.
-func GitlabGroupSanitize(name string) string {
-	name = strings.TrimPrefix(name, "-")
-	name = strings.TrimSuffix(name, ".")
-	if strings.HasSuffix(name, ".git") {
-		name = name[:len(name)-4] + "-git"
-	}
-	if strings.HasSuffix(name, ".atom") {
-		name = name[:len(name)-5] + "-atom"
-	}
-	return strings.Map(func(r rune) rune {
-		if unicode.IsLetter(r) || unicode.IsNumber(r) ||
-			r == '_' || r == '.' || r == '-' {
-			return r
-		}
-		return '-'
-	}, name)
-}
-
 func ValidObjName(name string) error {
 	re := regexp.MustCompile("^[a-zA-Z0-9_\\-.]*$")
 	if !re.MatchString(name) {
@@ -110,23 +88,11 @@ func ValidObjName(name string) error {
 	if !ValidLDAPName(name) {
 		return fmt.Errorf("invalid characters in Name")
 	}
-	if strings.HasPrefix(name, ".") {
-		return fmt.Errorf("Name cannot start with '.'")
+	if strings.Contains(name, "::") {
+		return fmt.Errorf("Name cannot contain ::")
 	}
-	if strings.HasPrefix(name, "-") {
-		return fmt.Errorf("Name cannot start with '-'")
-	}
-	if strings.HasSuffix(name, ".") {
-		return fmt.Errorf("Name cannot end with '.'")
-	}
-	if strings.HasSuffix(name, ".git") {
-		return fmt.Errorf("Name cannot end with '.git'")
-	}
-	if strings.HasSuffix(name, ".atom") {
-		return fmt.Errorf("Name cannot end with '.atom'")
-	}
-	if strings.HasSuffix(name, "-cache") {
-		return fmt.Errorf("Name cannot end with '-cache'")
+	if strings.Contains(name, "&") {
+		return fmt.Errorf("Name cannot contain &")
 	}
 	return nil
 }
