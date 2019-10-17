e3883992dd07252885e76c1d83ed7febc85610ee
Lev Shvarts <lev@mobiledgex.com>
2019-10-17 10:21:58 -0500

    Clean up debug code

--- cloud-resource-manager/platform/dind/dind-appinst.go
+++ cloud-resource-manager/platform/dind/dind-appinst.go
@@ -63,8 +63,7 @@ func (s *Platform) CreateAppInst(ctx context.Context, clusterInst *edgeproto.Clu
 	} else if appDeploymentType == cloudcommon.AppDeploymentTypeHelm {
 		err = k8smgmt.CreateHelmAppInst(client, names, clusterInst, app, appInst)
 	} else {
-		//		err = fmt.Errorf("invalid deployment type %s for dind", appDeploymentType)
-		err = nil
+		err = fmt.Errorf("invalid deployment type %s for dind", appDeploymentType)
 	}
 	if err != nil {
 		log.SpanLog(ctx, log.DebugLevelMexos, "error creating dind app")
@@ -98,8 +97,7 @@ func (s *Platform) DeleteAppInst(ctx context.Context, clusterInst *edgeproto.Clu
 	} else if appDeploymentType == cloudcommon.AppDeploymentTypeHelm {
 		err = k8smgmt.DeleteHelmAppInst(client, names, clusterInst)
 	} else {
-		//		err = fmt.Errorf("invalid deployment type %s for dind", appDeploymentType)
-		err = nil
+		err = fmt.Errorf("invalid deployment type %s for dind", appDeploymentType)
 	}
 	if err != nil {
 		return err
