5dfd0b4a5d602e306c4cec829f62060931d91ffc
matt williams <matt.williams@mobiledgex.com>
2019-10-22 17:06:39 -0700

    EC-643 Clean up console noise

--- controller/gputagtable_api.go
+++ controller/gputagtable_api.go
@@ -2,9 +2,8 @@ package main
 
 import (
 	"context"
-	//"errors"
-
 	"fmt"
+
 	"github.com/coreos/etcd/clientv3/concurrency"
 	"github.com/mobiledgex/edge-cloud/edgeproto"
 	"github.com/mobiledgex/edge-cloud/objstore"
@@ -19,7 +18,6 @@ type GpuTagTableApi struct {
 var gpuTagTableApi = GpuTagTableApi{}
 
 func InitGpuTagTableApi(sync *Sync) {
-	//fmt.Printf("\n\t ctl api InitGpuTagTableApi-I-start\n")
 	gpuTagTableApi.sync = sync
 	gpuTagTableApi.store = edgeproto.NewGpuTagTableStore(sync.store)
 	edgeproto.InitGpuTagTableCache(&gpuTagTableApi.cache)
@@ -28,13 +26,10 @@ func InitGpuTagTableApi(sync *Sync) {
 
 func (s *GpuTagTableApi) CreateGpuTagTable(ctx context.Context, in *edgeproto.GpuTagTable) (*edgeproto.Result, error) {
 
-	fmt.Printf("\n\tCreateGpuTagTable-I-what table to look at? in: %s \n", in)
-
 	if err := in.Validate(edgeproto.GpuTagTableAllFieldsMap); err != nil {
 		return &edgeproto.Result{}, err
 	}
 	err := s.sync.ApplySTMWait(ctx, func(stm concurrency.STM) error {
-
 		if s.store.STMGet(stm, &in.Key, nil) {
 			return objstore.ErrKVStoreKeyExists
 		}
@@ -46,9 +41,6 @@ func (s *GpuTagTableApi) CreateGpuTagTable(ctx context.Context, in *edgeproto.Gp
 }
 
 func (s *GpuTagTableApi) DeleteGpuTagTable(ctx context.Context, in *edgeproto.GpuTagTable) (*edgeproto.Result, error) {
-
-	fmt.Printf("\n\tDeleteGpuTagTable-I-what table to look at? in: %s \n", in)
-
 	err := s.sync.ApplySTMWait(ctx, func(stm concurrency.STM) error {
 		if !s.store.STMGet(stm, &in.Key, nil) {
 			return objstore.ErrKVStoreKeyNotFound
@@ -62,8 +54,6 @@ func (s *GpuTagTableApi) DeleteGpuTagTable(ctx context.Context, in *edgeproto.Gp
 
 func (s *GpuTagTableApi) GetGpuTagTable(ctx context.Context, in *edgeproto.GpuTagTableKey) (*edgeproto.GpuTagTable, error) {
 	var tbl edgeproto.GpuTagTable
-	fmt.Printf("\n\tShowGpuTagTable-I-what table to look at? in: %s \n", in.Name)
-
 	err := s.sync.ApplySTMWait(ctx, func(stm concurrency.STM) error {
 		if !s.store.STMGet(stm, in, &tbl) {
 			return objstore.ErrKVStoreKeyNotFound
@@ -75,7 +65,6 @@ func (s *GpuTagTableApi) GetGpuTagTable(ctx context.Context, in *edgeproto.GpuTa
 }
 
 func (s *GpuTagTableApi) ShowGpuTagTable(in *edgeproto.GpuTagTable, cb edgeproto.GpuTagTableApi_ShowGpuTagTableServer) error {
-
 	err := s.cache.Show(in, func(obj *edgeproto.GpuTagTable) error {
 		err := cb.Send(obj)
 		return err
@@ -85,24 +74,11 @@ func (s *GpuTagTableApi) ShowGpuTagTable(in *edgeproto.GpuTagTable, cb edgeproto
 
 func (s *GpuTagTableApi) UpdateGpuTagTable(ctx context.Context, in *edgeproto.GpuTagTable) (*edgeproto.Result, error) {
 
-	/*
-		err := s.sync.ApplySTMWait(ctx, func(stm concurrency.STM) error {
-			cur := edgeproto.App{}
-
-			if !s.store.STMGet(stm, &in.Key, &cur) {
-				return objstore.ErrKVStoreKeyNotFound
-			}
-
-	*/
-	fmt.Printf("\n\tUpdateGpuTagTable-I-what table to look at?\n")
 	for _, field := range in.Fields {
-
-		fmt.Printf("UpdateGpuTagTable-I-next field: %s\n", field)
 		if field == edgeproto.GpuTagTableFieldTags {
 			// which to avoid duplicate tag values
 			for _, f := range in.Tags {
 				if field == f {
-					fmt.Printf("%s already exists in table\n", field)
 					return &edgeproto.Result{}, nil
 				}
 			}
@@ -114,10 +90,6 @@ func (s *GpuTagTableApi) UpdateGpuTagTable(ctx context.Context, in *edgeproto.Gp
 }
 
 func (s *GpuTagTableApi) AddGpuTag(ctx context.Context, in *edgeproto.GpuTagTable) (*edgeproto.Result, error) {
-
-	fmt.Printf("AddGpuTag in: %s\n", in)
-
-	fmt.Printf("\n\tAddGpuTag-I-Add tag %s to tbl %s \n", in.Tags, in.Key)
 	var tbl edgeproto.GpuTagTable
 	err := s.sync.ApplySTMWait(ctx, func(stm concurrency.STM) error {
 		if !s.store.STMGet(stm, &in.Key, &tbl) {
@@ -137,8 +109,6 @@ func (s *GpuTagTableApi) AddGpuTag(ctx context.Context, in *edgeproto.GpuTagTabl
 }
 
 func (s *GpuTagTableApi) RemoveGpuTag(ctx context.Context, in *edgeproto.GpuTagTable) (*edgeproto.Result, error) {
-	fmt.Printf("\n\tRemoveGpuTag-I-remove tag %s from tbl %s\n", in.Tags, in.Key)
-
 	var tbl edgeproto.GpuTagTable
 	err := s.sync.ApplySTMWait(ctx, func(stm concurrency.STM) error {
 		if !s.store.STMGet(stm, &in.Key, &tbl) {
