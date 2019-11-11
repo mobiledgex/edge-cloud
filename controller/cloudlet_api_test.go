2a1d238d29e1691aa81744594d8f0c1b1830f570
Ashish Jain <ashish.jain@mobiledgex.com>
2019-11-11 13:39:34 +0530

    Add unit-tests to test state transitions from controller end

--- controller/cloudlet_api_test.go
+++ controller/cloudlet_api_test.go
@@ -2,13 +2,20 @@ package main
 
 import (
 	"context"
+	"fmt"
 	"testing"
+	"time"
 
 	"github.com/mobiledgex/edge-cloud/edgeproto"
 	"github.com/mobiledgex/edge-cloud/log"
 	"github.com/mobiledgex/edge-cloud/testutil"
 	"github.com/stretchr/testify/require"
 )
+
+type stateTransition struct {
+	triggerState  edgeproto.CloudletState
+	expectedState edgeproto.TrackedState
+}
 
 func TestCloudletApi(t *testing.T) {
 	log.SetDebugLevel(log.DebugLevelEtcd | log.DebugLevelApi)
@@ -54,6 +61,8 @@ func TestCloudletApi(t *testing.T) {
 	testBadLat(t, ctx, &cl, []float64{0, 90.1, -90.1, -1323213, 1232334}, "update")
 	testBadLong(t, ctx, &cl, []float64{0, 180.1, -180.1, -1323213, 1232334}, "update")
 
+	testCloudletUpgrade(t, ctx)
+
 	dummy.Stop()
 }
 
@@ -85,4 +94,125 @@ func testBadLong(t *testing.T, ctx context.Context, clbad *edgeproto.Cloudlet, l
 			require.NotNil(t, err, "update cloudlet bad longitude")
 		}
 	}
+}
+
+func WaitForState(key *edgeproto.CloudletKey, state edgeproto.TrackedState, count int) error {
+	lastState := edgeproto.TrackedState_TRACKED_STATE_UNKNOWN
+	for i := 0; i < count; i++ {
+		cloudlet := edgeproto.Cloudlet{}
+		if !cloudletApi.cache.Get(key, &cloudlet) {
+			return fmt.Errorf("unable to find cloudlet")
+		}
+		if cloudlet.State == state {
+			return nil
+		}
+		time.Sleep(20 * time.Millisecond)
+		lastState = cloudlet.State
+	}
+
+	return fmt.Errorf("Unable to get desired cloudlet state, actual state %s, desired state %s", lastState, state)
+}
+
+func forceCloudletInfoState(ctx context.Context, key *edgeproto.CloudletKey, state edgeproto.CloudletState) {
+	info := edgeproto.CloudletInfo{}
+	info.Key = *key
+	info.State = state
+	cloudletInfoApi.cache.Update(ctx, &info, 0)
+}
+
+func testCloudletUpgrade(t *testing.T, ctx context.Context) {
+	var stateTransitions []stateTransition
+
+	stateTransitions = []stateTransition{
+		stateTransition{
+			triggerState:  edgeproto.CloudletState_CLOUDLET_STATE_UPGRADE,
+			expectedState: edgeproto.TrackedState_UPDATING,
+		},
+		stateTransition{
+			triggerState:  edgeproto.CloudletState_CLOUDLET_STATE_INIT,
+			expectedState: edgeproto.TrackedState_CRM_INITOK,
+		},
+		stateTransition{
+			triggerState:  edgeproto.CloudletState_CLOUDLET_STATE_READY,
+			expectedState: edgeproto.TrackedState_READY,
+		},
+	}
+	testUpgradeScenario(t, ctx, &stateTransitions, "success")
+
+	stateTransitions = []stateTransition{
+		stateTransition{
+			triggerState:  edgeproto.CloudletState_CLOUDLET_STATE_INIT,
+			expectedState: edgeproto.TrackedState_CRM_INITOK,
+		},
+		stateTransition{
+			triggerState:  edgeproto.CloudletState_CLOUDLET_STATE_READY,
+			expectedState: edgeproto.TrackedState_READY,
+		},
+	}
+	testUpgradeScenario(t, ctx, &stateTransitions, "success")
+
+	stateTransitions = []stateTransition{
+		stateTransition{
+			triggerState:  edgeproto.CloudletState_CLOUDLET_STATE_UPGRADE,
+			expectedState: edgeproto.TrackedState_UPDATING,
+		},
+		stateTransition{
+			triggerState:  edgeproto.CloudletState_CLOUDLET_STATE_INIT,
+			expectedState: edgeproto.TrackedState_CRM_INITOK,
+		},
+		stateTransition{
+			triggerState:  edgeproto.CloudletState_CLOUDLET_STATE_ERRORS,
+			expectedState: edgeproto.TrackedState_UPDATE_ERROR,
+		},
+	}
+	testUpgradeScenario(t, ctx, &stateTransitions, "fail")
+
+	stateTransitions = []stateTransition{
+		stateTransition{
+			triggerState:  edgeproto.CloudletState_CLOUDLET_STATE_UPGRADE,
+			expectedState: edgeproto.TrackedState_UPDATING,
+		},
+		stateTransition{
+			triggerState:  edgeproto.CloudletState_CLOUDLET_STATE_ERRORS,
+			expectedState: edgeproto.TrackedState_UPDATE_ERROR,
+		},
+	}
+	testUpgradeScenario(t, ctx, &stateTransitions, "fail")
+
+	stateTransitions = []stateTransition{
+		stateTransition{
+			triggerState:  edgeproto.CloudletState_CLOUDLET_STATE_ERRORS,
+			expectedState: edgeproto.TrackedState_UPDATE_ERROR,
+		},
+	}
+	testUpgradeScenario(t, ctx, &stateTransitions, "fail")
+}
+
+func testUpgradeScenario(t *testing.T, ctx context.Context, transitions *[]stateTransition, scenario string) {
+	var err error
+	cloudlet := testutil.CloudletData[2]
+	err = cloudletApi.CreateCloudlet(&cloudlet, testutil.NewCudStreamoutCloudlet(ctx))
+	require.Nil(t, err)
+
+	go func() {
+		forceCloudletInfoState(ctx, &cloudlet.Key, edgeproto.CloudletState_CLOUDLET_STATE_READY)
+		err := cloudletApi.UpgradeCloudlet(ctx, &cloudlet, testutil.NewCudStreamoutCloudlet(ctx))
+		if scenario == "fail" {
+			require.NotNil(t, err, "upgrade cloudlet should fail")
+		} else {
+			require.Nil(t, err, "upgrade cloudlet should succeed")
+		}
+	}()
+
+	err = WaitForState(&cloudlet.Key, edgeproto.TrackedState_UPDATE_REQUESTED, 10)
+	require.Nil(t, err, "cloudlet state transtions")
+
+	for _, transition := range *transitions {
+		forceCloudletInfoState(ctx, &cloudlet.Key, transition.triggerState)
+		err = WaitForState(&cloudlet.Key, transition.expectedState, 10)
+		require.Nil(t, err, fmt.Sprintf("cloudlet state transtions for %s scenario", scenario))
+	}
+
+	err = cloudletApi.DeleteCloudlet(&cloudlet, testutil.NewCudStreamoutCloudlet(ctx))
+	require.Nil(t, err)
 }
