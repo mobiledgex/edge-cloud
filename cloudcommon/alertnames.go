72d14a6c9be4e68b02345ea48a79225728f5a20c
Lev Shvarts <lev@mobiledgex.com>
2019-12-10 22:11:17 +0900

    Added AppInst alert

--- cloudcommon/alertnames.go
+++ cloudcommon/alertnames.go
@@ -3,12 +3,15 @@ package cloudcommon
 // Alert names
 var AlertAutoScaleUp = "AutoScaleUp"
 var AlertAutoScaleDown = "AutoScaleDown"
+var AlertAppInstDown = "AppInstDown"
 
 // Alert label keys
 var AlertLabelDev = "dev"
 var AlertLabelOperator = "operator"
 var AlertLabelCloudlet = "cloudlet"
 var AlertLabelCluster = "cluster"
+var AlertLabelApp = "app"
+var AlertLabelAppVer = "appver"
 
 // Alert annotation keys
 // for autoscale:
