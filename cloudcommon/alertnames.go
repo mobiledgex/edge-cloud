b2c1de1af7f1563146f0444e845b38097b301b45
Lev Shvarts <lev.shvarts@gmail.com>
2019-12-19 10:46:24 +0900

    EDGECLOUD-1619  RootLB-based Health checking for application instances (#737)
    * Added AppInst health check tracking in DME
    * Added a new field to AppInst to track health check status
    * Health Check Alert handling
    * Added settings in cloudcommon to hold configurable parameters for shepherd

--- cloudcommon/alertnames.go
+++ cloudcommon/alertnames.go
@@ -3,15 +3,21 @@ package cloudcommon
 // Alert names
 var AlertAutoScaleUp = "AutoScaleUp"
 var AlertAutoScaleDown = "AutoScaleDown"
+var AlertAppInstDown = "AppInstDown"
 
 // Alert label keys
 var AlertLabelDev = "dev"
 var AlertLabelOperator = "operator"
 var AlertLabelCloudlet = "cloudlet"
 var AlertLabelCluster = "cluster"
+var AlertLabelApp = "app"
+var AlertLabelAppVer = "appver"
 
 // Alert annotation keys
 // for autoscale:
 var AlertKeyNodeCount = "nodecount"
 var AlertKeyLowCpuNodeCount = "lowcpunodecount"
 var AlertKeyMinNodes = "minnodes"
+
+// for healthCheck:
+var AlertHealthCheckStatus = "status"
