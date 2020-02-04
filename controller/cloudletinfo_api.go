a3c88756e373cf087c03c79e6ef7926a9022488f
Ashish Jain <ashish.jain@mobiledgex.com>
2020-02-04 16:02:01 +0530

    revert previous changes

--- controller/cloudletinfo_api.go
+++ controller/cloudletinfo_api.go
@@ -80,6 +80,9 @@ func (s *CloudletInfoApi) Update(ctx context.Context, in *edgeproto.CloudletInfo
 				return nil
 			})
 		}
+		if in.State == edgeproto.CloudletState_CLOUDLET_STATE_UPGRADE {
+			err = cloudletApi.UpdateCloudletState(ctx, &in.Key, edgeproto.TrackedState_UPDATING)
+		}
 	}
 	if err != nil {
 		log.DebugLog(log.DebugLevelNotify, "CloudletInfo state transition error",
