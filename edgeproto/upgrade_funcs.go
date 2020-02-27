fb09f01176c49721f3d6cecba9774ec28b8c8e63
Lev Shvarts <lev@mobiledgex.com>
2020-02-27 15:06:17 -0800

    Added upgrade function for MaxTrackedDmeClients

--- edgeproto/upgrade_funcs.go
+++ edgeproto/upgrade_funcs.go
@@ -236,3 +236,28 @@ func SetDefaultLoadBalancerMaxPortRange(ctx context.Context, objStore objstore.K
 	})
 	return err
 }
+
+func SetDefaultMaxTrackedDmeClients(ctx context.Context, objStore objstore.KVStore) error {
+	log.DebugLog(log.DebugLevelUpgrade, "SetDefaultMaxTrackedDmeClients - default to 100")
+	keystr := fmt.Sprintf("%s/", objstore.DbKeyPrefixString("Settings"))
+	err := objStore.List(keystr, func(key, val []byte, rev int64) error {
+		var settings Settings
+		err2 := json.Unmarshal(val, &settings)
+		if err2 != nil {
+			log.DebugLog(log.DebugLevelUpgrade, "Cannot unmarshal key", "val", string(val), "err", err2)
+			return err2
+		}
+		if settings.MaxTrackedDmeClients == 0 {
+			log.DebugLog(log.DebugLevelUpgrade, "Defaulting MaxTrackedDmeClients")
+			settings.MaxTrackedDmeClients = 100
+			val, err2 = json.Marshal(settings)
+			if err2 != nil {
+				log.DebugLog(log.DebugLevelUpgrade, "Failed to marshal obj", "key", key, "obj", settings, "err", err2)
+				return err2
+			}
+			objStore.Put(ctx, string(key), string(val))
+		}
+		return nil
+	})
+	return err
+}
