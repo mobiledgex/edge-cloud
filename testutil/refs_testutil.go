32f2a30c04b3066277b02ee40e32aef3b5e1b94c
Jon <38672162+gainsley@users.noreply.github.com>
2020-02-28 09:30:03 -0800

    proto import cleanup (#834)

--- testutil/refs_testutil.go
+++ testutil/refs_testutil.go
@@ -14,7 +14,6 @@ import "github.com/mobiledgex/edge-cloud/log"
 import proto "github.com/gogo/protobuf/proto"
 import fmt "fmt"
 import math "math"
-import _ "github.com/gogo/googleapis/google/api"
 import _ "github.com/mobiledgex/edge-cloud/protogen"
 import _ "github.com/gogo/protobuf/gogoproto"
 
