ae40c1360c2c122e38ee76fc5520cf0608d69013
Lev Shvarts <lev@mobiledgex.com>
2020-03-27 13:21:14 -0700

    Unit test for the DeviceReport Api

--- controller/device_api_test.go
+++ controller/device_api_test.go
@@ -9,6 +9,7 @@ import (
 	"github.com/mobiledgex/edge-cloud/edgeproto"
 	"github.com/mobiledgex/edge-cloud/log"
 	"github.com/mobiledgex/edge-cloud/testutil"
+	"github.com/stretchr/testify/require"
 )
 
 func TestDeviceApi(t *testing.T) {
@@ -46,14 +47,20 @@ func TestDeviceApi(t *testing.T) {
 	deviceApi.Flush(ctx, 0)
 	testutil.InternalDeviceTest(t, "show", &deviceApi, testutil.PlarformDeviceClientData)
 	// Test report to show only a single device in December
-	/*
-		report := edgeproto.DeviceReport{
-			Begin: testutil.GetTimestamp(time.Date(2009, time.December, 1, 23, 0, 0, 0, time.UTC)),
-			End:   testutil.GetTimestamp(time.Date(2009, time.December, 31, 23, 0, 0, 0, time.UTC)),
-		}
-	*/
-	// TODO- run report
-	//api := testutil.NewInternalDeviceApi(&deviceApi)
+	report := edgeproto.DeviceReport{
+		Begin: testutil.GetTimestamp(time.Date(2009, time.December, 1, 23, 0, 0, 0, time.UTC)),
+		End:   testutil.GetTimestamp(time.Date(2009, time.December, 31, 23, 0, 0, 0, time.UTC)),
+	}
+	show := testutil.ShowDevice{}
+	show.Init()
+	err := deviceApi.ShowDeviceReport(&report, &show)
+	require.Nil(t, err)
+	require.Equal(t, 1, len(show.Data))
+	// Verify that the one we got was correct
+	for _, dev := range show.Data {
+		require.Equal(t, testutil.PlarformDeviceClientData[2], dev)
+	}
+
 	// I want to call testutil.ApiClient.ShowDeviceReport()...
 
 	// Evict all the platform device
