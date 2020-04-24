f0fa80a2d56bfb2c0b6fed5b42bf5232cd380557
Jim <jim.morris@mobiledgex.com>
2020-04-23 19:33:19 -0500

    - assume imagepath beginning with localhost is just the local repo - fix delete due to name mismatch vs create

--- controller/app_api.go
+++ controller/app_api.go
@@ -201,10 +201,14 @@ func updateAppFields(ctx context.Context, in *edgeproto.App, revision string) er
 	if !cloudcommon.IsPlatformApp(in.Key.Organization, in.Key.Name) {
 		if in.ImageType == edgeproto.ImageType_IMAGE_TYPE_DOCKER && in.ImagePath != "" {
 			parts := strings.Split(in.ImagePath, "/")
-			// Append default registry address for internal image paths
-			if len(parts) < 2 || !strings.Contains(parts[0], ".") {
-				in.ImagePath = cloudcommon.DockerHub + "/" + in.ImagePath
-				log.SpanLog(ctx, log.DebugLevelApi, "Using default docker registry", "ImagePath", in.ImagePath)
+			if parts[0] == "localhost" {
+				in.ImagePath = strings.Replace(in.ImagePath, "localhost/", "", -1)
+			} else {
+				// Append default registry address for internal image paths
+				if len(parts) < 2 || !strings.Contains(parts[0], ".") {
+					in.ImagePath = cloudcommon.DockerHub + "/" + in.ImagePath
+					log.SpanLog(ctx, log.DebugLevelApi, "Using default docker registry", "ImagePath", in.ImagePath)
+				}
 			}
 		}
 	}
