b80ada7eddebc82e6fdeebd00555d468b9f69094
Jon <38672162+gainsley@users.noreply.github.com>
2020-05-12 10:26:32 -0700

    EDGECLOUD-2707 Multiple AutoProv Policies per App (#925)

---
+++ d-match-engine/dme-common/autoprov_dmestats_test.go
@@ -0,0 +1,301 @@
+package dmecommon
+
+import (
+	"context"
+	"testing"
+
+	"github.com/mobiledgex/edge-cloud/cloudcommon"
+	dme "github.com/mobiledgex/edge-cloud/d-match-engine/dme-proto"
+	"github.com/mobiledgex/edge-cloud/edgeproto"
+	"github.com/mobiledgex/edge-cloud/log"
+	"github.com/stretchr/testify/require"
+)
+
+func TestAutoProvStats(t *testing.T) {
+	log.SetDebugLevel(log.DebugLevelDmereq | log.DebugLevelMetrics)
+	log.InitTracer("")
+	defer log.FinishTracer()
+	ctx := log.StartTestSpan(context.Background())
+
+	org := "org"
+	app := edgeproto.App{
+		Key: edgeproto.AppKey{
+			Organization: org,
+			Name:         "someapp",
+			Version:      "1.0",
+		},
+	}
+	carrier := "oper"
+
+	// cloudlets for policies
+	apCloudlets := []edgeproto.AutoProvCloudlet{
+		edgeproto.AutoProvCloudlet{
+			Key: edgeproto.CloudletKey{
+				Organization: carrier,
+				Name:         "1,1",
+			},
+			Loc: dme.Loc{
+				Latitude:  1,
+				Longitude: 1,
+			},
+		},
+		edgeproto.AutoProvCloudlet{
+			Key: edgeproto.CloudletKey{
+				Organization: carrier,
+				Name:         "4,4",
+			},
+			Loc: dme.Loc{
+				Latitude:  4,
+				Longitude: 4,
+			},
+		},
+		edgeproto.AutoProvCloudlet{
+			Key: edgeproto.CloudletKey{
+				Organization: carrier,
+				Name:         "8,8",
+			},
+			Loc: dme.Loc{
+				Latitude:  8,
+				Longitude: 8,
+			},
+		},
+	}
+
+	// reservable ClusterInsts
+	clusterInsts := []edgeproto.ClusterInst{}
+	for _, cl := range apCloudlets {
+		ci := edgeproto.ClusterInst{
+			Key: edgeproto.ClusterInstKey{
+				ClusterKey: edgeproto.ClusterKey{
+					Name: "clust",
+				},
+				CloudletKey:  cl.Key,
+				Organization: cloudcommon.OrganizationMobiledgeX,
+			},
+			Reservable: true,
+		}
+		clusterInsts = append(clusterInsts, ci)
+	}
+
+	locs := []dme.Loc{}
+	for _, cl := range apCloudlets {
+		locs = append(locs, cl.Loc)
+	}
+
+	policies := []edgeproto.AutoProvPolicy{
+		edgeproto.AutoProvPolicy{
+			Key: edgeproto.PolicyKey{
+				Name:         "policy01",
+				Organization: org,
+			},
+			DeployClientCount:   2,
+			DeployIntervalCount: 2,
+			Cloudlets: []*edgeproto.AutoProvCloudlet{
+				&apCloudlets[0],
+				&apCloudlets[1],
+			},
+		},
+		edgeproto.AutoProvPolicy{
+			Key: edgeproto.PolicyKey{
+				Name:         "policy12",
+				Organization: org,
+			},
+			DeployClientCount:   2,
+			DeployIntervalCount: 2,
+			Cloudlets: []*edgeproto.AutoProvCloudlet{
+				&apCloudlets[1],
+				&apCloudlets[2],
+			},
+		},
+		edgeproto.AutoProvPolicy{
+			Key: edgeproto.PolicyKey{
+				Name:         "policy012",
+				Organization: org,
+			},
+			DeployClientCount:   2,
+			DeployIntervalCount: 2,
+			Cloudlets: []*edgeproto.AutoProvCloudlet{
+				&apCloudlets[0],
+				&apCloudlets[1],
+				&apCloudlets[2],
+			},
+		},
+	}
+	// immediate policies (interval count is 1)
+	immPolicies := []edgeproto.AutoProvPolicy{
+		edgeproto.AutoProvPolicy{
+			Key: edgeproto.PolicyKey{
+				Name:         "immpolicy01",
+				Organization: org,
+			},
+			DeployClientCount:   2,
+			DeployIntervalCount: 1,
+			Cloudlets: []*edgeproto.AutoProvCloudlet{
+				&apCloudlets[0],
+				&apCloudlets[1],
+			},
+		},
+		edgeproto.AutoProvPolicy{
+			Key: edgeproto.PolicyKey{
+				Name:         "immpolicy12",
+				Organization: org,
+			},
+			DeployClientCount:   2,
+			DeployIntervalCount: 1,
+			Cloudlets: []*edgeproto.AutoProvCloudlet{
+				&apCloudlets[1],
+				&apCloudlets[2],
+			},
+		},
+		edgeproto.AutoProvPolicy{
+			Key: edgeproto.PolicyKey{
+				Name:         "immpolicy012",
+				Organization: org,
+			},
+			DeployClientCount:   2,
+			DeployIntervalCount: 1,
+			Cloudlets: []*edgeproto.AutoProvCloudlet{
+				&apCloudlets[0],
+				&apCloudlets[1],
+				&apCloudlets[2],
+			},
+		},
+	}
+
+	emptyTest := apStatsTestData{
+		app:                app,
+		carrier:            carrier,
+		expectedCounts:     make(map[edgeproto.CloudletKey]uint64),
+		expectedSendCounts: make(map[edgeproto.CloudletKey]uint64),
+	}
+
+	// no policies means no stats
+	test := emptyTest
+	test.clusterInsts = clusterInsts
+	test.locs = locs
+	test.run(t, ctx)
+
+	// no reservable cluster insts means no stats
+	test = emptyTest
+	test.policies = append(policies, immPolicies...)
+	test.locs = locs
+	test.run(t, ctx)
+
+	// single policy01
+	test = emptyTest
+	test.clusterInsts = clusterInsts
+	test.policies = policies[0:1]
+	test.locs = locs
+	test.expectedCounts = map[edgeproto.CloudletKey]uint64{
+		apCloudlets[0].Key: 1, // req loc 1,1
+		apCloudlets[1].Key: 2, // req loc 4,4 and 8,8
+	}
+	test.run(t, ctx)
+
+	// single policy12
+	test = emptyTest
+	test.clusterInsts = clusterInsts
+	test.policies = policies[1:2]
+	test.locs = append(locs, locs...) // double requests
+	test.expectedCounts = map[edgeproto.CloudletKey]uint64{
+		apCloudlets[1].Key: 4, // req loc 1,1 and 4,4,
+		apCloudlets[2].Key: 2, // req loc 8,8
+	}
+	test.run(t, ctx)
+
+	// single policy012
+	test = emptyTest
+	test.clusterInsts = clusterInsts
+	test.policies = policies[2:3]
+	test.locs = locs
+	test.expectedCounts = map[edgeproto.CloudletKey]uint64{
+		apCloudlets[0].Key: 1, // req loc 1,1
+		apCloudlets[1].Key: 1, // req loc 4,4
+		apCloudlets[2].Key: 1, // req loc 8,8
+	}
+	test.run(t, ctx)
+
+	// all policies (except immediate)
+	test = emptyTest
+	test.clusterInsts = clusterInsts
+	test.policies = policies
+	test.locs = locs
+	test.expectedCounts = map[edgeproto.CloudletKey]uint64{
+		apCloudlets[0].Key: 1, // req loc 1,1
+		apCloudlets[1].Key: 1, // req loc 4,4
+		apCloudlets[2].Key: 1, // req loc 8,8
+	}
+
+	// all policies (immediate policies should trigger sends)
+	test = emptyTest
+	test.clusterInsts = clusterInsts
+	test.policies = append(policies, immPolicies...)
+	test.locs = append(locs, locs...) // double requests
+	test.expectedCounts = map[edgeproto.CloudletKey]uint64{
+		apCloudlets[0].Key: 2,
+		apCloudlets[1].Key: 2,
+		apCloudlets[2].Key: 2,
+	}
+	test.expectedSendCounts = map[edgeproto.CloudletKey]uint64{
+		apCloudlets[0].Key: 2,
+		apCloudlets[1].Key: 2,
+		apCloudlets[2].Key: 2,
+	}
+	test.run(t, ctx)
+}
+
+type apStatsTestData struct {
+	app                edgeproto.App
+	carrier            string
+	clusterInsts       []edgeproto.ClusterInst
+	policies           []edgeproto.AutoProvPolicy
+	locs               []dme.Loc
+	expectedCounts     map[edgeproto.CloudletKey]uint64
+	expectedSendCounts map[edgeproto.CloudletKey]uint64
+}
+
+func (s *apStatsTestData) run(t *testing.T, ctx context.Context) {
+	// reset all data
+	actualSendCounts := make(map[edgeproto.CloudletKey]uint64)
+	SetupMatchEngine()
+	InitAutoProvStats(500, 0, 1, &edgeproto.NodeKey{}, func(ctx context.Context, counts *edgeproto.AutoProvCounts) bool {
+		require.Equal(t, 1, len(counts.Counts))
+		apCount := counts.Counts[0]
+		actualSendCounts[apCount.CloudletKey] = apCount.Count
+		require.True(t, apCount.ProcessNow)
+		return true
+	})
+	apHandler := AutoProvPolicyHandler{}
+
+	// add policies
+	for _, policy := range s.policies {
+		apHandler.Update(ctx, &policy, 0)
+	}
+	// add ClusterInsts
+	for _, ci := range s.clusterInsts {
+		DmeAppTbl.FreeReservableClusterInsts.Update(ctx, &ci, 0)
+	}
+	// set policies on App
+	app := s.app
+	for _, p := range s.policies {
+		app.AutoProvPolicies = append(app.AutoProvPolicies, p.Key.Name)
+	}
+	// add App
+	AddApp(ctx, &app)
+
+	// do "find cloudlet" calls
+	for _, loc := range s.locs {
+		findBestForCarrier(ctx, s.carrier, &app.Key, &loc, 1)
+	}
+
+	// get actual stats
+	actualCounts := make(map[edgeproto.CloudletKey]uint64)
+	for ii, _ := range autoProvStats.shards {
+		for key, counts := range autoProvStats.shards[ii].appCloudletCounts {
+			actualCounts[key.CloudletKey] = counts.count
+		}
+	}
+
+	require.Equal(t, s.expectedCounts, actualCounts)
+	require.Equal(t, s.expectedSendCounts, actualSendCounts)
+}
--- edgeproto/app.pb.gw.go
+++ edgeproto/app.pb.gw.go
@@ -104,6 +104,40 @@ func request_AppApi_ShowApp_0(ctx context.Context, marshaler runtime.Marshaler, 
 
 }
 
+func request_AppApi_AddAppAutoProvPolicy_0(ctx context.Context, marshaler runtime.Marshaler, client AppApiClient, req *http.Request, pathParams map[string]string) (proto.Message, runtime.ServerMetadata, error) {
+	var protoReq AppAutoProvPolicy
+	var metadata runtime.ServerMetadata
+
+	newReader, berr := utilities.IOReaderFactory(req.Body)
+	if berr != nil {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", berr)
+	}
+	if err := marshaler.NewDecoder(newReader()).Decode(&protoReq); err != nil && err != io.EOF {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
+	}
+
+	msg, err := client.AddAppAutoProvPolicy(ctx, &protoReq, grpc.Header(&metadata.HeaderMD), grpc.Trailer(&metadata.TrailerMD))
+	return msg, metadata, err
+
+}
+
+func request_AppApi_RemoveAppAutoProvPolicy_0(ctx context.Context, marshaler runtime.Marshaler, client AppApiClient, req *http.Request, pathParams map[string]string) (proto.Message, runtime.ServerMetadata, error) {
+	var protoReq AppAutoProvPolicy
+	var metadata runtime.ServerMetadata
+
+	newReader, berr := utilities.IOReaderFactory(req.Body)
+	if berr != nil {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", berr)
+	}
+	if err := marshaler.NewDecoder(newReader()).Decode(&protoReq); err != nil && err != io.EOF {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
+	}
+
+	msg, err := client.RemoveAppAutoProvPolicy(ctx, &protoReq, grpc.Header(&metadata.HeaderMD), grpc.Trailer(&metadata.TrailerMD))
+	return msg, metadata, err
+
+}
+
 // RegisterAppApiHandlerFromEndpoint is same as RegisterAppApiHandler but
 // automatically dials to "endpoint" and closes the connection when "ctx" gets done.
 func RegisterAppApiHandlerFromEndpoint(ctx context.Context, mux *runtime.ServeMux, endpoint string, opts []grpc.DialOption) (err error) {
@@ -222,6 +256,46 @@ func RegisterAppApiHandlerClient(ctx context.Context, mux *runtime.ServeMux, cli
 
 	})
 
+	mux.Handle("POST", pattern_AppApi_AddAppAutoProvPolicy_0, func(w http.ResponseWriter, req *http.Request, pathParams map[string]string) {
+		ctx, cancel := context.WithCancel(req.Context())
+		defer cancel()
+		inboundMarshaler, outboundMarshaler := runtime.MarshalerForRequest(mux, req)
+		rctx, err := runtime.AnnotateContext(ctx, mux, req)
+		if err != nil {
+			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
+			return
+		}
+		resp, md, err := request_AppApi_AddAppAutoProvPolicy_0(rctx, inboundMarshaler, client, req, pathParams)
+		ctx = runtime.NewServerMetadataContext(ctx, md)
+		if err != nil {
+			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
+			return
+		}
+
+		forward_AppApi_AddAppAutoProvPolicy_0(ctx, mux, outboundMarshaler, w, req, resp, mux.GetForwardResponseOptions()...)
+
+	})
+
+	mux.Handle("POST", pattern_AppApi_RemoveAppAutoProvPolicy_0, func(w http.ResponseWriter, req *http.Request, pathParams map[string]string) {
+		ctx, cancel := context.WithCancel(req.Context())
+		defer cancel()
+		inboundMarshaler, outboundMarshaler := runtime.MarshalerForRequest(mux, req)
+		rctx, err := runtime.AnnotateContext(ctx, mux, req)
+		if err != nil {
+			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
+			return
+		}
+		resp, md, err := request_AppApi_RemoveAppAutoProvPolicy_0(rctx, inboundMarshaler, client, req, pathParams)
+		ctx = runtime.NewServerMetadataContext(ctx, md)
+		if err != nil {
+			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
+			return
+		}
+
+		forward_AppApi_RemoveAppAutoProvPolicy_0(ctx, mux, outboundMarshaler, w, req, resp, mux.GetForwardResponseOptions()...)
+
+	})
+
 	return nil
 }
 
@@ -233,6 +307,10 @@ var (
 	pattern_AppApi_UpdateApp_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1}, []string{"update", "app"}, ""))
 
 	pattern_AppApi_ShowApp_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1}, []string{"show", "app"}, ""))
+
+	pattern_AppApi_AddAppAutoProvPolicy_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1}, []string{"add", "appautoprovpolicy"}, ""))
+
+	pattern_AppApi_RemoveAppAutoProvPolicy_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1}, []string{"remove", "appautoprovpolicy"}, ""))
 )
 
 var (
@@ -243,4 +321,8 @@ var (
 	forward_AppApi_UpdateApp_0 = runtime.ForwardResponseMessage
 
 	forward_AppApi_ShowApp_0 = runtime.ForwardResponseStream
+
+	forward_AppApi_AddAppAutoProvPolicy_0 = runtime.ForwardResponseMessage
+
+	forward_AppApi_RemoveAppAutoProvPolicy_0 = runtime.ForwardResponseMessage
 )
