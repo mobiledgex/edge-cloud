af4c9796bc8a5695868b6260691c43807e89acd9
Lev Shvarts <lev.shvarts@gmail.com>
2020-06-10 11:37:07 -0700

    EDGECLOUD-2262  Need a way to exclude ports from healthcheck (#955)
     - Add health check excluded ports option when creating an app - sometimes apps don't always listen on the ports that are specified in the app definition, and we don't want to erroneously mark these appInstances as failing health check.
     - Added new field SkipHcPorts to CreateApp API. It takes a string similar to what AccessPorts takes and disables health checking on the ports . The information is used when creating envoy configuration to decide whether to run health-check on any given port, or not.

--- gencmd/app.cmd.go
+++ gencmd/app.cmd.go
@@ -479,6 +479,7 @@ var AppOptionalArgs = []string{
 	"defaultprivacypolicy",
 	"autoprovpolicies",
 	"templatedelimiter",
+	"skiphcports",
 }
 var AppAliasArgs = []string{
 	"app-org=key.organization",
@@ -517,6 +518,7 @@ var AppComments = map[string]string{
 	"deleteprepare":           "Preparing to be deleted",
 	"autoprovpolicies":        "Auto provisioning policy names",
 	"templatedelimiter":       "Delimiter to be used for template parsing, defaults to [[ ]]",
+	"skiphcports":             "Comma separated list of protocol:port pairs that we should not run health check on Should be configured in case app does not always listen on these ports all can be specified if no health check to be run for this app Numerical values must be decimal format. i.e. tcp:80,udp:10002,http:443",
 }
 var AppSpecialArgs = map[string]string{
 	"autoprovpolicies": "StringArray",
