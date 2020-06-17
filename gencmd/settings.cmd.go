86046efad48eb63cdb1d279a7f011cbe6478af19
Lev Shvarts <lev.shvarts@gmail.com>
2020-06-16 20:19:37 -0700

    EDGECLOUD-1752  Add different retention policies to InfluxDB measurements (#966)
    * InfluxDb retention policy setting for metrics

--- gencmd/settings.cmd.go
+++ gencmd/settings.cmd.go
@@ -210,6 +210,7 @@ var SettingsOptionalArgs = []string{
 	"loadbalancermaxportrange",
 	"maxtrackeddmeclients",
 	"chefclientinterval",
+	"influxdbmetricsretention",
 }
 var SettingsAliasArgs = []string{}
 var SettingsComments = map[string]string{
@@ -230,6 +231,7 @@ var SettingsComments = map[string]string{
 	"loadbalancermaxportrange":          "Max IP Port range when using a load balancer",
 	"maxtrackeddmeclients":              "Max DME clients to be tracked at the same time.",
 	"chefclientinterval":                "Default chef client interval (duration)",
+	"influxdbmetricsretention":          "Default influxDB metrics retention policy (duration)",
 }
 var SettingsSpecialArgs = map[string]string{
 	"fields": "StringArray",
