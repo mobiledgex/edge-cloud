07a82a68233389bdf3cbbca4d43a8c1112a46f84
Jim <jim.morris@mobiledgex.com>
2020-07-10 07:48:58 -0500

    skip cleanup on failure option (#997)
    * cleanup-on-fail
    
    * rename skip_cleanup_on failure to skip_crm_cleanup_on_failure

--- gencmd/alldata.cmd.go
+++ gencmd/alldata.cmd.go
@@ -114,6 +114,9 @@ func AllDataHideTags(in *edgeproto.AllData) {
 		if _, found := tags["nocmp"]; found {
 			in.ClusterInsts[i0].MasterNodeFlavor = ""
 		}
+		if _, found := tags["nocmp"]; found {
+			in.ClusterInsts[i0].SkipCrmCleanupOnFailure = false
+		}
 	}
 	for i0 := 0; i0 < len(in.Apps); i0++ {
 		if _, found := tags["nocmp"]; found {
@@ -387,6 +390,7 @@ var AllDataOptionalArgs = []string{
 	"clusterinsts:#.sharedvolumesize",
 	"clusterinsts:#.privacypolicy",
 	"clusterinsts:#.masternodeflavor",
+	"clusterinsts:#.skipcrmcleanuponfailure",
 	"apps:#.fields",
 	"apps:#.key.organization",
 	"apps:#.key.name",
@@ -653,6 +657,7 @@ var AllDataComments = map[string]string{
 	"clusterinsts:#.sharedvolumesize":                            "Size of an optional shared volume to be mounted on the master",
 	"clusterinsts:#.privacypolicy":                               "Optional privacy policy name",
 	"clusterinsts:#.masternodeflavor":                            "Generic flavor for k8s master VM when worker nodes > 0",
+	"clusterinsts:#.skipcrmcleanuponfailure":                     "Prevents cleanup of resources on failure within CRM, used for diagnostic purposes",
 	"apps:#.fields":                                              "Fields are used for the Update API to specify which fields to apply",
 	"apps:#.key.organization":                                    "App developer organization",
 	"apps:#.key.name":                                            "App name",
