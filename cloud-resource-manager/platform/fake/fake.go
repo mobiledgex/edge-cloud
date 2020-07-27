e5b9f5f2f98d3cd0e3c74ab71c885ff7a630d1ec
Ashish Jain <ashxjain@gmail.com>
2020-07-27 22:23:45 +0530

    fix deletecloudlet + add crmoverride + fix cli (#1010)

--- cloud-resource-manager/platform/fake/fake.go
+++ cloud-resource-manager/platform/fake/fake.go
@@ -131,7 +131,7 @@ func (s *Platform) CreateCloudlet(ctx context.Context, cloudlet *edgeproto.Cloud
 	return nil
 }
 
-func (s *Platform) DeleteCloudlet(ctx context.Context, cloudlet *edgeproto.Cloudlet, pfConfig *edgeproto.PlatformConfig, updateCallback edgeproto.CacheUpdateCallback) error {
+func (s *Platform) DeleteCloudlet(ctx context.Context, cloudlet *edgeproto.Cloudlet, pfConfig *edgeproto.PlatformConfig, caches *platform.Caches, updateCallback edgeproto.CacheUpdateCallback) error {
 	log.DebugLog(log.DebugLevelInfra, "delete fake Cloudlet", "key", cloudlet.Key)
 	updateCallback(edgeproto.UpdateTask, "Deleting Cloudlet")
 	updateCallback(edgeproto.UpdateTask, "Stopping CRMServer")
