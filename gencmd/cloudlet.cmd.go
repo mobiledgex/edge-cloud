e5b4add2997ae9f3a9d0041cf8986d6cfc065375
Jim <jim.morris@mobiledgex.com>
2020-11-04 14:04:10 -0600

    prevent specifying resource info on ShowCloudletInfo

--- gencmd/cloudlet.cmd.go
+++ gencmd/cloudlet.cmd.go
@@ -1476,17 +1476,6 @@ var CloudletInfoOptionalArgs = []string{
 	"osimages:#.diskformat",
 	"controllercachereceived",
 	"maintenancestate",
-	"resources.vms:#.name",
-	"resources.vms:#.type",
-	"resources.vms:#.status",
-	"resources.vms:#.infraflavor",
-	"resources.vms:#.ipaddresses:#.externalip",
-	"resources.vms:#.ipaddresses:#.internalip",
-	"resources.vms:#.containers:#.name",
-	"resources.vms:#.containers:#.type",
-	"resources.vms:#.containers:#.status",
-	"resources.vms:#.containers:#.clusterip",
-	"resources.vms:#.containers:#.restarts",
 }
 var CloudletInfoAliasArgs = []string{
 	"cloudlet-org=key.organization",
