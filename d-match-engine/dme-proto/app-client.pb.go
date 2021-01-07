a4d097fc25a609b9ba4e174aa73ea7bf267b1407
Ashish Jain <ashish.jain@mobiledgex.com>
2021-01-07 12:01:56 +0530

    Disallow args which are configured as part of noconfig + fix liveness for internal apps (#1173)
    * EDGECLOUD-4209 Invalid arguments should not be allowed as part of API input
    * EDGECLOUD-4210 Internal apps should have liveness set to DYNAMIC

--- d-match-engine/dme-proto/app-client.pb.go
+++ d-match-engine/dme-proto/app-client.pb.go
@@ -7225,6 +7225,45 @@ func (e *DynamicLocGroupRequest_DlgCommType) UnmarshalJSON(b []byte) error {
 	}
 	return fmt.Errorf("No enum value for %v", b)
 }
+func (m *RegisterClientRequest) IsValidArgsForRegisterClient() error {
+	return nil
+}
+
+func (m *FindCloudletRequest) IsValidArgsForFindCloudlet() error {
+	return nil
+}
+
+func (m *PlatformFindCloudletRequest) IsValidArgsForPlatformFindCloudlet() error {
+	return nil
+}
+
+func (m *VerifyLocationRequest) IsValidArgsForVerifyLocation() error {
+	return nil
+}
+
+func (m *GetLocationRequest) IsValidArgsForGetLocation() error {
+	return nil
+}
+
+func (m *DynamicLocGroupRequest) IsValidArgsForAddUserToGroup() error {
+	return nil
+}
+
+func (m *AppInstListRequest) IsValidArgsForGetAppInstList() error {
+	return nil
+}
+
+func (m *FqdnListRequest) IsValidArgsForGetFqdnList() error {
+	return nil
+}
+
+func (m *AppOfficialFqdnRequest) IsValidArgsForGetAppOfficialFqdn() error {
+	return nil
+}
+
+func (m *QosPositionRequest) IsValidArgsForGetQosPositionKpi() error {
+	return nil
+}
 
 type MatchOptions struct {
 	// Filter will ignore 0 or nil fields on the passed in object
--- edgeproto/cloudletpool.pb.go
+++ edgeproto/cloudletpool.pb.go
@@ -1716,6 +1716,62 @@ func (m *CloudletPoolMember) ValidateEnums() error {
 	return nil
 }
 
+func (m *CloudletPool) IsValidArgsForCreateCloudletPool() error {
+	if m.CreatedAt.Nanos != 0 {
+		return fmt.Errorf("Invalid field specified: CreatedAt.Nanos, this field is only for internal use")
+	}
+	if m.CreatedAt.Seconds != 0 {
+		return fmt.Errorf("Invalid field specified: CreatedAt.Seconds, this field is only for internal use")
+	}
+	if m.UpdatedAt.Nanos != 0 {
+		return fmt.Errorf("Invalid field specified: UpdatedAt.Nanos, this field is only for internal use")
+	}
+	if m.UpdatedAt.Seconds != 0 {
+		return fmt.Errorf("Invalid field specified: UpdatedAt.Seconds, this field is only for internal use")
+	}
+	return nil
+}
+
+func (m *CloudletPool) IsValidArgsForDeleteCloudletPool() error {
+	if m.CreatedAt.Nanos != 0 {
+		return fmt.Errorf("Invalid field specified: CreatedAt.Nanos, this field is only for internal use")
+	}
+	if m.CreatedAt.Seconds != 0 {
+		return fmt.Errorf("Invalid field specified: CreatedAt.Seconds, this field is only for internal use")
+	}
+	if m.UpdatedAt.Nanos != 0 {
+		return fmt.Errorf("Invalid field specified: UpdatedAt.Nanos, this field is only for internal use")
+	}
+	if m.UpdatedAt.Seconds != 0 {
+		return fmt.Errorf("Invalid field specified: UpdatedAt.Seconds, this field is only for internal use")
+	}
+	return nil
+}
+
+func (m *CloudletPool) IsValidArgsForUpdateCloudletPool() error {
+	if m.CreatedAt.Nanos != 0 {
+		return fmt.Errorf("Invalid field specified: CreatedAt.Nanos, this field is only for internal use")
+	}
+	if m.CreatedAt.Seconds != 0 {
+		return fmt.Errorf("Invalid field specified: CreatedAt.Seconds, this field is only for internal use")
+	}
+	if m.UpdatedAt.Nanos != 0 {
+		return fmt.Errorf("Invalid field specified: UpdatedAt.Nanos, this field is only for internal use")
+	}
+	if m.UpdatedAt.Seconds != 0 {
+		return fmt.Errorf("Invalid field specified: UpdatedAt.Seconds, this field is only for internal use")
+	}
+	return nil
+}
+
+func (m *CloudletPoolMember) IsValidArgsForAddCloudletPoolMember() error {
+	return nil
+}
+
+func (m *CloudletPoolMember) IsValidArgsForRemoveCloudletPoolMember() error {
+	return nil
+}
+
 func (m *CloudletPoolKey) Size() (n int) {
 	if m == nil {
 		return 0
