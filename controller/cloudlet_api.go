db1751fd1f885642a49f2b88a565bdbe14ed8076
Ashish Jain <ashish.jain@mobiledgex.com>
2021-03-01 22:21:54 +0530

    EDGECLOUD-4430: Delete Cloudlet fails with read timeout(intermittent)

--- controller/cloudlet_api.go
+++ controller/cloudlet_api.go
@@ -598,9 +598,6 @@ func (s *CloudletApi) createCloudletInternal(cctx *CallContext, in *edgeproto.Cl
 			}
 			cloudlet.ChefClientKey = in.ChefClientKey
 			cloudlet.State = newState
-			if in.DeploymentLocal || cloudletPlatform.IsCloudletServicesLocal() {
-				cloudlet.HostController = *externalApiAddr
-			}
 			s.store.STMPut(stm, &cloudlet)
 			return nil
 		})
@@ -635,6 +632,22 @@ func (s *CloudletApi) createCloudletInternal(cctx *CallContext, in *edgeproto.Cl
 		undoErr := s.deleteCloudletInternal(cctx.WithUndo(), in, cb)
 		if undoErr != nil {
 			log.SpanLog(ctx, log.DebugLevelInfo, "Undo create Cloudlet", "undoErr", undoErr)
+		}
+	} else {
+		// Store controller address if crmserver is started locally
+		cloudlet := edgeproto.Cloudlet{}
+		err = s.sync.ApplySTMWait(ctx, func(stm concurrency.STM) error {
+			if !s.store.STMGet(stm, &in.Key, &cloudlet) {
+				return in.Key.NotFoundError()
+			}
+			if in.DeploymentLocal || cloudletPlatform.IsCloudletServicesLocal() {
+				cloudlet.HostController = *externalApiAddr
+			}
+			s.store.STMPut(stm, &cloudlet)
+			return nil
+		})
+		if err != nil {
+			return err
 		}
 	}
 	if deleteAccessVars {
