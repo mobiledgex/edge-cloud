4f247980354ecc5fe1caba43e76a477978d25156
Ashish Jain <ashish.jain@mobiledgex.com>
2021-03-04 13:30:22 +0530

    update with review comments

--- controller/clusterinst_api.go
+++ controller/clusterinst_api.go
@@ -1038,7 +1038,7 @@ func (s *ClusterInstApi) deleteClusterInstInternal(cctx *CallContext, in *edgepr
 	}
 	// If it is autoClusterInst and creation had failed, then deletion should proceed
 	// even though clusterinst is in use by Application Instance
-	if in.Liveness != edgeproto.Liveness_LIVENESS_DYNAMIC {
+	if !(cctx.Undo && cctx.AutoCluster) {
 		if appInstApi.UsesClusterInst(&in.Key) {
 			return errors.New("ClusterInst in use by Application Instance")
 		}
