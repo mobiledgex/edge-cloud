59e93ddc931316cf6a89e183b7c2ae5d33668838
Ashish Jain <ashish.jain@mobiledgex.com>
2021-03-15 08:37:15 +0530

    EDGECLOUD-4575: Support VNC console for MobiledgeX managed IaaS Openstack (#1267)
    * EDGECLOUD-4575: Support VNC console for MobiledgeX managed IaaS Openstack
    
    * use set instead of add

--- edgeturn/server.go
+++ edgeturn/server.go
@@ -292,6 +292,10 @@ func (t *HttpTransport) RoundTrip(r *http.Request) (*http.Response, error) {
 		TurnProxy.Remove(token)
 		return nil, fmt.Errorf("missing required details in proxy value")
 	}
+	if proxyVal.InitURL != nil && proxyVal.InitURL.Scheme != "" {
+		r.Header.Set("X-Forwarded-Proto", proxyVal.InitURL.Scheme)
+	}
+
 	stream, err := proxyVal.ProxySess.OpenStream()
 	if err != nil {
 		return nil, fmt.Errorf("failed to open smux stream: %v", err)
