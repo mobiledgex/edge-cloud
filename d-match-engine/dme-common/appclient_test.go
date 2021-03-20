d0e750bc17b346b538493fbda0173f30441a2fd4
Lev Shvarts <lev.shvarts@gmail.com>
2021-03-20 00:41:44 -0700

    EDGECLOUD-2071  AppInstClients should be timed out on the DME and the Controller after some time (#1268)
    Added timeout setting for appInstClients and a thread in dme to periodically run and timeout clients that were not active for a while.
    Added unit-test and e2e-tests for the appinst clients cleanup thread.
    Set default timeout for the clients to 24 hours - same as dme cookie

--- d-match-engine/dme-common/appclient_test.go
+++ d-match-engine/dme-common/appclient_test.go
@@ -2,8 +2,11 @@ package dmecommon
 
 import (
 	"testing"
+	"time"
 
+	"github.com/mobiledgex/edge-cloud/cloudcommon"
 	dmetest "github.com/mobiledgex/edge-cloud/d-match-engine/dme-testutil"
+	"github.com/mobiledgex/edge-cloud/edgeproto"
 	"github.com/mobiledgex/edge-cloud/log"
 	"github.com/stretchr/testify/require"
 	"golang.org/x/net/context"
@@ -62,4 +65,27 @@ func TestAddClients(t *testing.T) {
 	list, found = clientsMap.clientsByApp[dmetest.AppInstClientData[0].ClientKey.AppInstKey.AppKey]
 	require.True(t, found)
 	require.Equal(t, 0, len(list))
+
+	// test timeout of the appInstances
+	tsOld := cloudcommon.TimeToTimestamp(time.Now().Add(-1 * time.Minute))
+	data := dmetest.AppInstClientData[3]
+	data.Location.Timestamp = &tsOld
+	UpdateClientsBuffer(ctx, &data)
+	tsFuture := cloudcommon.TimeToTimestamp(time.Now().Add(1 * time.Minute))
+	data = dmetest.AppInstClientData[4]
+	data.Location.Timestamp = &tsFuture
+	UpdateClientsBuffer(ctx, &data)
+	list, found = clientsMap.clientsByApp[dmetest.AppInstClientData[4].ClientKey.AppInstKey.AppKey]
+	require.True(t, found)
+	require.Equal(t, 2, len(list))
+	// set quick timeout
+	Settings.AppinstClientCleanupInterval = edgeproto.Duration(1 * time.Second)
+	clientsMap.UpdateClientTimeout(Settings.AppinstClientCleanupInterval)
+	// give the thread a bit of time to run
+	time.Sleep(2 * time.Second)
+	// Check to see that one of them got deleted
+	list, found = clientsMap.clientsByApp[dmetest.AppInstClientData[4].ClientKey.AppInstKey.AppKey]
+	require.True(t, found)
+	require.Equal(t, 1, len(list))
+	require.Equal(t, data, list[0])
 }
