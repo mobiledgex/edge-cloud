403e9085c94f1e6d4446aeac25bcd8033b0e42a8
franklin-huang-mobiledgex <franklin.huang@mobiledgex.com>
2021-03-26 15:33:04 -0500

    add getDefRPName function

--- controller/influxq_client/influxq_cq.go
+++ controller/influxq_client/influxq_cq.go
@@ -120,7 +120,7 @@ func CreateLatencyContinuousQuerySettings(collectionInterval time.Duration, newD
 		AggregationFunctions:      LatencyAggregationFunctions,
 		NewDbName:                 newDbName,
 		CollectionInterval:        collectionInterval,
-		RetentionPolicyName:       fmt.Sprintf("%s_default", newDbName),
+		RetentionPolicyName:       getDefaultRetentionPolicyName(newDbName),
 		NewRetentionPolicyCreated: rpDone,
 	}
 }
@@ -136,7 +136,7 @@ func CreateDeviceInfoContinuousQuerySettings(collectionInterval time.Duration, n
 		AggregationFunctions:      DeviceInfoAggregationFunctions,
 		NewDbName:                 newDbName,
 		CollectionInterval:        collectionInterval,
-		RetentionPolicyName:       fmt.Sprintf("%s_default", newDbName),
+		RetentionPolicyName:       getDefaultRetentionPolicyName(newDbName),
 		NewRetentionPolicyCreated: rpDone,
 	}
 }
