a044fc294e3b5f7eb3aa676d5c13c94f7ce803c4
franklin-huang-mobiledgex <52431019+franklin-huang-mobiledgex@users.noreply.github.com>
2021-04-07 22:20:31 -0500

    EDGECLOUD-4724: Edge Events should return errors (#1303)
    * only fallthrough from event latency to location update if valid location
    
    * add updatedStats to edgeEventsCookieKey so we can track cloudlets found from getAppInstList
    
    * return event_error with msg on non critical errors in edgeevents
    
    * fix unit test, locationtile has tile length, rolling stats include 0 values
    
    * fix appinstlist e2e tests
    
    * helper functions, pass time.Duration by value, error send event

--- d-match-engine/dme-common/gpslocation-stats_test.go
+++ d-match-engine/dme-common/gpslocation-stats_test.go
@@ -40,65 +40,65 @@ func TestGpsLocation(t *testing.T) {
 	// Get location tile for Beacon with location tile length == 1km
 	tile1Beacon := GetLocationTileFromGpsLocation(beacon, tl1)
 	// Get Gps Location ranges for tile generated above
-	under, over, err := GetGpsLocationRangeFromLocationTile(tile1Beacon, tl1)
+	under, over, err := GetGpsLocationRangeFromLocationTile(tile1Beacon)
 	assert.Nil(t, err)
 	// Check to make sure lat, long of beacon is within the location ranges generated above
 	checkLocationRange(t, under, over, beacon)
 
 	// Get location tile for Beacon with location tile length == 2km
 	tile2Beacon := GetLocationTileFromGpsLocation(beacon, tl2)
-	under, over, err = GetGpsLocationRangeFromLocationTile(tile2Beacon, tl2)
+	under, over, err = GetGpsLocationRangeFromLocationTile(tile2Beacon)
 	assert.Nil(t, err)
 	checkLocationRange(t, under, over, beacon)
 
 	// Get location tile for Beacon with location tile length == 5km
 	tile5Beacon := GetLocationTileFromGpsLocation(beacon, tl5)
-	under, over, err = GetGpsLocationRangeFromLocationTile(tile5Beacon, tl5)
+	under, over, err = GetGpsLocationRangeFromLocationTile(tile5Beacon)
 	assert.Nil(t, err)
 	checkLocationRange(t, under, over, beacon)
 
 	tile1SanJose := GetLocationTileFromGpsLocation(sanjose, tl1)
-	under, over, err = GetGpsLocationRangeFromLocationTile(tile1SanJose, tl1)
+	under, over, err = GetGpsLocationRangeFromLocationTile(tile1SanJose)
 	assert.Nil(t, err)
 	checkLocationRange(t, under, over, sanjose)
 
 	tile2SanJose := GetLocationTileFromGpsLocation(sanjose, tl2)
-	under, over, err = GetGpsLocationRangeFromLocationTile(tile2SanJose, tl2)
+	under, over, err = GetGpsLocationRangeFromLocationTile(tile2SanJose)
 	assert.Nil(t, err)
 	checkLocationRange(t, under, over, sanjose)
 
 	tile5SanJose := GetLocationTileFromGpsLocation(sanjose, tl5)
-	under, over, err = GetGpsLocationRangeFromLocationTile(tile5SanJose, tl5)
+	under, over, err = GetGpsLocationRangeFromLocationTile(tile5SanJose)
 	assert.Nil(t, err)
 	checkLocationRange(t, under, over, sanjose)
 
 	tile1Sydney := GetLocationTileFromGpsLocation(sydney, tl1)
-	under, over, err = GetGpsLocationRangeFromLocationTile(tile1Sydney, tl1)
+	under, over, err = GetGpsLocationRangeFromLocationTile(tile1Sydney)
 	assert.Nil(t, err)
 	checkLocationRange(t, under, over, sydney)
 
 	tile2Sydney := GetLocationTileFromGpsLocation(sydney, tl2)
-	under, over, err = GetGpsLocationRangeFromLocationTile(tile2Sydney, tl2)
+	under, over, err = GetGpsLocationRangeFromLocationTile(tile2Sydney)
 	assert.Nil(t, err)
 	checkLocationRange(t, under, over, sydney)
 
 	tile5Sydney := GetLocationTileFromGpsLocation(sydney, tl5)
-	under, over, err = GetGpsLocationRangeFromLocationTile(tile5Sydney, tl5)
+	under, over, err = GetGpsLocationRangeFromLocationTile(tile5Sydney)
 	assert.Nil(t, err)
 	checkLocationRange(t, under, over, sydney)
 
 	tile1Rio := GetLocationTileFromGpsLocation(rio, tl1)
-	under, over, err = GetGpsLocationRangeFromLocationTile(tile1Rio, tl1)
+	under, over, err = GetGpsLocationRangeFromLocationTile(tile1Rio)
 	assert.Nil(t, err)
 	checkLocationRange(t, under, over, rio)
 
 	tile2Rio := GetLocationTileFromGpsLocation(rio, tl2)
-	under, over, err = GetGpsLocationRangeFromLocationTile(tile2Rio, tl2)
+	under, over, err = GetGpsLocationRangeFromLocationTile(tile2Rio)
 	assert.Nil(t, err)
 	checkLocationRange(t, under, over, rio)
 
 	tile5Rio := GetLocationTileFromGpsLocation(rio, tl5)
-	under, over, err = GetGpsLocationRangeFromLocationTile(tile5Rio, tl5)
+	under, over, err = GetGpsLocationRangeFromLocationTile(tile5Rio)
 	assert.Nil(t, err)
 	checkLocationRange(t, under, over, rio)
 }
--- metrics/grpc/rolling-stats_test.go
+++ metrics/grpc/rolling-stats_test.go
@@ -3,7 +3,6 @@ package grpcstats
 import (
 	"math"
 	"testing"
-	"time"
 
 	dme "github.com/mobiledgex/edge-cloud/d-match-engine/dme-proto"
 	"github.com/stretchr/testify/require"
@@ -23,16 +22,16 @@ func TestRollingStatsCalculations(t *testing.T) {
 		samples1 = append(samples1, s)
 	}
 	latency1 := CalculateStatistics(samples1)
-	require.Equal(t, 1.2, latency1.Min)
+	require.Equal(t, 0.0, latency1.Min)
 	require.Equal(t, 13.4, latency1.Max)
-	require.True(t, math.Abs(4.9-latency1.Avg) < errorThreshold)
-	require.True(t, math.Abs(24.025-latency1.Variance) < errorThreshold)
-	require.True(t, math.Abs(4.901530373261-latency1.StdDev) < errorThreshold)
-	require.Equal(t, uint64(5), latency1.NumSamples)
+	require.True(t, math.Abs(4.083333333333333-latency1.Avg) < errorThreshold)
+	require.True(t, math.Abs(23.221666666666664-latency1.Variance) < errorThreshold)
+	require.True(t, math.Abs(4.818886455050239-latency1.StdDev) < errorThreshold)
+	require.Equal(t, uint64(6), latency1.NumSamples)
 
 	// Test UpdateRollingLatency: min, max, avg, variance, stddev, numsamples calculation
 	r := NewRollingStatistics()
-	list2 := []float64{.53, 14.2, 21.3, 0, -4.5, 6.7, 8.8}
+	list2 := []float64{.53, 14.2, 21.3, -4.5, 6.7, 8.8}
 	// Try adding no elements, 0, and a negative number
 	r.UpdateRollingStatistics()
 	r.UpdateRollingStatistics(0)
@@ -41,32 +40,31 @@ func TestRollingStatsCalculations(t *testing.T) {
 	for _, elem := range list2 {
 		r.UpdateRollingStatistics(elem)
 	}
-	require.Equal(t, .53, r.Statistics.Min)
+	require.Equal(t, 0.0, r.Statistics.Min)
 	require.Equal(t, 21.3, r.Statistics.Max)
-	require.True(t, math.Abs(10.306-r.Statistics.Avg) < errorThreshold)
-	require.True(t, math.Abs(61.71818-r.Statistics.Variance) < errorThreshold)
-	require.True(t, math.Abs(7.8560919037394-r.Statistics.StdDev) < errorThreshold)
-	require.Equal(t, uint64(5), r.Statistics.NumSamples)
+	require.True(t, math.Abs(8.5883333333333-r.Statistics.Avg) < errorThreshold)
+	require.True(t, math.Abs(67.076816666667-r.Statistics.Variance) < errorThreshold)
+	require.True(t, math.Abs(8.1900437524269-r.Statistics.StdDev) < errorThreshold)
+	require.Equal(t, uint64(6), r.Statistics.NumSamples)
 
 	// Test UpdateRollingLatency: Adding Samples: rolling avg, min, max, stddev, variance, numsamples
 	// Update latency2 with samples1
 	// Add entire list1
 	r.UpdateRollingStatistics(list1...)
-	require.Equal(t, .53, r.Statistics.Min)
+	require.Equal(t, 0.0, r.Statistics.Min)
 	require.Equal(t, 21.3, r.Statistics.Max)
-	require.True(t, math.Abs(7.603-r.Statistics.Avg) < errorThreshold)
-	require.True(t, math.Abs(46.22609-r.Statistics.Variance) < errorThreshold)
-	require.True(t, math.Abs(6.7989771289511-r.Statistics.StdDev) < errorThreshold)
-	require.Equal(t, uint64(10), r.Statistics.NumSamples)
+	require.True(t, math.Abs(6.335833333333333-r.Statistics.Avg) < errorThreshold)
+	require.True(t, math.Abs(46.579771969696964-r.Statistics.Variance) < errorThreshold)
+	require.True(t, math.Abs(6.824937506651395-r.Statistics.StdDev) < errorThreshold)
+	require.Equal(t, uint64(12), r.Statistics.NumSamples)
 
 	// Test UpdateRollingLatency: Removing Samples: rolling avg, min, max, stddev, variance, numsamples
-	time.Sleep(time.Second * 10)
 	list3 := []float64{.34, 33.21, 11.1, 4.2, 1.5, 0, -1.2}
 	r.UpdateRollingStatistics(list3...)
-	require.Equal(t, .34, r.Statistics.Min)
+	require.Equal(t, 0.0, r.Statistics.Min)
 	require.Equal(t, 33.21, r.Statistics.Max)
-	require.True(t, math.Abs(8.4253333333333-r.Statistics.Avg) < errorThreshold)
-	require.True(t, math.Abs(83.958355238095-r.Statistics.Variance) < errorThreshold)
-	require.True(t, math.Abs(9.1628792002348-r.Statistics.StdDev) < errorThreshold)
-	require.Equal(t, uint64(15), r.Statistics.NumSamples)
+	require.True(t, math.Abs(7.021111111111112-r.Statistics.Avg) < errorThreshold)
+	require.True(t, math.Abs(79.58132810457516-r.Statistics.Variance) < errorThreshold)
+	require.True(t, math.Abs(8.92083673791731-r.Statistics.StdDev) < errorThreshold)
+	require.Equal(t, uint64(18), r.Statistics.NumSamples)
 }
