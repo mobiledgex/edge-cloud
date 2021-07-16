20c86e5eac09d8c5051b530492e62db5149d93f4
Devdatta <84735424+devdatta-MobiledgeX@users.noreply.github.com>
2021-07-16 23:04:04 +0530

    EDGECLOUD-4292 Alert needs to be raised by cluster-svc when prometheu… (#1415)
    * EDGECLOUD-4292 Alert needs to be raised by cluster-svc when prometheus deployment fails
    
    * EDGECLOUD-4292 Alert needs to be raised by cluster-svc when prometheus deployment fails - review comments
    
    * EDGECLOUD-4292 Alert needs to be raised by cluster-svc when prometheus deployment fails - review comments2

--- cloudcommon/alertnames.go
+++ cloudcommon/alertnames.go
@@ -2,14 +2,16 @@ package cloudcommon
 
 // Alert names
 const (
-	AlertAutoScaleUp             = "AutoScaleUp"
-	AlertAutoScaleDown           = "AutoScaleDown"
-	AlertClusterAutoScale        = "ClusterAutoScale"
-	AlertAppInstDown             = "AppInstDown"
-	AlertAutoUndeploy            = "AutoProvUndeploy"
-	AlertCloudletDown            = "CloudletDown"
-	AlertCloudletDownDescription = "Cloudlet resource manager is offline"
-	AlertCloudletResourceUsage   = "CloudletResourceUsage"
+	AlertAutoScaleUp                         = "AutoScaleUp"
+	AlertAutoScaleDown                       = "AutoScaleDown"
+	AlertClusterAutoScale                    = "ClusterAutoScale"
+	AlertAppInstDown                         = "AppInstDown"
+	AlertClusterSvcAppInstFailure            = "ClusterSvcAppInstFailure"
+	AlertAutoUndeploy                        = "AutoProvUndeploy"
+	AlertCloudletDown                        = "CloudletDown"
+	AlertCloudletDownDescription             = "Cloudlet resource manager is offline"
+	AlertClusterSvcAppInstFailureDescription = "Cluster-svc create AppInst failed"
+	AlertCloudletResourceUsage               = "CloudletResourceUsage"
 )
 
 // Alert types
@@ -20,6 +22,7 @@ const (
 	AlertSeverityLabel         = "severity"
 	AlertScopeApp              = "Application"
 	AlertScopeCloudlet         = "Cloudlet"
+	AlertScopePlatform         = "Platform"
 )
 
 // Alert annotation keys
