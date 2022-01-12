6228432504418e16b5ad7a014061aa79e08af2c5
Jim <jim.morris@mobiledgex.com>
2022-01-12 16:30:12 -0600

    handle redis down case

--- cloud-resource-manager/redundancy/ha_manager.go
+++ cloud-resource-manager/redundancy/ha_manager.go
@@ -35,9 +35,9 @@ type HighAvailabilityManager struct {
 	activePollInterval         time.Duration
 	PlatformInstanceActive     bool
 	ActiveTransitionInProgress bool // active from a redis standpoint but still doing switchover actions
-
-	haWatcher HAWatcher
-	nodeMgr   *node.NodeMgr
+	RedisConnectionFailed      bool // used when primary becomes active due to redis unavailability
+	haWatcher                  HAWatcher
+	nodeMgr                    *node.NodeMgr
 }
 
 func (s *HighAvailabilityManager) InitFlags() {
@@ -63,10 +63,19 @@ func (s *HighAvailabilityManager) Init(nodeGroupKey string, nodeMgr *node.NodeMg
 	s.HAEnabled = true
 	err := s.connectRedis(ctx)
 	if err != nil {
+		log.SpanLog(ctx, log.DebugLevelInfo, "connectRedis failed", "err", err)
+		if s.HARole == string(process.HARolePrimary) {
+			log.SpanLog(ctx, log.DebugLevelInfo, "Assuming active status for primary node due to redis failure")
+			s.PlatformInstanceActive = true
+			s.RedisConnectionFailed = true
+			return nil
+		}
 		return err
 	}
-	s.PlatformInstanceActive = s.tryActive(ctx)
-	return nil
+	// prior to the first active check, give the other unit an opportunity to gain activity in case this is a restart case
+	time.Sleep(s.activePollInterval)
+	s.PlatformInstanceActive, err = s.tryActive(ctx)
+	return err
 }
 
 func (s *HighAvailabilityManager) pingRedis(ctx context.Context) error {
@@ -111,39 +120,47 @@ func (s *HighAvailabilityManager) connectRedis(ctx context.Context) error {
 }
 
 // TryActive is called on startup
-func (s *HighAvailabilityManager) tryActive(ctx context.Context) bool {
-
-	if s.PlatformInstanceActive || s.ActiveTransitionInProgress {
-		// this should not happen. Only 1 thread should be doing tryActive
-		log.FatalLog("Platform already active")
-	}
-	// see if we are already active, which can happen if the process just died and was restarted quickly
-	// before the SetNX expired
-	alreadyActive := s.CheckActive(ctx)
-	if alreadyActive {
-		log.SpanLog(ctx, log.DebugLevelInfra, "Platform already active", "key", s.nodeGroupKey)
-		// bump the active timer in case it is close to expiry
-		s.BumpActiveExpire(ctx)
-		return true
+func (s *HighAvailabilityManager) tryActive(ctx context.Context) (bool, error) {
+	if !s.RedisConnectionFailed {
+		if s.PlatformInstanceActive || s.ActiveTransitionInProgress {
+			// this should not happen. Only 1 thread should be doing tryActive
+			log.FatalLog("Platform already active", "PlatformInstanceActive", s.PlatformInstanceActive, "ActiveTransitionInProgress", s.ActiveTransitionInProgress)
+		}
+		// see if we are already active, which can happen if the process just died and was restarted quickly
+		// before the SetNX expired
+		alreadyActive, err := s.CheckActive(ctx)
+		if err != nil {
+			return false, err
+		}
+		if alreadyActive {
+			log.SpanLog(ctx, log.DebugLevelInfra, "Platform already active", "key", s.nodeGroupKey)
+			// bump the active timer in case it is close to expiry
+			s.BumpActiveExpire(ctx)
+			return true, nil
+		}
 	}
 	cmd := s.redisClient.SetNX(s.nodeGroupKey, s.HARole, s.activeDuration)
 	v, err := cmd.Result()
 	if err != nil {
-		log.SpanLog(ctx, log.DebugLevelInfra, "tryActive setNX error", "key", s.nodeGroupKey, "cmd", cmd, "v", v, "err", err)
+		// don't log this if the redis is already down because it result in excessive logs
+		if !s.RedisConnectionFailed {
+			log.SpanLog(ctx, log.DebugLevelInfra, "tryActive setNX error", "key", s.nodeGroupKey, "cmd", cmd, "v", v, "err", err)
+		}
+		return false, err
 	}
-	return v
+	return v, nil
 }
 
-func (s *HighAvailabilityManager) CheckActive(ctx context.Context) bool {
+func (s *HighAvailabilityManager) CheckActive(ctx context.Context) (bool, error) {
 
 	cmd := s.redisClient.Get(s.nodeGroupKey)
 	v, err := cmd.Result()
 	if err != nil {
 		log.SpanLog(ctx, log.DebugLevelInfra, "CheckActive error", "key", s.nodeGroupKey, "cmd", cmd, "v", v, "err", err)
-		return false
+		return false, err
 	}
 	isActive := v == s.HARole
-	return isActive
+	return isActive, nil
 }
 
 func (s *HighAvailabilityManager) BumpActiveExpire(ctx context.Context) error {
@@ -174,9 +191,12 @@ func (s *HighAvailabilityManager) CheckActiveLoop(ctx context.Context) {
 				log.SpanLog(ctx, log.DebugLevelInfra, "Platform inactive, doing TryActive")
 				timeLastLog = time.Now()
 			}
-			newActive := s.tryActive(ctx)
+			newActive, err := s.tryActive(ctx)
+			if err != nil {
+				log.FatalLog("Error in tryActive - %v", err)
+			}
 			if newActive {
-				log.SpanLog(ctx, log.DebugLevelInfra, "Platform became active")
+				log.SpanLog(ctx, log.DebugLevelInfra, "Platform became active", "role", s.HARole)
 				timeLastBumpActive = time.Now()
 				// switchover is handled in a separate thread so we do not miss polling redis
 				s.ActiveTransitionInProgress = true
@@ -196,38 +216,73 @@ func (s *HighAvailabilityManager) CheckActiveLoop(ctx context.Context) {
 			}
 		} else {
 			if elapsedSinceLog >= CheckActiveLogInterval {
-				log.SpanLog(ctx, log.DebugLevelInfra, "Doing BumpActiveExpire", "ActiveTransitionInProgress", s.ActiveTransitionInProgress, "PlatformInstanceActive", s.PlatformInstanceActive)
+				log.SpanLog(ctx, log.DebugLevelInfra, "Unit is active", "ActiveTransitionInProgress", s.ActiveTransitionInProgress, "PlatformInstanceActive", s.PlatformInstanceActive, "RedisConnectionFailed", s.RedisConnectionFailed, "role", s.HARole)
 				timeLastLog = time.Now()
 			}
-			checkStillActive := false
-			if elapsedSinceBumpActive >= s.activePollInterval*2 {
-				// Somehow we missed at least one poll. this can cause another process to seize activity
-				// We need to check we are still active to avoid a split brain situation
-				log.SpanLog(ctx, log.DebugLevelInfra, "Missed active poll", "elapsedSinceBumpActive", elapsedSinceBumpActive)
-				checkStillActive = true
-			}
-			// periodically we check that we are still active to detect an unexpected split brain scenario
-			if elapsedSinceCheckActive > s.activeDuration {
-				checkStillActive = true
-			}
-			if checkStillActive {
-				stillActive := s.CheckActive(ctx)
-				if !stillActive {
-					// somehow we lost activity, this is a big problem
-					log.SpanLog(ctx, log.DebugLevelInfra, "Activity Lost Unexpectedly")
-					log.FatalLog("ERROR!: Lost activity")
+			if s.RedisConnectionFailed {
+				// redis went down at some point, we cannot bump the active state, try to regain it
+				active, err := s.tryActive(ctx)
+				if err == nil {
+					log.SpanLog(ctx, log.DebugLevelInfra, "redis connection re-established", "active", active)
+					if !active {
+						// activity was stolen when redis came back. This is possible as the secondary will restart
+						log.FatalLog("activity lost when redis connection re-established")
+					}
+					elapsedSinceBumpActive = time.Since(time.Now())
+					s.RedisConnectionFailed = false
 				}
-				timeLastCheckActive = time.Now()
+				if err != nil && elapsedSinceLog >= CheckActiveLogInterval {
+					log.SpanLog(ctx, log.DebugLevelInfra, "error in tryActive", "err", err)
+				}
 			}
-			err := s.BumpActiveExpire(ctx)
-			if err != nil {
-				log.SpanLog(ctx, log.DebugLevelInfra, "BumpActiveExpire failed, retry", "err", err)
-				err = s.BumpActiveExpire(ctx)
+			if !s.RedisConnectionFailed {
+				checkStillActive := false
+				if elapsedSinceBumpActive >= s.activePollInterval*2 {
+					// Somehow we missed at least one poll. this can cause another process to seize activity
+					// We need to check we are still active to avoid a split brain situation
+					log.SpanLog(ctx, log.DebugLevelInfra, "Missed active poll", "elapsedSinceBumpActive", elapsedSinceBumpActive)
+					checkStillActive = true
+				}
+				// periodically we check that we are still active to detect an unexpected split brain scenario
+				if elapsedSinceCheckActive > s.activeDuration {
+					checkStillActive = true
+				}
+				if checkStillActive {
+					stillActive, err := s.CheckActive(ctx)
+					if !stillActive {
+						// somehow we lost activity
+						if err != nil {
+							if s.HARole == string(process.HARolePrimary) {
+								log.SpanLog(ctx, log.DebugLevelInfra, "Maintaining active status for primary due to redis error", "err", err)
+								s.RedisConnectionFailed = true
+								s.nodeMgr.Event(ctx, "Redis error", s.nodeMgr.MyNode.Key.CloudletKey.Organization, s.nodeMgr.MyNode.Key.CloudletKey.GetTags(), err, "Node Type", s.nodeMgr.MyNode.Key.Type, "HA Role", s.HARole, "Redis Addr", s.redisAddr)
+								continue
+							} else {
+								log.FatalLog("secondary unit lost activity due to redis error")
+							}
+						} else {
+							// this is unexpected
+							log.SpanLog(ctx, log.DebugLevelInfra, "Activity Lost Unexpectedly")
+						}
+					}
+					timeLastCheckActive = time.Now()
+				}
+				err := s.BumpActiveExpire(ctx)
 				if err != nil {
-					log.FatalLog("BumpActiveExpire failed!", "err", err)
+					log.SpanLog(ctx, log.DebugLevelInfra, "BumpActiveExpire failed, retry", "err", err)
+					err = s.BumpActiveExpire(ctx)
+					if err != nil {
+						if s.HARole == string(process.HARolePrimary) {
+							log.SpanLog(ctx, log.DebugLevelInfra, "Maintaining active status for primary due to redis error", "err", err)
+							s.RedisConnectionFailed = true
+							s.nodeMgr.Event(ctx, "Redis error", s.nodeMgr.MyNode.Key.CloudletKey.Organization, s.nodeMgr.MyNode.Key.CloudletKey.GetTags(), err, "Node Type", s.nodeMgr.MyNode.Key.Type, "HA Role", s.HARole, "Redis Addr", s.redisAddr)
+						} else {
+							log.FatalLog("BumpActiveExpire failed!", "err", err)
+						}
+					}
 				}
+				timeLastBumpActive = time.Now()
 			}
-			timeLastBumpActive = time.Now()
 		}
 		time.Sleep(s.activePollInterval)
 	}
