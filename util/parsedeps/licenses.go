0cc6f1e61a9986958babe54d5cf2ae37263e2d51
Jon Gainsley <jon.gainsley@mobiledgex.com>
2022-01-15 09:31:59 -0800

    comment

--- util/parsedeps/licenses.go
+++ util/parsedeps/licenses.go
@@ -17,6 +17,7 @@ type License struct {
 	Text string
 }
 
+// These are the offical license texts.
 var Licenses = []License{{
 	Name: MITLicense,
 	Text: `Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
