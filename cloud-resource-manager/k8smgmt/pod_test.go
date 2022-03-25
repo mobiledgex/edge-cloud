1486c35c02a34ce1dfea8910cc7c7cb2ebd27acb
Jim <jim.morris@mobiledgex.com>
2022-03-25 17:24:32 -0500

    fix test (#1706)

--- cloud-resource-manager/k8smgmt/pod_test.go
+++ cloud-resource-manager/k8smgmt/pod_test.go
@@ -7,18 +7,20 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
+var podInsuffCpuMessage string = "0/2 nodes are available: 1 Insufficient cpu, 1 Insufficient memory, 1 node(s) had taint {node-role.kubernetes.io/master: }, that the pod didn't tolerate"
+
 var podStates = []string{
-	"gh-someapp-758594bfc9-px6bm   0/1   Init:0/1   0     2s",
-	"autoprov-75df8b4cfb-bndnq              1/1       Running   0          30d",
-	"demo-deployment-78bd547db7-sbqqp          0/1     ContainerCreating   0          4s",
-	"demo-deployment-78bd547db7-sbqqp          0/1     Terminating   0          24s",
+	"gh-someapp-758594bfc9-px6bm   Init   <none>   <none>",
+	"autoprov-75df8b4cfb-bndnq   Running   <none>   <none>",
+	"demo-deployment-78bd547db7-sbqqp   ContainerCreating   <none>   <none>",
+	"demo-deployment-7f8445bc4d-8jh2k   Pending   Unschedulable   " + podInsuffCpuMessage,
 }
 
 func TestPodStateRegex(t *testing.T) {
 	r := regexp.MustCompile(podStateRegString)
 	match0 := r.FindStringSubmatch(podStates[0])
 	require.NotNil(t, match0)
-	require.Equal(t, "Init:0/1", match0[2])
+	require.Equal(t, "Init", match0[2])
 
 	match1 := r.FindStringSubmatch(podStates[1])
 	require.NotNil(t, match1)
@@ -26,9 +28,11 @@ func TestPodStateRegex(t *testing.T) {
 
 	match2 := r.FindStringSubmatch(podStates[2])
 	require.NotNil(t, match2)
-	require.Equal(t, "ContainerCreating", match2[2])
+	require.Equal(t, "<none>", match2[3])
+	require.Equal(t, "<none>", match2[4])
 
 	match3 := r.FindStringSubmatch(podStates[3])
 	require.NotNil(t, match3)
-	require.Equal(t, "Terminating", match3[2])
+	require.Equal(t, "Unschedulable", match3[3])
+	require.Equal(t, podInsuffCpuMessage, match3[4])
 }
