3a861e16e4c460702929b55475e515a2fa5e31b0
Ashish Jain <ashish.jain@mobiledgex.com>
2022-04-01 13:06:58 +0530

    Incorporate review comment

--- controller/cloudlet_api.go
+++ controller/cloudlet_api.go
@@ -2590,30 +2590,14 @@ func (s *CloudletApi) GetOrganizationsOnCloudlet(in *edgeproto.CloudletKey, cb e
 
 func (s *CloudletApi) GetCloudletGPUDriverLicenseConfig(ctx context.Context, key *edgeproto.CloudletKey) (*edgeproto.Result, error) {
 	cloudlet := edgeproto.Cloudlet{}
-	gpuDriver := edgeproto.GPUDriver{}
-	err := s.sync.ApplySTMWait(ctx, func(stm concurrency.STM) error {
-		if !s.store.STMGet(stm, key, &cloudlet) {
-			return key.NotFoundError()
-		}
-		if cloudlet.GpuConfig.Driver.Name == "" {
-			return fmt.Errorf("Cloudlet is not associated with any GPU driver")
-		}
-		if !s.all.gpuDriverApi.store.STMGet(stm, &cloudlet.GpuConfig.Driver, &gpuDriver) {
-			return cloudlet.GpuConfig.Driver.NotFoundError()
-		}
-		if cloudlet.LicenseConfigStoragePath == "" {
-			return fmt.Errorf("Cloudlet license config storage path is empty")
-		}
-		return nil
-	})
-	if err != nil {
-		return &edgeproto.Result{}, err
+	if !s.store.Get(ctx, key, &cloudlet) {
+		return key.NotFoundError()
 	}
-	cfg, err := GetLicenseConfigFromStorageServer(ctx, &gpuDriver, cloudlet.LicenseConfigStoragePath)
-	if err != nil {
-		return &edgeproto.Result{}, err
+	if cloudlet.GpuConfig.Driver.Name == "" {
+		return fmt.Errorf("Cloudlet is not associated with any GPU driver")
 	}
-	return &edgeproto.Result{
-		Message: cfg,
-	}, nil
+	if cloudlet.LicenseConfigStoragePath == "" {
+		return fmt.Errorf("Cloudlet license config storage path is empty")
+	}
+	return s.api.gpuDriverApi.GetGPUDriverLicenseConfig(ctx, &cloudlet.GpuConfig.Driver)
 }
