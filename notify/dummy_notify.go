3d5254ca75251e459f5ecd0fec86b10ce76d7f71
Jon <38672162+gainsley@users.noreply.github.com>
2022-04-12 13:18:21 -0700

    Open-source under apache license (#1729)

--- notify/dummy_notify.go
+++ notify/dummy_notify.go
@@ -1,3 +1,17 @@
+// Copyright 2022 MobiledgeX, Inc
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 // Dummy Sender/Receiver for unit testing.
 // These are exported because the notify package is meant to be included
 // in other processes, so to include these structs in other package's
